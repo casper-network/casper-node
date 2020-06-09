a0e8b0934cbf0b526bfb64a5b61fb19373a5ab6b
Fraser Hutchison <fraser@casperlabs.io>
2020-06-09 22:58:13 +0100

    NDRS-76: remove some nested error variants

--- src/components/small_network/error.rs
+++ src/components/small_network/error.rs
@@ -1,6 +1,5 @@
-use std::{io, result, time::SystemTimeError};
+use std::{result, time::SystemTimeError};
 
-use openssl::error::ErrorStack;
 use thiserror::Error;
 use tokio::net::TcpStream;
 use tokio_openssl::HandshakeError;
@@ -15,21 +14,36 @@ pub enum Error {
     /// Server failed to present certificate.
     #[error("no server certificate presented")]
     NoServerCertificate,
-    /// Peer failed to present certificate.
-    #[error("no peer certificate presented")]
-    NoPeerCertificate,
-    /// Peer has wrong ID.
+    /// Client failed to present certificate.
+    #[error("no client certificate presented")]
+    NoClientCertificate,
+    /// Peer ID presented does not match the expected one.
     #[error("remote node has wrong ID")]
     WrongId,
-    /// Invalid configuration.
+    /// The config must have both or neither of certificate and private key.
     #[error("need either both or none of cert, private_key in network config")]
     InvalidConfig,
-    /// I/O error.
-    #[error("I/O error: {0}")]
-    Io(#[from] io::Error),
-    /// OpenSSL error.
-    #[error("OpenSSL error: {0}")]
-    OpenSSL(#[from] ErrorStack),
+    /// Failed to create a TCP listener.
+    #[error("failed to create listener")]
+    ListenerCreation,
+    /// Failed to get TCP listener address.
+    #[error("failed to get listener addr")]
+    ListenerAddr,
+    /// Failed to convert std TCP listener to tokio TCP listener.
+    #[error("failed to convert listener to tokio")]
+    ListenerConversion,
+    /// Failed to send message.
+    #[error("failed to send message")]
+    MessageNotSent,
+    /// Failed to create TLS acceptor.
+    #[error("failed to create acceptor")]
+    AcceptorCreation,
+    /// Failed to create configuration for TLS connector.
+    #[error("failed to configure connector")]
+    ConnectorConfiguration,
+    /// Failed to generate node TLS certificate.
+    #[error("failed to generate cert")]
+    CertificateGeneration,
     /// Handshaking error.
     #[error("handshake error: {0}")]
     Handshake(#[from] HandshakeError<TcpStream>),
