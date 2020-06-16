4704d10a80087e4e1c5f893120cd3fcd2376d4c5
Marc Brinkmann <marc@casperlabs.io>
2020-06-16 18:53:23 +0200

    Add missing `validator::config` module

---
+++ src/reactor/validator/config.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    ApiServerConfig, SmallNetworkConfig, StorageConfig, ROOT_PUBLIC_LISTENING_PORT,
+    ROOT_VALIDATOR_LISTENING_PORT,
+};
+
+/// Root configuration.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Config {
+    /// Network configuration for the validator-only network.
+    pub validator_net: SmallNetworkConfig,
+    /// Network configuration for the public network.
+    pub public_net: SmallNetworkConfig,
+    /// Network configuration for the HTTP API.
+    pub http_server: ApiServerConfig,
+    /// On-disk storage configuration.
+    pub storage: StorageConfig,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            validator_net: SmallNetworkConfig::default_on_port(ROOT_VALIDATOR_LISTENING_PORT),
+            public_net: SmallNetworkConfig::default_on_port(ROOT_PUBLIC_LISTENING_PORT),
+            http_server: ApiServerConfig::default(),
+            storage: StorageConfig::default(),
+        }
+    }
+}
