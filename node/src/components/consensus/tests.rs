29a4c7b0251b758c3e6668d3cd1fca8fe7ad8dd8
Mateusz Górski <gorski.mateusz@protonmail.ch>
2020-07-17 19:14:51 +0200

    HWY-93: Add comments to `tests.rs`.

--- node/src/components/consensus/tests.rs
+++ node/src/components/consensus/tests.rs
@@ -1,2 +1,4 @@
+/// Module containing basic building blocks for the Discrete Event Simulator (DES).
 pub(crate) mod consensus_des_testing;
+/// Module containing message queue.
 pub(crate) mod queue;
