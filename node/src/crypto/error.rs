997ab2d2aa79fa6fcca1cd1f2c80f676904fd76a
Michał Papierski <michal@casperlabs.io>
2020-07-17 14:00:27 +0200

    NO-TICKET: Hold signature error as string.
    This solves lack of useful traits in `SignatureError` in recent
    ed25519-dalek crate.

--- node/src/crypto/error.rs
+++ node/src/crypto/error.rs
@@ -10,11 +10,11 @@ use thiserror::Error;
 pub type Result<T> = result::Result<T, Error>;
 
 /// Cryptographic errors.
-#[derive(Debug, Error)]
+#[derive(Clone, PartialEq, Debug, Error)]
 pub enum Error {
     /// Error resulting from creating or using asymmetric key types.
     #[error("asymmetric key error: {0}")]
-    AsymmetricKey(SignatureError),
+    AsymmetricKey(String),
     /// Error resulting when decoding a type from a hex-encoded representation.
     #[error("parsing from hex: {0}")]
     FromHex(#[from] FromHexError),
@@ -31,7 +31,7 @@ pub enum Error {
 
 impl From<SignatureError> for Error {
     fn from(signature_error: SignatureError) -> Self {
-        Error::AsymmetricKey(signature_error)
+        Error::AsymmetricKey(signature_error.to_string())
     }
 }
 
