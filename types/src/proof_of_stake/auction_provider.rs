ed10d262d6beb06f9c22c930e35be64f64079ce3
Onur Solmaz <onursolmaz@gmail.com>
2020-08-05 14:29:57 +0200

    Added missing file

---
+++ types/src/proof_of_stake/auction_provider.rs
@@ -0,0 +1,19 @@
+use crate::{auction::SeigniorageRecipients, account::AccountHash, U512, system_contract_errors::pos::Error};
+use alloc::vec::Vec;
+use std::collections::BTreeSet;
+
+pub trait AuctionProvider {
+    fn read_winners(&mut self) -> BTreeSet<AccountHash> {
+        todo!()
+    }
+
+    fn read_seigniorage_recipients(&mut self) -> SeigniorageRecipients {
+        todo!()
+    }
+
+    /// Distributes `amount` to delegators associated with `validator_account_hash` proportional
+    /// to delegated amount.
+    fn distribute_to_delegators(&mut self, validator_account_hash: AccountHash, amount: U512) -> Result<(), Error> {
+        todo!()
+    }
+}
