1b5b20db4d001b726ba060e7c21e167c3198f7a4
Michał Papierski <michal@papierski.net>
2020-08-14 16:45:00 +0200

    Update types/src/mint/era_provider.rs
    Co-authored-by: Andreas Fackler <afck@users.noreply.github.com>

--- types/src/mint/era_provider.rs
+++ types/src/mint/era_provider.rs
@@ -1,6 +1,6 @@
 use crate::auction::EraId;
 
-/// Provider for obtainin current era id.
+/// Provider for obtaining current era id.
 pub trait EraProvider {
     /// Returns current era id.
     fn read_era_id(&mut self) -> EraId;
