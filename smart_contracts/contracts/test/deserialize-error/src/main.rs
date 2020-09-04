ca3e434bb14844a2763bb7a3507e6335c6e7ec56
Andreas Fackler <andreas@casperlabs.io>
2020-09-04 15:20:26 +0200

    Fix Clippy lint.

--- smart_contracts/contracts/test/deserialize-error/src/main.rs
+++ smart_contracts/contracts/test/deserialize-error/src/main.rs
@@ -5,11 +5,7 @@ extern crate alloc;
 
 use alloc::{vec, vec::Vec};
 
-use casper_contract::{
-    self,
-    contract_api::{self, storage},
-    unwrap_or_revert::UnwrapOrRevert,
-};
+use casper_contract::{self, contract_api::storage, unwrap_or_revert::UnwrapOrRevert};
 use casper_types::{
     api_error, bytesrepr::ToBytes, contracts::Parameters, CLType, ContractHash, EntryPoint,
     EntryPointAccess, EntryPointType, EntryPoints, RuntimeArgs,
