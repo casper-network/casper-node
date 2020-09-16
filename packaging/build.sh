72df45a467da12edf11acc20f2204f9aff31b31e
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-09-15 20:59:30 -0400

    .deb building docker updates and test for genesis.

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Having to -f because of right from docker build. Need to fix.
+rm -f ./share/*.deb
 docker-compose up
-docker-compose rm
+docker-compose rm -y
+docker rm packaging_deb_1804_1
+docker rm packaging_deb_2004_1
+docker rm packaging_builddeb_1
