432c4f03ecdeb45d4fee2c3f6d2bbd5f52810ed8
Marc Brinkmann <marc@casperlabs.io>
2020-10-08 16:10:33 +0200

    Insert `@DEBUG` only on non-release versions

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -33,6 +33,7 @@ pub mod tls;
 pub mod types;
 pub mod utils;
 
+use ansi_term::Color::Red;
 use lazy_static::lazy_static;
 
 pub(crate) use components::small_network;
@@ -49,17 +50,23 @@ pub use utils::OS_PAGE_SIZE;
 
 lazy_static! {
     /// Version string for the compiled node. Filled in at build time, output allocated at runtime.
-    pub static ref VERSION_STRING: String = if env!("VERGEN_SEMVER_LIGHTWEIGHT") == "UNKNOWN" {
-        format!("{}@{}",
-            env!("CARGO_PKG_VERSION"),
-            env!("NODE_BUILD_PROFILE"),
-        )
-    } else {
-        format!(
-            "{}-{}@{}",
-            env!("VERGEN_SEMVER_LIGHTWEIGHT"),
-            env!("VERGEN_SHA_SHORT"),
-            env!("NODE_BUILD_PROFILE"),
-        )
+    pub static ref VERSION_STRING: String = {
+        let mut version = if env!("VERGEN_SEMVER_LIGHTWEIGHT") == "UNKNOWN" {
+            env!("CARGO_PKG_VERSION").to_string()
+        } else {
+            format!(
+                "{}-{}",
+                env!("VERGEN_SEMVER_LIGHTWEIGHT"),
+                env!("VERGEN_SHA_SHORT"),
+            )
+        };
+
+        // Add an `@DEBUG` (or similar) tag in read on non-release versions.
+        if env!("NODE_BUILD_PROFILE") != "release" {
+            version += "@";
+            version.push_str(&Red.paint(&env!("NODE_BUILD_PROFILE").to_uppercase()).to_string());
+        }
+
+        version
     };
 }
