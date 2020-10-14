030c97af6f14d09975d991d23c52d670c983b5c2
Marc Brinkmann <marc@casperlabs.io>
2020-10-14 17:58:54 +0200

    Cleanup cosy utils, unused functions

--- cosy-macro/src/util.rs
+++ cosy-macro/src/util.rs
@@ -1,15 +1,10 @@
-use inflector::cases::snakecase::to_snake_case;
 use syn::{export::Span, Ident};
-
-pub(crate) fn snake_ident(id: &Ident) -> Ident {
-    to_ident(&to_snake_case(&id.to_string()))
-}
 
 pub(crate) fn to_ident(s: &str) -> Ident {
     Ident::new(s, Span::call_site())
 }
 
+/// Returns an ident with a suffix.
 pub(crate) fn suffix_ident(ident: &Ident, suffix: &str) -> Ident {
-    /// Returns an ident with a suffix.
     to_ident(&format!("{}{}", ident.to_string(), suffix))
 }
