842b95d79dd1be1025b45898b22befe99080fc13
Matthew Doty <mpwd@casperlabs.io>
2020-10-15 15:47:15 -0400

    [NDRS-499] - Introducing scheduling system for vertices to be added in the future

--- node/src/components/consensus/protocols/highway.rs
+++ node/src/components/consensus/protocols/highway.rs
@@ -42,6 +42,9 @@ where
     pending_values: HashMap<C::ConsensusValue, Vec<ValidVertex<C>>>,
     finality_detector: FinalityDetector<C>,
     highway: Highway<C>,
+    /// The vertices that are scheduled to be processed at a later time.  The keys of this
+    /// `BTreeMap` are timestamps when the corresponding vector of vertices will be added added.
+    vertices_to_be_added_later: BTreeMap<Timestamp, Vec<(I, PreValidatedVertex<C>)>>,
 }
 
 impl<I: NodeIdT, C: Context> HighwayProtocol<I, C> {
@@ -56,6 +59,7 @@ impl<I: NodeIdT, C: Context> HighwayProtocol<I, C> {
             pending_values: HashMap::new(),
             finality_detector: FinalityDetector::new(ftt),
             highway: Highway::new(instance_id, validators, params),
+            vertices_to_be_added_later: BTreeMap::new(),
         }
     }
 
@@ -118,6 +122,46 @@ impl<I: NodeIdT, C: Context> HighwayProtocol<I, C> {
             .run(&self.highway)
             .expect("too many faulty validators")
             .map(ConsensusProtocolResult::FinalizedBlock)
+    }
+
+    /// Store a (pre-validated) vertex which will be added later.  This creates a timer to be sent
+    /// to the reactor. The vertex be added using `Self::add_vertices` when that timer goes off.
+    fn store_vertex_for_addition_later(
+        &mut self,
+        future_timestamp: Timestamp,
+        sender: I,
+        pvv: PreValidatedVertex<C>,
+    ) -> Vec<CpResult<I, C>> {
+        self.vertices_to_be_added_later
+            .entry(future_timestamp)
+            .or_insert(vec![])
+            .push((sender, pvv));
+        vec![ConsensusProtocolResult::ScheduleTimer(future_timestamp)]
+    }
+
+    /// Call `Self::add_vertices` on any vertices in `vertices_to_be_added_later` which are
+    /// scheduled for after the given `transpired_timestamp`.  In general the specified
+    /// `transpired_timestamp` is approximately `Timestamp::now()`.  Vertices keyed by timestamps
+    /// chronologically before `transpired_timestamp` should all be added.
+    fn add_past_due_stored_vertices(
+        &mut self,
+        timestamp: Timestamp,
+        rng: &mut dyn CryptoRngCore,
+    ) -> Vec<CpResult<I, C>> {
+        let mut results = vec![];
+        let past_due_timestamps: Vec<Timestamp> = self
+            .vertices_to_be_added_later
+            .range(..=timestamp) // Inclusive range
+            .map(|(past_due_timestamp, _)| past_due_timestamp.to_owned())
+            .collect();
+        for past_due_timestamp in past_due_timestamps {
+            if let Some(vertices_to_add) =
+                self.vertices_to_be_added_later.remove(&past_due_timestamp)
+            {
+                results.extend(self.add_vertices(vertices_to_add, rng))
+            }
+        }
+        results
     }
 
     /// Adds the given vertices to the protocol state, if possible, or requests missing
@@ -256,17 +300,22 @@ where
                 Ok(vec![])
             }
             Ok(HighwayMessage::NewVertex(v)) => {
-                // TODO: Incoming vertex, check timestamp and schedule a delay
-                match self.highway.pre_validate_vertex(v) {
-                    Ok(pvv) => Ok(self.add_vertices(vec![(sender, pvv)], rng)),
+                let pvv = match self.highway.pre_validate_vertex(v) {
+                    Ok(pvv) => pvv,
                     Err((_, err)) => {
                         // TODO: Disconnect from senders.
-                        Ok(vec![ConsensusProtocolResult::InvalidIncomingMessage(
+                        return Ok(vec![ConsensusProtocolResult::InvalidIncomingMessage(
                             msg,
                             sender,
                             err.into(),
-                        )])
+                        )]);
                     }
+                };
+                match pvv.timestamp() {
+                    Some(timestamp) if timestamp > Timestamp::now() => {
+                        Ok(self.store_vertex_for_addition_later(timestamp, sender, pvv))
+                    }
+                    _ => Ok(self.add_vertices(vec![(sender, pvv)], rng)),
                 }
             }
             Ok(HighwayMessage::RequestDependency(dep)) => {
@@ -293,7 +342,9 @@ where
         rng: &mut dyn CryptoRngCore,
     ) -> Result<Vec<CpResult<I, C>>, Error> {
         let effects = self.highway.handle_timer(timestamp, rng);
-        Ok(self.process_av_effects(effects))
+        let mut results = self.process_av_effects(effects);
+        results.extend(self.add_past_due_stored_vertices(timestamp, rng));
+        Ok(results)
     }
 
     fn propose(
