548c373960ce5af7584e78f2eb2fe218d82128f8
Karan Dhareshwar <karan@casperlabs.io>
2020-10-19 09:17:39 -0500

    Updated ConsensusMetrics

--- node/src/components/consensus/metrics.rs
+++ node/src/components/consensus/metrics.rs
@@ -7,6 +7,8 @@ pub struct ConsensusMetrics {
     pub finalization_time: Gauge,
     /// Amount of finalized blocks.
     pub finalized_block_count: IntCounter,
+    /// Timestamp of the most recently accepted proto block. 
+    pub time_since_proto_block: Gauge,
     /// registry component.
     registry: Registry,
 }
@@ -19,11 +21,13 @@ impl ConsensusMetrics {
         )?;
         let finalized_block_count =
             IntCounter::new("amount_of_blocks", "the number of blocks finalized so far")?;
+        let time_since_proto_block = Gauge::new("time_of_last_proto_block","timestamp of the most recently accepted proto block")?;
         registry.register(Box::new(finalization_time.clone()))?;
         registry.register(Box::new(finalized_block_count.clone()))?;
         Ok(ConsensusMetrics {
             finalization_time,
             finalized_block_count,
+            time_since_proto_block,
             registry: registry.clone(),
         })
     }
