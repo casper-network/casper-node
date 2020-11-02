caf335115483cce4e0d265948fe9b34072751af3
Daniel N. Werner <dan@casperlabs.io>
2020-11-02 15:29:59 -0800

    Update client/examples/ffi/src/test.c
    Co-authored-by: Henry Till <henrytill@gmail.com>

--- client/examples/ffi/src/test.c
+++ client/examples/ffi/src/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "headers/casper_client.h"
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     const char *node_address = "http://localhost:7777";
     const char *rpc_id = "1";
     bool verbose = true;
