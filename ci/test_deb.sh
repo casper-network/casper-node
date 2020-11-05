707c1eb1ddbfc5d15ed9c5d21d9f283efcb533ef
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-11-05 11:37:24 -0500

    apt to apt-get and -y for test_deb.sh

--- ci/test_deb.sh
+++ ci/test_deb.sh
@@ -2,10 +2,10 @@
 set -ex
 
 echo "$1"/"$2"*.deb
-apt install "$1"/target/debian/"$2"*.deb
+apt-get install -y "$1"/target/debian/"$2"*.deb
 
 if ! type "$2" > /dev/null; then
   exit 1
 fi
 
-apt remove -y "$2"
+apt-get remove -y "$2"
