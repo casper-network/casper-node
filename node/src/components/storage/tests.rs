f6877a2f8f840d00460ef67e17ead30642ef28a7
Marc Brinkmann <marc@casperlabs.io>
2020-11-09 12:23:29 +0100

    Complete tests for putting and getting blocks

--- node/src/components/storage/tests.rs
+++ node/src/components/storage/tests.rs
@@ -1,7 +1,10 @@
 use super::{Config, Storage};
 use crate::{
-    crypto::hash::Digest, effect::requests::StorageRequest, testing::ComponentHarness,
-    types::BlockHash, utils::WithDir,
+    crypto::hash::Digest,
+    effect::requests::StorageRequest,
+    testing::ComponentHarness,
+    types::{Block, BlockHash},
+    utils::WithDir,
 };
 
 /// Storage component test fixture.
@@ -40,15 +43,42 @@ fn get_block_of_non_existant_block_returns_none() {
     assert!(response.is_none());
 }
 
-// #[test]
-// fn can_put_and_get_block() {
-//     panic!("YAY");
-// }
+#[test]
+fn can_put_and_get_block() {
+    let mut harness = ComponentHarness::new();
+    let mut storage = storage_fixture(&mut harness);
+
+    // Create a random block, load and store it.
+    let block = Box::new(Block::random(&mut harness.rng));
+    let block2 = block.clone();
+
+    let was_new = harness.send_request(&mut storage, move |responder| {
+        StorageRequest::PutBlock {
+            block: block,
+            responder,
+        }
+        .into()
+    });
+
+    assert!(was_new, "putting block should have returned `true`");
+
+    // Storing the same block again should work, but yield a result of `false`.
+    let was_new_second_time = harness.send_request(&mut storage, move |responder| {
+        StorageRequest::PutBlock {
+            block: block2,
+            responder,
+        }
+        .into()
+    });
+
+    assert!(
+        !was_new_second_time,
+        "storing block the second time should have returned `false`"
+    );
+}
 
 // fn STORAGE(s: StorageRequest) {
 //     match s {
-//         StorageRequest::PutBlock { block, responder } => {}
-//         StorageRequest::GetBock { block_hash, responder } => {}
 //         StorageRequest::GetBlockAtHeight { height, responder } => {}
 //         StorageRequest::GetHighestBlock { responder } => {}
 //         StorageRequest::GetBlockHeader { block_hash, responder } => {}
