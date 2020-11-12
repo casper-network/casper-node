d57d1d9527e5db35f4dfd9974d822d93132fe241
Michał Papierski <michal@casperlabs.io>
2020-11-12 13:08:36 +0100

    NDRS-473: Remove wabt dependency from codebase.
    Under WSL2 it gave me linking errors related to wabt-sys which is quite
    weird as my WSL2 VM is more or less vanilla Ubuntu 20.04. Anyway the
    dependency wasn't necessary for most of the part, and some usages of
    handcrafted WATs were suboptimal anyway.

--- execution_engine/src/shared/wasm.rs
+++ execution_engine/src/shared/wasm.rs
@@ -1,24 +1,27 @@
-use parity_wasm::elements::Module;
+use casper_types::contracts::DEFAULT_ENTRY_POINT_NAME;
+use parity_wasm::{builder, elements::Module};
 
 use crate::shared::wasm_prep::{PreprocessingError, Preprocessor};
 
-static DO_NOTHING: &str = r#"
-    (module
-      (type (;0;) (func))
-      (func $call (type 0))
-      (table (;0;) 1 1 funcref)
-      (memory (;0;) 16)
-      (global (;0;) (mut i32) (i32.const 1048576))
-      (global (;1;) i32 (i32.const 1048576))
-      (global (;2;) i32 (i32.const 1048576))
-      (export "memory" (memory 0))
-      (export "call" (func $call))
-      (export "__data_end" (global 1))
-      (export "__heap_base" (global 2)))
-    "#;
-
-pub fn do_nothing_bytes() -> Vec<u8> {
-    wabt::wat2wasm(DO_NOTHING).expect("failed to parse wat")
+/// Creates minimal session code that does nothing
+fn do_nothing_bytes() -> Vec<u8> {
+    let module = builder::module()
+        .function()
+        // A signature with 0 params and no return type
+        .signature()
+        .build()
+        .body()
+        .build()
+        .build()
+        // Export above function
+        .export()
+        .field(DEFAULT_ENTRY_POINT_NAME)
+        .build()
+        // Memory section is mandatory
+        .memory()
+        .build()
+        .build();
+    parity_wasm::serialize(module).expect("should serialize")
 }
 
 pub fn do_nothing_module(preprocessor: &Preprocessor) -> Result<Module, PreprocessingError> {
