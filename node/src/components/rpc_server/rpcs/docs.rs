86bbfc5e2f99bcac23dd73dc4f9427ae33e5d610
Karan Dhareshwar <karan@casperlabs.io>
2020-11-12 16:49:00 -0600

    Filled in placeholder value for MerkleProof

--- node/src/components/rpc_server/rpcs/docs.rs
+++ node/src/components/rpc_server/rpcs/docs.rs
@@ -37,6 +37,9 @@ use crate::{
 };
 
 lazy_static! {
+    static ref MERKLE_PROOF: String = String::from("01000000006ef2e0949ac76e55812421f755abe129b6244fe7168b77f47a72536147614625016ef2e0949ac76e55812421f755abe129b6244fe7168b77f47a72536147614625000000003529cde5c621f857f75f3810611eb4af3f998caaa9d4a3413cf799f99c67db0307010000006ef2e0949ac76e55812421f755abe129b6244fe7168b77f47a7253614761462501010102000000006e06000000000074769d28aac597a36a03a932d4b43e4f10bf0403ee5c41dd035102553f5773631200b9e173e8f05361b681513c14e25e3138639eb03232581db7557c9e8dbbc83ce94500226a9a7fe4f2b7b88d5103a4fc7400f02bf89c860c9ccdd56951a2afe9be0e0267006d820fb5676eb2960e15722f7725f3f8f41030078f8b2e44bf0dc03f71b176d6e800dc5ae9805068c5be6da1a90b2528ee85db0609cc0fb4bd60bbd559f497a98b67f500e1e3e846592f4918234647fca39830b7e1e6ad6f5b7a99b39af823d82ba1873d000003000000010186ff500f287e9b53f823ae1582b1fa429dfede28015125fd233a31ca04d5012002015cc42669a55467a1fdf49750772bfc1aed59b9b085558eb81510e9b015a7c83b0301e3cf4a34b1db6bfa58808b686cb8fe21ebe0c1bcbcee522649d2b135fe510fe3");
+
+
     static ref DOCS_RPC_RESULT: GetRpcsResult = {
         let mut result = GetRpcsResult {
             api_version: CLIENT_API_VERSION.clone(),
@@ -182,8 +185,7 @@ lazy_static! {
             let response_result = GetBalanceResult {
                 api_version: CLIENT_API_VERSION.clone(),
                 balance_value: U512::from(1234567),
-                // TODO! Find a concrete example of proof.
-                merkle_proof: String::from("Proof"),
+                merkle_proof: MERKLE_PROOF.clone().to_string(),
             };
 
             result.push_with_params::<GetBalance> (
