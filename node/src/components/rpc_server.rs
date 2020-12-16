f7e674396bb0b11e6de83f9f449e6b3ffc7b0757
Karan Dhareshwar <karan@casperlabs.io>
2020-12-16 13:05:20 -0600

    Removed commented lines

--- node/src/components/rpc_server.rs
+++ node/src/components/rpc_server.rs
@@ -176,13 +176,9 @@ where
         event: Self::Event,
     ) -> Effects<Self::Event> {
         match event {
-            Event::RpcRequest(RpcRequest::SubmitDeploy { deploy, responder }) => {
-                effect_builder
-                    .announce_deploy_received(deploy, Some(responder))
-                    .ignore()
-                //effects.extend(responder.respond(()).ignore());
-                //effects
-            }
+            Event::RpcRequest(RpcRequest::SubmitDeploy { deploy, responder }) => effect_builder
+                .announce_deploy_received(deploy, Some(responder))
+                .ignore(),
             Event::RpcRequest(RpcRequest::GetBlock {
                 maybe_id: Some(BlockIdentifier::Hash(hash)),
                 responder,
