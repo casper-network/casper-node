4cbae56ed26745d3a2d0f38f171e64b12d165df4
Fraser Hutchison <fraser@casperlabs.io>
2020-12-29 17:09:22 +0000

    NO-TICKET: avoid injecting unique ID into each fresh round of address gossiping

--- node/src/reactor/validator/tests.rs
+++ node/src/reactor/validator/tests.rs
@@ -13,7 +13,7 @@ use log::info;
 use num_rational::Ratio;
 
 use crate::{
-    components::{consensus::EraId, small_network, storage},
+    components::{consensus::EraId, gossiper, small_network, storage},
     crypto::asymmetric_key::{PublicKey, SecretKey},
     reactor::{initializer, joiner, validator, Runner},
     testing::{self, network::Network, ConditionCheckReactor, TestRng},
@@ -91,6 +91,7 @@ impl TestChain {
             } else {
                 small_network::Config::default_local_net(first_node_port)
             },
+            gossip: gossiper::Config::new_with_small_timeouts(),
             ..Default::default()
         };
 
