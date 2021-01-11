cee658cef285f9b6161187fcb1aafba384bdc709
Marc Brinkmann <marc@casperlabs.io>
2021-01-11 13:03:42 +0100

    Added publishing note

--- nix/build-node.sh
+++ nix/build-node.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+# Creates a new docker image, without importing it into the local docker repo or uploading it.
+
 set -eu
 
 TAG=$(git describe --always --dirty)
+TARGET_REPO=clmarc
 
 nix-build --argstr tag "${TAG}" node-container.nix
 
-echo "Created new docker image."
+echo "Created new docker image casper-node:${TAG}."
 echo
 echo "Load into local docker:"
 echo "docker load -i ./result"
+echo
+echo "Publish image"
+echo "skopeo --insecure-policy copy docker-archive:./result docker://${TARGET_REPO}/casper-node:${TAG}"
