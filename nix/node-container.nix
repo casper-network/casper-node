6c977b88722271ad6490c6504dd97090f25c9b50
Marc Brinkmann <marc@casperlabs.io>
2021-01-11 20:15:10 +0100

    Add `busybox` to container image

--- nix/node-container.nix
+++ nix/node-container.nix
@@ -8,6 +8,7 @@ let casper-node = (import ./node.nix) { };
 in pkgs.dockerTools.buildImage {
   name = "casper-node";
   tag = tag;
+  contents = with pkgs; [ busybox ];
   config = {
     Cmd = [ "validator" "/config/config.toml" ];
     Entrypoint = [ "${casper-node}/bin/casper-node" ];
