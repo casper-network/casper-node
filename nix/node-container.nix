910abdd14f9aa8680a074131eec88e4604e2efbf
Marc Brinkmann <marc@casperlabs.io>
2021-01-13 21:07:11 +0100

    Include `dnsutils` in container image

--- nix/node-container.nix
+++ nix/node-container.nix
@@ -8,7 +8,7 @@ let casper-node = (import ./node.nix) { };
 in pkgs.dockerTools.buildImage {
   name = "casper-node";
   tag = tag;
-  contents = with pkgs; [ busybox ];
+  contents = with pkgs; [ busybox dnsutils ];
   config = {
     Cmd = [ "validator" "/config/node/config.toml" ];
     Entrypoint = [ "${casper-node}/bin/casper-node" ];
