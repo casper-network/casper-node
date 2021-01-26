8685cb5c066e6208630f2b7cc44bc31617cafcc3
Marc Brinkmann <marc@casperlabs.io>
2021-01-27 00:10:05 +0100

    Updated `cargoSha256` hash

--- nix/node.nix
+++ nix/node.nix
@@ -9,7 +9,7 @@ let
 in rustPlatform.buildRustPackage rec {
   name = "casper-node";
   pname = "casper-node";
-  cargoSha256 = "0mzp9r62vx4cayis5fhm0sksr5pn0zly85my30mprrn3dfvwma01";
+  cargoSha256 = "1v7did47ckmmlkrkp6rd5r4gqjrl22gm5zwi710w66lr00zvc092";
   src = source;
   buildInputs = with pkgs; [ openssl ];
   nativeBuildInputs = with pkgs; [ pkg-config ];
