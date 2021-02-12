c5c396bd586cc22b833019116398c88d37008f7b
Marc Brinkmann <marc@casperlabs.io>
2021-02-12 16:30:26 +0100

    Finish first usable version of `mem_export.py`

--- utils/nctl-metrics/mem_export.py
+++ utils/nctl-metrics/mem_export.py
@@ -6,7 +6,6 @@
 
 from http.client import HTTPConnection
 import os
-from pprint import pprint
 import socket
 import sys
 import time
@@ -31,61 +30,68 @@ class UnixStreamTransport(client.Transport, object):
         return UnixStreamHTTPConnection(self.socket_path)
 
 
-net_name = "net-1"
-sock_addr = os.path.join(
-    os.path.dirname(__file__),
-    "..",
-    "nctl",
-    "assets",
-    net_name,
-    "daemon",
-    "socket",
-    "supervisord.sock",
-)
-delay = 1
+def main():
+    net_name = "net-1"
+    sock_addr = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "nctl",
+        "assets",
+        net_name,
+        "daemon",
+        "socket",
+        "supervisord.sock",
+    )
+    delay = 1
 
-gauges = {
-    "rss": Gauge("os_mem_rss_bytes", "Resident Set Size", ["node"]),
-    "vms": Gauge("os_mem_vms_bytes", "Virtual Memory Size", ["node"]),
-    "shared": Gauge("os_mem_shared_bytes", "Shared memory size", ["node"]),
-    "text": Gauge("os_mem_text_bytes", "Text memory size", ["node"]),
-    "lib": Gauge("os_mem_lib_bytes", "Lib memory size", ["node"]),
-    "data": Gauge("os_mem_data_bytes", "Data memory size", ["node"]),
-    "dirty": Gauge("os_mem_dirty_bytes", "Dirty memory size", ["node"]),
-}
+    gauges = {
+        "rss": Gauge("os_mem_rss_bytes", "Resident Set Size", ["node"]),
+        "vms": Gauge("os_mem_vms_bytes", "Virtual Memory Size", ["node"]),
+        "shared": Gauge("os_mem_shared_bytes", "Shared memory size", ["node"]),
+        "text": Gauge("os_mem_text_bytes", "Text memory size", ["node"]),
+        "lib": Gauge("os_mem_lib_bytes", "Lib memory size", ["node"]),
+        "data": Gauge("os_mem_data_bytes", "Data memory size", ["node"]),
+        "dirty": Gauge("os_mem_dirty_bytes", "Dirty memory size", ["node"]),
+    }
 
-while True:
-    print("Retrieving data for from {}".format(sock_addr))
+    start_http_server(8000)
 
-    try:
-        proxy = client.ServerProxy(
-            "http://localhost", transport=UnixStreamTransport(sock_addr)
-        )
+    while True:
+        print("Retrieving data for from {}".format(sock_addr))
 
-        all_proc_info = proxy.supervisor.getAllProcessInfo()
+        try:
+            proxy = client.ServerProxy(
+                "http://localhost", transport=UnixStreamTransport(sock_addr)
+            )
 
-        for info in all_proc_info:
-            name = info["name"]
+            all_proc_info = proxy.supervisor.getAllProcessInfo()
 
-            # Only interested in casper nodes.
-            if not name.startswith("casper-net"):
-                continue
+            for info in all_proc_info:
+                name = info["name"]
 
-            # PID 0 means the process is not running.
-            pid = info["pid"]
-            if pid == 0:
-                continue
+                # Only interested in casper nodes.
+                if not name.startswith("casper-net"):
+                    continue
 
-            try:
-                proc = psutil.Process(info["pid"])
-                mem_info = proc.memory_info()
-                print("{}: {}".format(name, mem_info))
+                # PID 0 means the process is not running.
+                pid = info["pid"]
+                if pid == 0:
+                    continue
 
-                for key in gauges.keys():
-                    gauges[key].labels(node=name).set(getattr(mem_info, key))
-            except Exception as e:
-                print("failed to get process info for {}: {}".format(name, e))
-    except Exception as e:
-        print("failed: {}".format(e))
+                try:
+                    proc = psutil.Process(info["pid"])
+                    mem_info = proc.memory_info()
+                    print("{}: {}".format(name, mem_info))
 
-    time.sleep(delay)
+                    for key in gauges.keys():
+                        gauges[key].labels(node=name).set(getattr(mem_info, key))
+                except Exception as e:
+                    print("failed to get process info for {}: {}".format(name, e))
+        except Exception as e:
+            print("failed: {}".format(e))
+
+        time.sleep(delay)
+
+
+if __name__ == "__main__":
+    main()
