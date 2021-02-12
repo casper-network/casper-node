902683886c17b47d8296300f55d87b08d64b0fc1
Marc Brinkmann <marc@casperlabs.io>
2021-02-12 17:12:36 +0100

    Working version of `prometheus.sh` start script

--- utils/nctl-metrics/prometheus.sh
+++ utils/nctl-metrics/prometheus.sh
@@ -2,6 +2,16 @@
 
 #: Run a prometheus instance that collects metrics from a local nctl network.
 
-cd $(dirname $0))
+cd $(dirname $0)
 
-docker run --net=host -p 9090:9090 -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+PROMETHEUS_TAG=docker.io/prom/prometheus
+
+echo "Genarating config."
+./gen_prometheus_config.py > prometheus.yml
+
+echo "Starting prometheus."
+podman run \
+  --net=host \
+  -p 9090:9090 \
+  -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml \
+  ${PROMETHEUS_TAG}
