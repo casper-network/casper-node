a2d6015c3cc5c2b4fcf7aa25956847f76fd4ffcb
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2021-02-15 11:47:44 -0500

    Simplified build_update_package.sh to ignore protocol version. Changed upgrade_package_s3_storage.sh to upload to drone/git_hash/protocol_version.

--- ci/build_update_package.sh
+++ ci/build_update_package.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This script will build bin.tar.gz and config.tar.gz in target/upgrade_build
+
 set -e
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
@@ -10,73 +12,39 @@ CONFIG_DIR="$UPGRADE_DIR/config"
 NODE_BUILD_DIR="$ROOT_DIR/node"
 GENESIS_FILES_DIR="$ROOT_DIR/resources/production"
 
-check_python_has_toml() {
-    set +e
-    python3 -c "import toml" 2>/dev/null
-    if [[ $? -ne 0 ]]; then
-        printf "Ensure you have 'toml' installed for Python3\n"
-        printf "e.g. run\n"
-        printf "    pip3 install toml --user\n\n"
-        exit 3
-    fi
-    set -e
-}
+echo "Building casper-node"
+cd "$NODE_BUILD_DIR"
+cargo build --release
 
-protocol_version() {
-    PROTOCOL_VERSION=$(cat "$GENESIS_FILES_DIR/chainspec.toml" | python3 -c "import sys, toml; print(toml.load(sys.stdin)['protocol']['version'].replace('.','_'))")
-    printf "Protocol version: %s\n" $PROTOCOL_VERSION
-}
+echo "Generating bin README.md"
+mkdir -p "$BIN_DIR"
+readme="$BIN_DIR/README.md"
+{
+  echo "Build for Ubuntu 18.04."
+  echo ""
+  echo "To run on other platforms, build from https://github.com/CasperLabs/casper-node"
+  echo " cd node"
+  echo " cargo build --release"
+  echo ""
+  echo "git commit hash: $(git rev-parse HEAD)"
+} > "$readme"
 
-build_casper_node() {
-    echo "Building casper-node"
-    cd "$NODE_BUILD_DIR"
-    cargo build --release
-}
+echo "Packaging bin.tar.gz"
+mkdir -p "$BIN_DIR"
+cp "$NODE_BUILD_TARGET" "$BIN_DIR"
+# To get no path in tar, need to cd in.
+cd "$BIN_DIR"
+tar -czvf "../bin.tar.gz" .
+cd ..
+rm -rf "$BIN_DIR"
 
-make_bin_readme() {
-    echo "Generating bin README.md"
-    mkdir -p "$BIN_DIR"
-    local readme="$BIN_DIR/README.md"
-    {
-      echo "Build for Ubuntu 18.04."
-      echo ""
-      echo "To run on other platforms, build from https://github.com/CasperLabs/casper-node"
-      echo " cd node"
-      echo " cargo build --release"
-      echo ""
-      echo "git commit hash: $(git rev-parse HEAD)"
-    } > "$readme"
-}
-
-package_bin_tar_gz() {
-    printf "Packaging ${PROTOCOL_VERSION}_bin.tar.gz"
-    mkdir -p "$BIN_DIR"
-    cp "$NODE_BUILD_TARGET" "$BIN_DIR"
-    # To get no path in tar, need to cd in.
-    cd "$BIN_DIR"
-    tar -czvf "../${PROTOCOL_VERSION}_bin.tar.gz" .
-    cd ..
-    rm -rf "$BIN_DIR"
-}
-
-package_config_tar_gz() {
-    printf "Packaging ${PROTOCOL_VERSION}_config.tar.gz"
-    mkdir -p "$CONFIG_DIR"
-    cp "$GENESIS_FILES_DIR/chainspec.toml" "$CONFIG_DIR"
-    cp "$GENESIS_FILES_DIR/config-example.toml" "$CONFIG_DIR"
-    cp "$GENESIS_FILES_DIR/accounts.csv" "$CONFIG_DIR"
-    # To get no path in tar, need to cd in.
-    cd "$CONFIG_DIR"
-    tar -czvf "../${PROTOCOL_VERSION}_config.tar.gz" .
-    cd ..
-    rm -rf "$CONFIG_DIR"
-}
-
-check_python_has_toml
-protocol_version
-
-build_casper_node
-make_bin_readme
-package_bin_tar_gz
-package_config_tar_gz
-
+echo "Packaging config.tar.gz"
+mkdir -p "$CONFIG_DIR"
+cp "$GENESIS_FILES_DIR/chainspec.toml" "$CONFIG_DIR"
+cp "$GENESIS_FILES_DIR/config-example.toml" "$CONFIG_DIR"
+cp "$GENESIS_FILES_DIR/accounts.csv" "$CONFIG_DIR"
+# To get no path in tar, need to cd in.
+cd "$CONFIG_DIR"
+tar -czvf "../config.tar.gz" .
+cd ..
+rm -rf "$CONFIG_DIR"
--- ci/upgrade_package_s3_storage.sh
+++ ci/upgrade_package_s3_storage.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 # This script allows uploading, downloading and purging of files to genesis.casperlabs.io s3 for storing
 # possible upgrade package releases to promote to a network or use for testing.
 
-# Using DRONE_BUILD_NUMBER as s3 bucket location
+# Using drone/GIT_HASH/PROTOCOL_VERSION as s3 bucket location in genesis.casperlabs.io
+
+# Check python has toml for getting PROTOCOL_VERSION
+set +e
+python3 -c "import toml" 2>/dev/null
+if [[ $? -ne 0 ]]; then
+  echo "Ensure you have 'toml' installed for Python3"
+  echo "e.g. run"
+  echo "    python3 -m pip install toml --user"
+  echo ""
+  exit 3
+fi
+set -e
+
+PROTOCOL_VERSION=$(cat "$GENESIS_FILES_DIR/chainspec.toml" | python3 -c "import sys, toml; print(toml.load(sys.stdin)['protocol']['version'].replace('.','_'))")
+echo "Protocol version: $PROTOCOL_VERSION"
+GIT_HASH=$(git rev-parse HEAD)
+echo "Git hash: $GIT_HASH"
 
 valid_commands=("put" "get" "del")
 ACTION=$1
@@ -32,9 +49,6 @@ if [[ "$ACTION" == "del" ]]; then
   fi
 fi
 
-CL_S3_BUCKET="genesis.casperlabs.io"
-CL_S3_LOCATION="drone"
-
 echo "CL_VAULT_TOKEN: '${CL_VAULT_TOKEN}'"
 echo "CL_VAULT_HOST: '${CL_VAULT_HOST}'"
 # get aws credentials files
@@ -46,17 +60,20 @@ export AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY=$(echo "$CREDENTIALS" | jq -r .data.cicd_agent_to_s3.aws_secret_key)
 export AWS_SECRET_ACCESS_KEY
 
+CL_S3_BUCKET="genesis.casperlabs.io"
+CL_S3_LOCATION="drone/$GIT_HASH"
+
 case "$ACTION" in
-  "put")
-    echo "sync ${LOCAL} s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${DRONE_BUILD_NUMBER}"
-    s3cmd sync "${LOCAL}" "s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${DRONE_BUILD_NUMBER}"
-    ;;
-  "get")
-    echo "sync s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${DRONE_BUILD_NUMBER} ${LOCAL}"
-    s3cmd sync "s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${DRONE_BUILD_NUMBER}" "${LOCAL}"
-    ;;
-  "del")
-    echo "del --recursive s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${DRONE_BUILD_NUMBER}"
-    s3cmd del --recursive "s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${DRONE_BUILD_NUMBER}"
-    ;;
+"put")
+  echo "sync ${LOCAL} s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${PROTOCOL_VERSION}"
+  s3cmd sync "${LOCAL}" "s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${PROTOCOL_VERSION}"
+  ;;
+"get")
+  echo "sync s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${PROTOCOL_VERSION} ${LOCAL}"
+  s3cmd sync "s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}/${PROTOCOL_VERSION}" "${LOCAL}"
+  ;;
+"del")
+  echo "del --recursive s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}"
+  s3cmd del --recursive "s3://${CL_S3_BUCKET}/${CL_S3_LOCATION}"
+  ;;
 esac
