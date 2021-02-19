ee633aedcfaf58a66b8fc0f5af8f6e9484978f5d
Fraser Hutchison <fraser@casperlabs.io>
2021-02-19 18:31:57 +0000

    Revert "EE-1170: Set era length to 4 in test"
    This reverts commit 5550e0d144c3ed4234e96057cb94e1434a55b273.

--- node/src/testing/multi_stage_test_reactor/test_chain.rs
+++ node/src/testing/multi_stage_test_reactor/test_chain.rs
@@ -108,7 +108,7 @@ impl TestChain {
         // Make the genesis timestamp 45 seconds from now, to allow for all validators to start up.
         chainspec.network_config.timestamp = Timestamp::now() + 45000.into();
 
-        chainspec.core_config.minimum_era_height = 4;
+        chainspec.core_config.minimum_era_height = 1;
         chainspec.highway_config.finality_threshold_fraction = Ratio::new(34, 100);
         chainspec.core_config.era_duration = 10.into();
 
@@ -238,6 +238,8 @@ async fn run_validator_network() {
     }
 }
 
+// TODO: fix this test
+#[ignore]
 #[tokio::test]
 async fn run_equivocator_network() {
     // Test that we won't panic if a node equivocates
