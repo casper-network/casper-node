779ee9f8927be82e140be1630503f7b0647c2d86
TomVasile <43349666+TomVasile@users.noreply.github.com>
2021-02-22 17:13:36 +0000

    addressing PR comments

--- utils/nctl/sh/scenarios/itst02.sh
+++ utils/nctl/sh/scenarios/itst02.sh
@@ -3,7 +3,6 @@
 source "$NCTL"/sh/utils/main.sh
 source "$NCTL"/sh/views/utils.sh
 source "$NCTL"/sh/node/svc_"$NCTL_DAEMON_TYPE".sh
-source "$NCTL"/sh/utils/infra.sh
 
 # Exit if any of the commands fail.
 set -e
@@ -26,30 +25,34 @@ function main() {
     do_await_genesis_era_to_complete
     # 1. Allow chain to progress
     do_await_era_change
-    # 2-4. Stop three nodes
+    # 2. Verify all nodes are in sync
+    check_network_sync
+    # 3-5. Stop three nodes
     do_stop_node "5"
     do_stop_node "4"
     do_stop_node "3"
-    # 5. Ensure chain stalled
-    sleep_and_compare "60"
-    # 6-8. Restart three nodes
+    # 6. Ensure chain stalled
+    assert_chain_stalled "60"
+    # 7-9. Restart three nodes
     do_start_node "5"
     do_start_node "4"
     do_start_node "3"
-    # 9-11. Check sync of restarted node
+    # 10-12. Check sync of restarted node
     do_await_full_synchronization "5"
     do_await_full_synchronization "4"
     do_await_full_synchronization "3"
-    # 12. Ensure era proceeds after restart
+    # 13. Ensure era proceeds after restart
     do_await_era_change "2"
-    # 13-15. Check sync of nodes again
+    # 14-16. Check sync of nodes again
     do_await_full_synchronization "5"
     do_await_full_synchronization "4"
     do_await_full_synchronization "3"
-    # 16-18. Compare stalled lfb hash to current
-    compare "5" "$STALLED_LFB"
-    compare "4" "$STALLED_LFB"
-    compare "3" "$STALLED_LFB"
+    # 17. Verify all nodes are in sync
+    check_network_sync
+    # 18-20. Compare stalled lfb hash to current
+    assert_chain_progressed "5" "$STALLED_LFB"
+    assert_chain_progressed "4" "$STALLED_LFB"
+    assert_chain_progressed "3" "$STALLED_LFB"
 
     log "------------------------------------------------------------"
     log "Scenario itst02 complete"
@@ -63,7 +66,9 @@ function log_step() {
     STEP=$((STEP + 1))
 }
 
-function compare() {
+function assert_chain_progressed() {
+    # Function accepts two hashes as arguments and checks to
+    # see if they match.
     log_step "node-${1}: checking chain progressed"
     local LFB1=$(do_read_lfb_hash ${1})
     local LFB2=${2}
@@ -74,20 +79,29 @@ function compare() {
     fi
 }
 
-function sleep_and_compare() {
+function assert_chain_stalled() {
+    # Fucntion checks that the two remaining node's LFB checked
+    # n-seconds apart doesnt progress
     log_step "ensuring chain stalled"
     local SLEEP_TIME=${1}
-    local LFB1=$(do_read_lfb_hash 1)
+    local LFB_1_PRE=$(do_read_lfb_hash 1)
+    local LFB_2_PRE=$(do_read_lfb_hash 2)
     log "Sleeping ${SLEEP_TIME}s..."
     sleep $SLEEP_TIME
-    local LFB2=$(do_read_lfb_hash 1)
+    local LFB_1_POST=$(do_read_lfb_hash 1)
+    local LFB_2_POST=$(do_read_lfb_hash 2)
 
-    if [ "$LFB1" != "$LFB2" ]; then
+    if [ "$LFB_1_PRE" != "$LFB_1_POST" ] && [ "$LFB_2_PRE" != "$LFB_2_POST" ]; then
        log "Error: Chain progressed."
        exit 1
     fi
 
-    STALLED_LFB=$LFB2
+    if [ "$LFB_1_POST" != "$LFB_2_POST" ]; then
+        log "Error: LFB mismatch on nodes"
+	exit 1
+    else
+        STALLED_LFB=$LFB_1_POST
+    fi
 }
 
 function do_await_genesis_era_to_complete() {
@@ -121,11 +135,32 @@ function do_start_node() {
     fi
 }
 
+function check_network_sync() {
+    local WAIT_TIME_SEC=0
+    log_step "check all nodes are in sync"
+    while [ "$WAIT_TIME_SEC" != "$SYNC_TIMEOUT_SEC" ]; do
+        if [ "$(do_read_lfb_hash '5')" = "$(do_read_lfb_hash '1')" ] && \
+		[ "$(do_read_lfb_hash '4')" = "$(do_read_lfb_hash '1')" ] && \
+		[ "$(do_read_lfb_hash '3')" = "$(do_read_lfb_hash '1')" ] && \
+		[ "$(do_read_lfb_hash '2')" = "$(do_read_lfb_hash '1')" ]; then
+	    log "all nodes in sync, proceeding..."
+	    break
+        fi
+
+        if [ "$WAIT_TIME_SEC" = "$SYNC_TIMEOUT_SEC" ]; then
+            log "ERROR: Failed to confirm network sync"
+            exit 1
+        fi
+        WAIT_TIME_SEC=$((WAIT_TIME_SEC + 1))
+        sleep 1
+    done
+}
+
 function do_await_full_synchronization() {
     local NODE_ID=${1}
     local WAIT_TIME_SEC=0
     log_step "awaiting full synchronization of node=${NODE_ID}…"
-    while [ "$(do_read_lfb_hash "$NODE_ID")" != "$(do_read_lfb_hash "1")" ]; do
+    while [ "$(do_read_lfb_hash ${NODE_ID})" != "$(do_read_lfb_hash '1')" ]; do
         if [ "$WAIT_TIME_SEC" = "$SYNC_TIMEOUT_SEC" ]; then
             log "ERROR: Failed to synchronize node-${NODE_ID} in ${SYNC_TIMEOUT_SEC} seconds"
             exit 1
