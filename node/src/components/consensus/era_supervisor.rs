d66daa3c9901b882a3165147b6194265c512958c
Andreas Fackler <andreas@casperlabs.io>
2021-02-24 12:22:47 +0100

    Add a delay before shutdown due to too many equivocations.
    This allows us to still broadcast the evidence of the equivocation.

--- node/src/components/consensus/era_supervisor.rs
+++ node/src/components/consensus/era_supervisor.rs
@@ -22,6 +22,7 @@ use blake2::{
     VarBlake2b,
 };
 use datasize::DataSize;
+use futures::FutureExt;
 use itertools::Itertools;
 use prometheus::Registry;
 use rand::Rng;
@@ -54,6 +55,10 @@ use crate::{
 };
 
 pub use self::era::{Era, EraId};
+
+/// The delay in milliseconds before we shutdown after the number of faulty validators exceeded the
+/// fault tolerance threshold.
+const FTT_EXCEEDED_SHUTDOWN_DELAY_MILLIS: u64 = 60 * 1000;
 
 type ConsensusConstructor<I> = dyn Fn(
     Digest,                                       // the era's unique instance ID
@@ -1019,6 +1024,12 @@ where
                 .collect(),
             ProtocolOutcome::WeAreFaulty => Default::default(),
             ProtocolOutcome::DoppelgangerDetected => Default::default(),
+            // TODO: Replace the panic with the appropriate shutdown effect once that's possible.
+            ProtocolOutcome::FttExceeded => self
+                .effect_builder
+                .set_timeout(Duration::from_millis(FTT_EXCEEDED_SHUTDOWN_DELAY_MILLIS))
+                .then(|_| async move { panic!("shutting down: too many faulty validators") })
+                .ignore(),
         }
     }
 
@@ -1032,7 +1043,8 @@ where
         if should_emit_error {
             fatal!(
                 self.effect_builder,
-                "Consensus shutting down due to inability to participate in the network; inactive era = {}",
+                "Consensus shutting down due to inability to participate in the network; \
+                inactive era = {}",
                 self.era_supervisor.current_era
             )
         } else {
