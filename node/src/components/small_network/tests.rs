089be4e56612072d13a30e0560b61892b340c529
Fraser Hutchison <fraser@casperlabs.io>
2021-02-25 00:00:39 +0000

    Revert "NDRS-865: Make handshake depend on network name."
    This reverts commit a2e897b33deeccf8c484fba079edc4bc535ea82d.

--- node/src/components/small_network/tests.rs
+++ node/src/components/small_network/tests.rs
@@ -119,7 +119,7 @@ impl Reactor for TestReactor {
             cfg,
             registry,
             small_network_identity,
-            "test_network".to_string(),
+            Digest::default(),
             false,
         )?;
         let gossiper_config = gossiper::Config::new_with_small_timeouts();
