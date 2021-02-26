318d5887ce1203f2d947ef2d163cb05984f43dbf
TomVasile <43349666+TomVasile@users.noreply.github.com>
2021-02-26 16:35:21 +0000

    syncing both files with master

--- ci/build_update_package.sh
+++ ci/build_update_package.sh
@@ -5,12 +5,13 @@
 set -e
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
+GENESIS_FILES_DIR="$ROOT_DIR/resources/production"
 NODE_BUILD_TARGET="$ROOT_DIR/target/release/casper-node"
-UPGRADE_DIR="$ROOT_DIR/target/upgrade_build"
+PROTOCOL_VERSION=$(cat "$GENESIS_FILES_DIR/chainspec.toml" | python3 -c "import sys, toml; print(toml.load(sys.stdin)['protocol']['version'].replace('.','_'))")
+UPGRADE_DIR="$ROOT_DIR/target/upgrade_build/$PROTOCOL_VERSION"
 BIN_DIR="$UPGRADE_DIR/bin"
 CONFIG_DIR="$UPGRADE_DIR/config"
 NODE_BUILD_DIR="$ROOT_DIR/node"
-GENESIS_FILES_DIR="$ROOT_DIR/resources/production"
 
 echo "Building casper-node"
 cd "$NODE_BUILD_DIR"
@@ -42,7 +43,7 @@ echo "Packaging config.tar.gz"
 mkdir -p "$CONFIG_DIR"
 cp "$GENESIS_FILES_DIR/chainspec.toml" "$CONFIG_DIR"
 cp "$GENESIS_FILES_DIR/config-example.toml" "$CONFIG_DIR"
-cp "$GENESIS_FILES_DIR/accounts.csv" "$CONFIG_DIR"
+cp "$GENESIS_FILES_DIR/accounts.toml" "$CONFIG_DIR"
 # To get no path in tar, need to cd in.
 cd "$CONFIG_DIR"
 tar -czvf "../config.tar.gz" .
