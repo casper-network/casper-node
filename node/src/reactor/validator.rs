6c7110b8a35ef82435b8ca57b01e37f34e0d318b
Mateusz Górski <gorski.mateusz@protonmail.ch>
2021-04-07 12:44:51 +0200

    NDRS-833: Remove unused `activation_era_id`.

--- node/src/reactor/validator.rs
+++ node/src/reactor/validator.rs
@@ -491,11 +491,6 @@ impl reactor::Reactor for Reactor {
             *protocol_version,
             chainspec_loader.chainspec().core_config.auction_delay,
             chainspec_loader.chainspec().core_config.unbonding_delay,
-            chainspec_loader
-                .chainspec()
-                .protocol_config
-                .activation_point
-                .era_id(),
         )?;
 
         effects.extend(reactor::wrap_effects(Event::Network, network_effects));
