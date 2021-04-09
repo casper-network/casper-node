5c17229d98b44041a6f6d94be9e0c09a8c8b7947
Marc Brinkmann <marc@casperlabs.io>
2021-04-09 15:21:36 +0200

    Improve the `e!` macro by using a custom formatter

--- node/src/logging.rs
+++ node/src/logging.rs
@@ -1,6 +1,10 @@
 //! Logging via the tracing crate.
 
-use std::{env, fmt, io};
+use std::{
+    env, error,
+    fmt::{self, Display, Formatter},
+    io,
+};
 
 use ansi_term::{Color, Style};
 use anyhow::anyhow;
@@ -295,25 +299,94 @@ pub fn init_with_config(config: &LoggingConfig) -> anyhow::Result<()> {
     .map_err(|error| anyhow!(error))
 }
 
+/// An error formatter.
+///
+/// Stores a pointer to a trait object internally instead of using a generic `T` to avoid an
+/// explosion of monomorphized instances.
+#[derive(Clone, Copy, Debug)]
+pub struct ErrFormatter<'a>(pub &'a dyn std::error::Error);
+
+impl<'a> Display for ErrFormatter<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut opt_source: Option<&(dyn error::Error)> = Some(self.0);
+
+        while let Some(source) = opt_source {
+            write!(f, "{}", source)?;
+            opt_source = source.source();
+
+            if opt_source.is_some() {
+                f.write_str(": ")?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 /// Wraps an error to ensure it gets properly captured by tracing.
 ///
-/// Ensures that errors are properly captured as errors, not strings, by performing the necessary
-/// cast to a trait object. As a result, the `cause` of errors is shown.
+/// Ensures that errors are properly captured.
 ///
-/// This macro should be removed once/if the tracing issue
-/// https://github.com/tokio-rs/tracing/issues/1308 has been resolved, which adds a special syntax
-/// for this case.
-///
-/// # Known issues
-///
-/// There is a known bugs in tracing where only the first source in a chain of errors is logged, see
-/// https://github.com/tokio-rs/tracing/issues/1308 for details. If this becomes an issue, this
-/// macro can be replaced with a custom `Fn(&std::error::Error) -> &str` and
-/// `($orig_err:expr) => { custom_fn($orig_err) };` to show the whole chain. At the time of this
-/// writing, most of our errors are pretty shallow though.
+/// This macro should be removed once/if the tracing
+/// issue https://github.com/tokio-rs/tracing/issues/1308 has been resolved, which adds a special
+/// syntax for this case and the known issue https://github.com/tokio-rs/tracing/issues/1308 has
+/// been fixed, which cuts traces short after the first cause.
 #[macro_export]
 macro_rules! e {
     ($orig_err:expr) => {
-        $orig_err.borrow() as &dyn std::error::Error
+        ::tracing::field::display($crate::logging::ErrFormatter(
+            ::std::borrow::Borrow::borrow(&$orig_err),
+        ))
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use thiserror::Error;
+    use tracing::Value;
+
+    use super::ErrFormatter;
+    use crate::e;
+
+    #[derive(Debug, Error)]
+    #[error("this is baz")]
+    struct Baz;
+
+    #[derive(Debug, Error)]
+    #[error("this is bar")]
+    struct Bar(#[source] Baz);
+
+    #[derive(Debug, Error)]
+    enum MyError {
+        #[error("this is foo")]
+        Foo {
+            #[source]
+            bar: Bar,
+        },
+    }
+
+    #[test]
+    fn test_formatter_formats_single() {
+        let single = Baz;
+
+        assert_eq!(ErrFormatter(&single).to_string().as_str(), "this is baz");
+    }
+
+    #[test]
+    fn test_formatter_formats_nested() {
+        let nested = MyError::Foo { bar: Bar(Baz) };
+
+        assert_eq!(
+            ErrFormatter(&nested).to_string().as_str(),
+            "this is foo: this is bar: this is baz"
+        );
+    }
+
+    #[test]
+    #[allow(trivial_casts)]
+    fn test_e_produces_value() {
+        let err = Baz;
+        let wrapped = e!(err);
+        let _value_ref: &dyn Value = &wrapped as &dyn Value;
+    }
 }
