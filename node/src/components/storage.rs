cd31d2ce088473d923d743b3e9e65c8e5a354c10
Austin Buddington <austin.j.buddington@gmail.com>
2021-06-04 13:10:33 -0400

    Update node/src/components/storage.rs
    Co-authored-by: Fraser Hutchison <190532+Fraser999@users.noreply.github.com>

--- node/src/components/storage.rs
+++ node/src/components/storage.rs
@@ -167,14 +167,14 @@ pub enum Error {
     #[error("internal database error: {0}")]
     InternalStorage(#[from] LmdbExtError),
 
-    /// filesystem error while trying to move file.
+    /// Filesystem error while trying to move file.
     #[error("unable to move file {source_path} to {dest_path}: {original_error}")]
     UnableToMoveFile {
-        /// the path to the file that should have been moved.
+        /// The path to the file that should have been moved.
         source_path: PathBuf,
-        /// the path where the file should have been moved to.
+        /// The path where the file should have been moved to.
         dest_path: PathBuf,
-        /// the original io::Error from fs::rename.
+        /// The original `io::Error` from `fs::rename`.
         original_error: io::Error,
     },
 }
