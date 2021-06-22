82dbc04c1d0336c75cf03557362f672d2f937a10
nilz3ro <austin.j.buddington@gmail.com>
2021-06-22 08:01:52 -0400

    improve timeouts tests.

--- node/src/components/gossiper/gossip_table.rs
+++ node/src/components/gossiper/gossip_table.rs
@@ -990,12 +990,19 @@ mod tests {
     }
 
     #[test]
-    fn timeouts_may_under_drain_if_system_time_changed() {
+    fn timeouts_depends_on_binary_search_by_implementation() {
+        // This test is meant to document the dependency of
+        // Timeouts::purge on https://doc.rust-lang.org/std/vec/struct.Vec.html#method.binary_search_by.
+        // If this test is failing then it's reasonable to believe that the implementation of
+        // binary_search_by has been updated.
         let mut timeouts = Timeouts::new();
         let now = Instant::now();
         let later_100 = now + Duration::from_millis(100);
         let later_200 = now + Duration::from_millis(200);
         let later_300 = now + Duration::from_millis(300);
+        let later_400 = now + Duration::from_millis(400);
+        let later_500 = now + Duration::from_millis(500);
+        let later_600 = now + Duration::from_millis(600);
 
         timeouts.push(later_100, 1);
         timeouts.push(later_200, 2);
@@ -1005,50 +1012,48 @@ mod tests {
         // the earliest timeout, and a new timeout is added with an instant
         // corresponding to this new early time, then this would make the earliest
         // timeout the LAST timeout in the vec.
+        // [100 < 200 < 300 > 0]
         timeouts.push(now, 0);
 
         let now_after_time_travel = now + Duration::from_millis(10);
+        // Intuitively, we would expect [1,2,3,0] to be in the "purged" vec here.
+        // This is not the case because we're using binary_search_by, which (currently)
+        // is implemented with logic that checks if a, b, ... z are in a consistent order.
+        // in this case, the order that we've established is a < b < ... < z for each element in the
+        // vec, but we broke that order by inserting '0' last, and for some reason,
+        // binary_search_by won't find this unless there is a number > n occurring AFTER n
+        // in the vec.
+
         let purged = timeouts.purge(&now_after_time_travel).collect::<Vec<i32>>();
         let empty: Vec<i32> = vec![];
 
         // This isn't a problem and the order will eventually
         // be restored.
         assert_eq!(purged, empty);
-    }
 
-    #[test]
-    fn timeouts_may_over_drain_if_system_time_changed() {
-        let mut timeouts = Timeouts::new();
-        let now = Instant::now();
-        let later_100 = now + Duration::from_millis(100);
-        let later_200 = now + Duration::from_millis(200);
-        let later_300 = now + Duration::from_millis(300);
-        let later_400 = now + Duration::from_millis(400);
-        let later_500 = now + Duration::from_millis(500);
-
-        timeouts.push(later_100, 1);
-        timeouts.push(later_200, 2);
-        timeouts.push(later_300, 3);
-
-        // If a node's system time was changed to a time earlier than
-        // the earliest timeout, and a new timeout is added with an instant
-        // corresponding to this new "early" time, then this would make the earliest
-        // timeout the LAST timeout in the vec.
-        timeouts.push(now, 0);
-
-        // And then more timeouts are added, in chronological order, starting
-        // with an instant later than the "early" time referenced above
         timeouts.push(later_400, 4);
         timeouts.push(later_500, 5);
+        timeouts.push(later_600, 6);
 
-        let now_after_time_travel = now + Duration::from_millis(10);
+        // Now, we advance time another 10 ms and purge again.
+        // In this scenario, timeouts with a later time are added after our
+        // improperly ordered "now" timeout
+        // [100 < 200 < 300 > 0 < 400 < 500 < 600]
+        let now_after_time_travel = now + Duration::from_millis(20);
         let purged = timeouts.purge(&now_after_time_travel).collect::<Vec<i32>>();
+        let expected = [1, 2, 3, 0];
 
-        // Then the cache vec may be over-drained.
-        // Depending on how out-of-order the vec is,
-        // this could happen again and again, but eventually
-        // the order would be restored.
-        assert_eq!(purged, vec![1, 2, 3, 0]);
+        assert_eq!(purged, expected);
+
+        // After the previous purge, an order is restored where a < b for consecutive elements in
+        // the vec. [400 < 500 < 600], so, purging timeouts up to 610 will properly clear
+        // the vec.
+        let now_after_time_travel = now + Duration::from_millis(610);
+        let purged = timeouts.purge(&now_after_time_travel).collect::<Vec<i32>>();
+        let expected = [4, 5, 6];
+
+        assert_eq!(purged, expected);
+        assert_eq!(0, timeouts.values.len());
     }
 
     #[bench]
