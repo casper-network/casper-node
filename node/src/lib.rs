aa7a9fb1b1d47b83e1a83c18a9e7e8bf18f96331
nilz3ro <austin.j.buddington@gmail.com>
2021-07-22 16:44:32 -0400

    parameterize sync timeout.

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -59,6 +59,7 @@ pub use components::{
     event_stream_server::Config as EventStreamServerConfig,
     fetcher::Config as FetcherConfig,
     gossiper::{Config as GossipConfig, Error as GossipError},
+    linear_chain_sync::Config as LinearChainSyncConfig,
     rest_server::Config as RestServerConfig,
     rpc_server::{rpcs, Config as RpcServerConfig},
     small_network::{Config as SmallNetworkConfig, Error as SmallNetworkError},
