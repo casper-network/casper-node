e125bfadf4c336a1fb24fec6aa25dfbb6895be35
Mateusz Górski <goral09@users.noreply.github.com>
2021-07-23 15:34:12 +0200

    Update node/src/components/chainspec_loader.rs
    Co-authored-by: Andreas Fackler <afck@users.noreply.github.com>

--- node/src/components/chainspec_loader.rs
+++ node/src/components/chainspec_loader.rs
@@ -305,7 +305,7 @@ impl ChainspecLoader {
     }
 
     /// Returns whether the current node instance is started immediately after an upgrade –
-    /// i.e. whether the last/highest block stored is a block that triggerred the upgrade.
+    /// i.e. whether the last/highest block stored is a block that triggered the upgrade.
     pub(crate) fn after_upgrade(&self) -> bool {
         self.after_upgrade
     }
