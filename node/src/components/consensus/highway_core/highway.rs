4d7dbea20bf36e7a2e68662cf554cb250fe3ddc2
Andreas Fackler <andreas@casperlabs.io>
2021-07-23 11:36:32 +0200

    Add own initial ping message immediately; don't interpret it as a doppelgänger's.

--- node/src/components/consensus/highway_core/highway.rs
+++ node/src/components/consensus/highway_core/highway.rs
@@ -219,7 +219,7 @@ impl<C: Context> Highway<C> {
             self.instance_id,
         );
         self.active_validator = Some(av);
-        effects
+        self.add_new_own_vertices(effects, current_time)
     }
 
     /// Turns this instance into a passive observer, that does not create any new vertices.
@@ -500,7 +500,19 @@ impl<C: Context> Highway<C> {
         F: FnOnce(&mut ActiveValidator<C>, &State<C>) -> Vec<Effect<C>>,
     {
         let effects = f(self.active_validator.as_mut()?, &self.state);
-        let mut result = vec![];
+        Some(self.add_new_own_vertices(effects, timestamp))
+    }
+
+    /// Handles all `NewVertex` effects and adds the vertices to the protocol state.
+    ///
+    /// This needs to be applied to all effects created by `ActiveValidator`, so that new vertices
+    /// are not interpreted as coming from a doppelgänger.
+    fn add_new_own_vertices(
+        &mut self,
+        effects: Vec<Effect<C>>,
+        timestamp: Timestamp,
+    ) -> Vec<Effect<C>> {
+        let mut result = Vec::with_capacity(effects.len());
         for effect in &effects {
             match effect {
                 Effect::NewVertex(vv) => {
@@ -511,7 +523,7 @@ impl<C: Context> Highway<C> {
             }
         }
         result.extend(effects);
-        Some(result)
+        result
     }
 
     /// Performs initial validation and returns an error if `vertex` is invalid. (See
