f658fd9ef7b7f60a9eefa72c33063cc42874e8d8
Austin Buddington <austin@casperlabs.io>
2021-08-19 19:16:25 +0000

    fix comments.

--- types/src/gas.rs
+++ types/src/gas.rs
@@ -29,12 +29,14 @@ impl Gas {
             .map(Self::new)
     }
 
-    /// Safe addition method that returns an `Option<Gas>`. Returns `None` if an overflow has occurred.
+    /// Safe addition method that returns an `Option<Gas>`. Returns `None` if an overflow has
+    /// occurred.
     pub fn checked_add(&self, rhs: Self) -> Option<Self> {
         self.0.checked_add(rhs.value()).map(Self::new)
     }
 
-    /// Safe subtraction method that returns an `Option<Gas>`. Returns `None` if an overflowhas occurred.
+    /// Safe subtraction method that returns an `Option<Gas>`. Returns `None` if an overflow has
+    /// occurred.
     pub fn checked_sub(&self, rhs: Self) -> Option<Self> {
         self.0.checked_sub(rhs.value()).map(Self::new)
     }
