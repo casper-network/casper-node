b3ba64140ed6e451ad47636d4b21325c2001f918
Andreas Fackler <andreas@casperlabs.io>
2021-08-23 17:35:21 +0200

    Make run_equivocator_network test more reliable.
    This is still flaky, possibly because sometimes the nodes only start
    a bit after the genesis timestamp. With this change, we delay messages
    for three rounds after the first message has been sent instead.

--- node/src/components/consensus.rs
+++ node/src/components/consensus.rs
@@ -49,8 +49,6 @@ use crate::{
 
 pub(crate) use cl_context::ClContext;
 pub(crate) use config::Config;
-#[cfg(test)]
-pub(crate) use config::ProtocolConfig;
 pub(crate) use consensus_protocol::{BlockContext, EraReport, ProposedBlock};
 pub(crate) use era_supervisor::EraSupervisor;
 pub(crate) use protocols::highway::HighwayProtocol;
