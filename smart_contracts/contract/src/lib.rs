04b5202a7a8c05c632b533bd2c9ea86c9c6030ea
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2021-09-13 10:45:13 -0400

    Version bump to 1.3.3

--- smart_contracts/contract/src/lib.rs
+++ smart_contracts/contract/src/lib.rs
@@ -53,7 +53,7 @@
     not(feature = "std"),
     feature(alloc_error_handler, core_intrinsics, lang_items)
 )]
-#![doc(html_root_url = "https://docs.rs/casper-contract/1.3.2")]
+#![doc(html_root_url = "https://docs.rs/casper-contract/1.3.3")]
 #![doc(
     html_favicon_url = "https://raw.githubusercontent.com/casper-network/casper-node/master/images/CasperLabs_Logo_Favicon_RGB_50px.png",
     html_logo_url = "https://raw.githubusercontent.com/casper-network/casper-node/master/images/CasperLabs_Logo_Symbol_RGB.png",
