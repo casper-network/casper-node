6b15c7eb6a86624a138efdca7657e1d7593bc6ab
Karan Dhareshwar <42871449+darthsiroftardis@users.noreply.github.com>
2021-11-08 10:56:27 -0600

    Update execution_engine/src/core/engine_state/mod.rs
    Co-authored-by: Fraser Hutchison <190532+Fraser999@users.noreply.github.com>

--- execution_engine/src/core/engine_state/mod.rs
+++ execution_engine/src/core/engine_state/mod.rs
@@ -1961,7 +1961,7 @@ where
         let gas_limit = Gas::new(U512::from(std::u64::MAX));
         let deploy_hash = {
             // seeds address generator w/ era_end_timestamp_millis
-            let bytes: Vec<u8> = step_request.era_end_timestamp_millis.into_bytes()?.to_vec();
+            let bytes = step_request.era_end_timestamp_millis.into_bytes()?;
             DeployHash::new(Digest::hash(&bytes).value())
         };
 
