978e2ac5b0156f80233b5ce2473577e4c31bca88
Austin Buddington <austin.j.buddington@gmail.com>
2021-12-27 13:07:31 -0500

    remove unused code.

--- execution_engine/src/core/runtime/auction_internal.rs
+++ execution_engine/src/core/runtime/auction_internal.rs
@@ -241,26 +241,6 @@ where
                     .map_err(|_| Error::Storage)?,
             )
             .map_err(|_| Error::RuntimeStackOverflow)?;
-        let mint_contract_hash = self.get_mint_contract().map_err(|exec_error| {
-            <Option<Error>>::from(exec_error).unwrap_or(Error::MissingValue)
-        })?;
-
-        let mint_contract_key: Key = mint_contract_hash.into();
-
-        let mint = match self
-            .context
-            .read_gs(&mint_contract_key)
-            .map_err(|exec_error| {
-                <Option<Error>>::from(exec_error).unwrap_or(Error::MissingValue)
-            })? {
-            Some(StoredValue::Contract(contract)) => contract,
-            Some(_) => {
-                return Err(Error::MissingValue);
-            }
-            None => return Err(Error::MissingKey),
-        };
-
-        let mut mint_named_keys = mint.named_keys().clone();
 
         let cl_value = self
             .call_host_mint(
