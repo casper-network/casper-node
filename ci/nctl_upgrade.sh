e431321d4821c66bbe04315f45700fb0e19b7c46
Fraser Hutchison <fraser@casperlabs.io>
2021-12-31 16:34:15 +0000

    update CI test scripts to clone the client repo if required

--- ci/nctl_upgrade.sh
+++ ci/nctl_upgrade.sh
@@ -2,17 +2,18 @@
 set -e
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
-LAUNCHER_DIR="$ROOT_DIR/../"
-
-# NCTL compile requires casper-node-launcher
-if [ ! -d "$LAUNCHER_DIR/casper-node-launcher" ]; then
-    pushd $LAUNCHER_DIR
-    git clone https://github.com/CasperLabs/casper-node-launcher.git
-fi
 
 # Activate Environment
 pushd "$ROOT_DIR"
 source $(pwd)/utils/nctl/activate
+
+# Clone the client and launcher repos if required.
+if [ ! -d "$NCTL_CASPER_CLIENT_HOME" ]; then
+    git clone https://github.com/casper-ecosystem/casper-client-rs "$NCTL_CASPER_CLIENT_HOME"
+fi
+if [ ! -d "$NCTL_CASPER_NODE_LAUNCHER_HOME" ]; then
+    git clone https://github.com/casper-network/casper-node-launcher "$NCTL_CASPER_NODE_LAUNCHER_HOME"
+fi
 
 # NCTL Build
 nctl-compile
