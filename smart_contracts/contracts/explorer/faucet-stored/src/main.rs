5124b34db5a553b51cc415bca080cd8cbb5e09c1
Austin Buddington <austin.j.buddington@gmail.com>
2022-02-02 11:54:32 -0500

    make params optional; update readme.

--- smart_contracts/contracts/explorer/faucet-stored/src/main.rs
+++ smart_contracts/contracts/explorer/faucet-stored/src/main.rs
@@ -6,13 +6,12 @@ extern crate alloc;
 use alloc::{boxed::Box, format, string::ToString, vec};
 
 use casper_contract::{
-    contract_api::{account, runtime, storage, system::transfer_from_purse_to_purse},
+    contract_api::{account, runtime, storage, system},
     unwrap_or_revert::UnwrapOrRevert,
 };
 use casper_types::{
-    contracts::{ContractHash, NamedKeys},
-    ApiError, CLType, ContractVersion, EntryPoint, EntryPointAccess, EntryPointType, EntryPoints,
-    Parameter, RuntimeArgs, URef,
+    contracts::NamedKeys, ApiError, CLType, EntryPoint, EntryPointAccess, EntryPointType,
+    EntryPoints, Parameter, URef, U512,
 };
 
 #[repr(u16)]
@@ -25,7 +24,41 @@ pub extern "C" fn call_faucet() {
     faucet::delegate();
 }
 
-fn store() -> (ContractHash, ContractVersion) {
+fn build_named_keys_and_purse() -> (NamedKeys, URef) {
+    let mut named_keys = NamedKeys::new();
+    let purse = system::create_purse();
+    named_keys.insert(faucet::FAUCET_PURSE.to_string(), purse.into());
+
+    named_keys.insert(faucet::INSTALLER.to_string(), runtime::get_caller().into());
+    named_keys.insert(
+        faucet::TIME_INTERVAL.to_string(),
+        storage::new_uref(faucet::TWO_HOURS_AS_MILLIS).into(),
+    );
+    named_keys.insert(
+        faucet::LAST_DISTRIBUTION_TIME.to_string(),
+        storage::new_uref(0u64).into(),
+    );
+    named_keys.insert(
+        faucet::AVAILABLE_AMOUNT.to_string(),
+        storage::new_uref(U512::zero()).into(),
+    );
+    named_keys.insert(
+        faucet::REMAINING_AMOUNT.to_string(),
+        storage::new_uref(U512::zero()).into(),
+    );
+    named_keys.insert(
+        faucet::DISTRIBUTIONS_PER_INTERVAL.to_string(),
+        storage::new_uref(0u64).into(),
+    );
+
+    (named_keys, purse)
+}
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let id: u64 = runtime::get_named_arg(faucet::ARG_ID);
+    let (faucet_named_keys, faucet_purse) = build_named_keys_and_purse();
+
     let entry_points = {
         let mut entry_points = EntryPoints::new();
 
@@ -40,20 +73,21 @@ fn store() -> (ContractHash, ContractVersion) {
             EntryPointType::Contract,
         );
 
-        let init_purse = EntryPoint::new(
-            faucet::ENTRY_POINT_INIT,
-            vec![],
-            CLType::URef,
-            EntryPointAccess::Public,
-            EntryPointType::Contract,
-        );
-
         let set_variables = EntryPoint::new(
             faucet::ENTRY_POINT_SET_VARIABLES,
             vec![
-                Parameter::new(faucet::ARG_AVAILABLE_AMOUNT, CLType::U512),
-                Parameter::new(faucet::ARG_TIME_INTERVAL, CLType::U64),
-                Parameter::new(faucet::ARG_DISTRIBUTIONS_PER_INTERVAL, CLType::U64),
+                Parameter::new(
+                    faucet::ARG_AVAILABLE_AMOUNT,
+                    CLType::Option(Box::new(CLType::U512)),
+                ),
+                Parameter::new(
+                    faucet::ARG_TIME_INTERVAL,
+                    CLType::Option(Box::new(CLType::U64)),
+                ),
+                Parameter::new(
+                    faucet::ARG_DISTRIBUTIONS_PER_INTERVAL,
+                    CLType::Option(Box::new(CLType::U64)),
+                ),
             ],
             CLType::Unit,
             EntryPointAccess::Public,
@@ -61,49 +95,30 @@ fn store() -> (ContractHash, ContractVersion) {
         );
 
         entry_points.add_entry_point(faucet);
-        entry_points.add_entry_point(init_purse);
         entry_points.add_entry_point(set_variables);
 
         entry_points
     };
 
-    let named_keys = {
-        let mut named_keys = NamedKeys::new();
-        named_keys.insert(faucet::INSTALLER.to_string(), runtime::get_caller().into());
-        named_keys
-    };
-
-    storage::new_contract(
+    let (contract_hash, contract_version) = storage::new_contract(
         entry_points,
-        Some(named_keys),
+        Some(faucet_named_keys),
         Some(faucet::HASH_KEY_NAME.to_string()),
         Some(faucet::ACCESS_KEY_NAME.to_string()),
-    )
-}
+    );
 
-#[no_mangle]
-pub extern "C" fn call() {
-    let id: u64 = runtime::get_named_arg(faucet::ARG_ID);
-
-    let (contract_hash, contract_version) = store();
     runtime::put_key(
         faucet::CONTRACT_VERSION,
         storage::new_uref(contract_version).into(),
     );
     runtime::put_key(faucet::CONTRACT_NAME, contract_hash.into());
+    runtime::put_key(&format!("faucet_{}", id), faucet_purse.into());
 
-    // init the newly installed contract,
-    // keep track of the purse it returns so that it can be easily topped off later
-    // then fund that purse so that the faucet has some token to distribute
-    let purse: URef = runtime::call_contract(
-        contract_hash,
-        faucet::ENTRY_POINT_INIT,
-        RuntimeArgs::default(),
-    );
-    runtime::put_key(&format!("faucet_{}", id), purse.into());
     let main_purse = account::get_main_purse();
     let amount = runtime::get_named_arg(faucet::ARG_AMOUNT);
-    transfer_from_purse_to_purse(main_purse, purse, amount, Some(id)).unwrap_or_revert_with(
-        ApiError::User(InstallerSessionError::FailedToTransfer as u16),
-    );
+
+    system::transfer_from_purse_to_purse(main_purse, faucet_purse, amount, Some(id))
+        .unwrap_or_revert_with(ApiError::User(
+            InstallerSessionError::FailedToTransfer as u16,
+        ));
 }
