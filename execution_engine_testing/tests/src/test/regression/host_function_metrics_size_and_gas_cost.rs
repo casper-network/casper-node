d6f085e1b8af6b538482394d18d9fbaa428b151a
Michał Papierski <michal@casperlabs.io>
2022-02-03 18:13:48 +0100

    Minor consistency improvements

--- execution_engine_testing/tests/src/test/regression/host_function_metrics_size_and_gas_cost.rs
+++ execution_engine_testing/tests/src/test/regression/host_function_metrics_size_and_gas_cost.rs
@@ -1,8 +1,13 @@
+use std::convert::TryInto;
+
 use casper_engine_test_support::{
     utils, DeployItemBuilder, ExecuteRequestBuilder, InMemoryWasmTestBuilder, DEFAULT_ACCOUNT_ADDR,
-    DEFAULT_PAYMENT, DEFAULT_RUN_GENESIS_REQUEST,
+    DEFAULT_PAYMENT, DEFAULT_RUN_GENESIS_REQUEST, MINIMUM_ACCOUNT_CREATION_BALANCE,
 };
-use casper_execution_engine::core::engine_state::ExecuteRequest;
+use casper_execution_engine::core::{
+    engine_state::{self, ExecuteRequest},
+    execution,
+};
 use casper_types::{
     account::{AccountHash, ACCOUNT_HASH_LENGTH},
     bytesrepr::Bytes,
@@ -10,21 +15,37 @@ use casper_types::{
     system::standard_payment,
     ApiError, RuntimeArgs, U512,
 };
-use rand::prelude::{Rng, SeedableRng, StdRng};
 
 const CONTRACT_HOST_FUNCTION_METRICS: &str = "host_function_metrics.wasm";
 const CONTRACT_TRANSFER_TO_ACCOUNT_U512: &str = "transfer_to_account_u512.wasm";
 
 const HOST_FUNCTION_METRICS_STANDARD_SIZE: usize = 90_620;
-const HOST_FUNCTION_METRICS_STANDARD_GAS_COST: u64 = 141_111_502_050;
+const HOST_FUNCTION_METRICS_STANDARD_GAS_COST: u64 = 144_018_843_520;
 
-const HOST_FUNCTION_METRICS_MIN_SIZE: usize = HOST_FUNCTION_METRICS_STANDARD_SIZE * 95 / 100;
-const HOST_FUNCTION_METRICS_MAX_SIZE: usize = HOST_FUNCTION_METRICS_STANDARD_SIZE * 105 / 100;
-const HOST_FUNCTION_METRICS_MIN_GAS_COST: u64 = HOST_FUNCTION_METRICS_STANDARD_GAS_COST * 95 / 100;
-const HOST_FUNCTION_METRICS_MAX_GAS_COST: u64 = HOST_FUNCTION_METRICS_STANDARD_GAS_COST * 105 / 100;
+/// Acceptable size regression/improvement in percentage.
+const SIZE_MARGIN: usize = 5;
+/// Acceptable gas cost regression/improvement in percentage.
+const GAS_COST_MARGIN: u64 = 5;
+
+const HOST_FUNCTION_METRICS_MIN_SIZE: usize =
+    HOST_FUNCTION_METRICS_STANDARD_SIZE * (100 - SIZE_MARGIN) / 100;
+const HOST_FUNCTION_METRICS_MAX_SIZE: usize =
+    HOST_FUNCTION_METRICS_STANDARD_SIZE * (100 + SIZE_MARGIN) / 100;
+const HOST_FUNCTION_METRICS_MIN_GAS_COST: u64 =
+    HOST_FUNCTION_METRICS_STANDARD_GAS_COST * (100 - GAS_COST_MARGIN) / 100;
+const HOST_FUNCTION_METRICS_MAX_GAS_COST: u64 =
+    HOST_FUNCTION_METRICS_STANDARD_GAS_COST * (100 + GAS_COST_MARGIN) / 100;
 
 const ACCOUNT0_ADDR: AccountHash = AccountHash::new([42; ACCOUNT_HASH_LENGTH]);
 const ACCOUNT1_ADDR: AccountHash = AccountHash::new([43; ACCOUNT_HASH_LENGTH]);
+
+const ARG_TARGET: &str = "target";
+const ARG_AMOUNT: &str = "amount";
+
+const ARG_SEED: &str = "seed";
+const ARG_OTHERS: &str = "others";
+const EXPECTED_REVERT_VALUE: u16 = 9;
+const SEED_VALUE: u64 = 821_577_831_833_715_345;
 
 #[ignore]
 #[test]
@@ -48,8 +69,8 @@ fn create_account_exec_request(address: AccountHash) -> ExecuteRequest {
         *DEFAULT_ACCOUNT_ADDR,
         CONTRACT_TRANSFER_TO_ACCOUNT_U512,
         runtime_args! {
-            "target" => address,
-            "amount" => *DEFAULT_PAYMENT,
+            ARG_TARGET => address,
+            ARG_AMOUNT => U512::from(MINIMUM_ACCOUNT_CREATION_BALANCE),
         },
     )
     .build()
@@ -58,44 +79,48 @@ fn create_account_exec_request(address: AccountHash) -> ExecuteRequest {
 #[ignore]
 #[test]
 fn host_function_metrics_has_acceptable_gas_cost() {
-    let rng: &mut StdRng = &mut SeedableRng::seed_from_u64(0);
+    let mut builder = setup();
 
-    let mut builder = InMemoryWasmTestBuilder::default();
-    builder
-        .run_genesis(&*DEFAULT_RUN_GENESIS_REQUEST)
-        .exec(create_account_exec_request(ACCOUNT0_ADDR))
-        .expect_success()
-        .commit()
-        .exec(create_account_exec_request(ACCOUNT1_ADDR))
-        .expect_success()
-        .commit();
+    let seed: u64 = SEED_VALUE;
+    let random_bytes = {
+        let mut random_bytes = vec![0_u8; 10_000];
+        for (i, byte) in random_bytes.iter_mut().enumerate() {
+            *byte = i.checked_rem(256).unwrap().try_into().unwrap();
+        }
+        random_bytes
+    };
 
-    let seed: u64 = rng.gen();
-    let mut random_bytes = vec![0_u8; 10_000];
-    rng.fill(random_bytes.as_mut_slice());
-
-    let deploy = DeployItemBuilder::new()
-        .with_address(ACCOUNT0_ADDR)
-        .with_deploy_hash(rng.gen())
-        .with_session_code(
-            CONTRACT_HOST_FUNCTION_METRICS,
-            runtime_args! {
-                "seed" => seed,
-                "others" => (Bytes::from(random_bytes), ACCOUNT0_ADDR, ACCOUNT1_ADDR),
-            },
-        )
-        .with_empty_payment_bytes(
-            runtime_args! { standard_payment::ARG_AMOUNT => U512::from(1_000_000_000_000_u64) },
-        )
-        .with_authorization_keys(&[ACCOUNT0_ADDR])
-        .build();
-    let exec_request = ExecuteRequestBuilder::new().push_deploy(deploy).build();
+    let exec_request = {
+        let deploy_item = DeployItemBuilder::new()
+            .with_address(ACCOUNT0_ADDR)
+            .with_deploy_hash([55; 32])
+            .with_session_code(
+                CONTRACT_HOST_FUNCTION_METRICS,
+                runtime_args! {
+                    ARG_SEED => seed,
+                    ARG_OTHERS => (Bytes::from(random_bytes), ACCOUNT0_ADDR, ACCOUNT1_ADDR),
+                },
+            )
+            .with_empty_payment_bytes(
+                runtime_args! { standard_payment::ARG_AMOUNT => U512::from(*DEFAULT_PAYMENT) },
+            )
+            .with_authorization_keys(&[ACCOUNT0_ADDR])
+            .build();
+        ExecuteRequestBuilder::from_deploy_item(deploy_item).build()
+    };
 
     builder.exec(exec_request);
-    let error_message = builder
-        .exec_error_message(2)
-        .expect("should have an error message");
-    assert!(error_message.contains(&format!("{:?}", ApiError::User(9))));
+
+    let error = builder.get_error().expect("should have error");
+    assert!(
+        matches!(
+            error,
+            engine_state::Error::Exec(execution::Error::Revert(ApiError::User(user_error)))
+            if user_error == EXPECTED_REVERT_VALUE
+        ),
+        "Expected revert but actual error is {:?}",
+        error
+    );
 
     let gas_cost = builder.last_exec_gas_cost().value();
     assert!(
@@ -109,4 +134,17 @@ fn host_function_metrics_has_acceptable_gas_cost() {
         "Performance improvement: contract host-function-metrics used only {} gas; please adjust this regression test.",
         gas_cost
     );
+}
+
+fn setup() -> InMemoryWasmTestBuilder {
+    let mut builder = InMemoryWasmTestBuilder::default();
+    builder
+        .run_genesis(&*DEFAULT_RUN_GENESIS_REQUEST)
+        .exec(create_account_exec_request(ACCOUNT0_ADDR))
+        .expect_success()
+        .commit()
+        .exec(create_account_exec_request(ACCOUNT1_ADDR))
+        .expect_success()
+        .commit();
+    builder
 }
