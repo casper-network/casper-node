ffb68fbfbd9e7973fc8252222fe0693f5e08af4c
Karan Dhareshwar <karan@casperlabs.io>
2022-02-28 13:24:50 -0600

    Undo changes to host_function_metrics size test

--- execution_engine_testing/tests/src/test/regression/host_function_metrics_size_and_gas_cost.rs
+++ execution_engine_testing/tests/src/test/regression/host_function_metrics_size_and_gas_cost.rs
@@ -21,11 +21,11 @@ const CONTRACT_TRANSFER_TO_ACCOUNT_U512: &str = "transfer_to_account_u512.wasm";
 
 // This value is not systemic, as code is added the size of WASM will increase,
 // you can change this value to reflect the increase in WASM size.
-const HOST_FUNCTION_METRICS_STANDARD_SIZE: usize = 157_055;
+const HOST_FUNCTION_METRICS_STANDARD_SIZE: usize = 116_800;
 const HOST_FUNCTION_METRICS_STANDARD_GAS_COST: u64 = 151_323_460_380;
 
 /// Acceptable size regression/improvement in percentage.
-const SIZE_MARGIN: usize = 25;
+const SIZE_MARGIN: usize = 5;
 /// Acceptable gas cost regression/improvement in percentage.
 const GAS_COST_MARGIN: u64 = 5;
 
