24dbe7133c6b428b4ccc0370e691b57aa7c8ce5f
Rafał Chabowski <rafal@casperlabs.io>
2022-03-25 12:18:06 +0100

    Add comment about potential optimization

--- node/src/components/rpc_server/rpcs/chain.rs
+++ node/src/components/rpc_server/rpcs/chain.rs
@@ -483,6 +483,9 @@ pub(super) async fn get_block_with_metadata<REv: ReactorEventT>(
         return Ok(block_with_metadata);
     }
 
+    // TODO: Potential optimization: We might want to make the `GetBlock` actually return the
+    // highest disjoint sequence of block heights, so we don't need to request it again inside the
+    // `missing_block_or_state_root_error` function.
     let error = match maybe_id {
         Some(BlockIdentifier::Hash(block_hash)) => common::missing_block_or_state_root_error(
             effect_builder,
