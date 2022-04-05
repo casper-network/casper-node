3c0f03a8ff7000bd07192c87b97c665f5206760d
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2022-04-05 18:01:06 -0400

    Removing build.sh casper-client build.

--- utils/nctl/sh/staging/build.sh
+++ utils/nctl/sh/staging/build.sh
@@ -55,13 +55,6 @@ function set_stage_binaries()
         cargo build --release --package casper-node --features casper-mainnet
     fi
 
-    # Set client binary.
-    if [ "$NCTL_COMPILE_TARGET" = "debug" ]; then
-        cargo build --package casper-client --features casper-mainnet
-    else
-        cargo build --release --package casper-client --features casper-mainnet
-    fi
-
     # Set client-side wasm.
     make build-contract-rs/activate-bid
     make build-contract-rs/add-bid
