e4ee1191dea6d8f51d31d628520be7f112913d97
Rafał Chabowski <rafal@casperlabs.io>
2022-04-20 14:28:02 +0200

    Use proc_macro to remove the repetition in code

--- node_macros/src/lib.rs
+++ node_macros/src/lib.rs
@@ -13,7 +13,10 @@ mod parse;
 mod rust_type;
 mod util;
 
+use casper_types::PublicKey;
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
 use syn::parse_macro_input;
 
 use parse::ReactorDefinition;
@@ -31,6 +34,83 @@ pub fn reactor(input: TokenStream) -> TokenStream {
     output.extend(gen::generate_reactor(&def));
     output.extend(gen::generate_reactor_types(&def));
     output.extend(gen::generate_reactor_impl(&def));
+
+    output.into()
+}
+
+/// Generates a function to set bytes in the ed25519 public key
+#[proc_macro]
+pub fn make_ed25519_capnp_functions(_input: TokenStream) -> TokenStream {
+    let mut output: proc_macro2::TokenStream = Default::default();
+
+    let mut inner_loop_set: proc_macro2::TokenStream = Default::default();
+    for i in 0..PublicKey::ED25519_LENGTH {
+        let ident = Ident::new(&format!("set_byte{}", i), Span::call_site());
+        inner_loop_set.extend(quote!(
+            msg.#ident(bytes[#i]);
+        ));
+    }
+
+    output.extend(quote!(
+        fn set_ed25519(msg: &mut public_key_capnp::ed25519_public_key::Builder, bytes: &[u8; PublicKey::ED25519_LENGTH]) {
+            #inner_loop_set
+        }
+    ));
+
+    let mut inner_loop_get: proc_macro2::TokenStream = Default::default();
+    for i in 0..PublicKey::ED25519_LENGTH {
+        let ident = Ident::new(&format!("get_byte{}", i), Span::call_site());
+        inner_loop_get.extend(quote!(
+            reader.#ident(),
+        ));
+    }
+
+    output.extend(quote!(
+        fn get_ed25519(reader: public_key_capnp::ed25519_public_key::Reader) -> [u8; PublicKey::ED25519_LENGTH] {
+            [
+                #inner_loop_get
+            ]
+        }
+    ));
+
+    output.into()
+}
+
+/// Generates a function to set bytes in the SECP256K1 public key
+// TODO[RC]: Deduplicate with `make_ed25519_capnp_functions`
+#[proc_macro]
+pub fn make_secp256k1_capnp_functions(_input: TokenStream) -> TokenStream {
+    let mut output: proc_macro2::TokenStream = Default::default();
+
+    let mut inner_loop_set: proc_macro2::TokenStream = Default::default();
+    for i in 0..PublicKey::SECP256K1_LENGTH {
+        let ident = Ident::new(&format!("set_byte{}", i), Span::call_site());
+        inner_loop_set.extend(quote!(
+            msg.#ident(bytes[#i]);
+        ));
+    }
+
+    output.extend(quote!(
+        fn set_secp256k1(msg: &mut public_key_capnp::secp256k1_public_key::Builder, bytes: &[u8; PublicKey::SECP256K1_LENGTH]) {
+            #inner_loop_set
+        }
+    ));
+
+    let mut inner_loop_get: proc_macro2::TokenStream = Default::default();
+    for i in 0..PublicKey::SECP256K1_LENGTH {
+        let ident = Ident::new(&format!("get_byte{}", i), Span::call_site());
+        inner_loop_get.extend(quote!(
+            reader.#ident(),
+        ));
+    }
+
+    output.extend(quote!(
+        fn get_secp256k1(reader: public_key_capnp::secp256k1_public_key::Reader) -> [u8; PublicKey::SECP256K1_LENGTH] {
+            [
+                #inner_loop_get
+            ]
+        }
+    ));
 
     output.into()
 }
