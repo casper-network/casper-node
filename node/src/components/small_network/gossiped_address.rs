a563b97d0b107567bfb8b776fc1a58471d48a564
George Pisaltu <georgep@casperlabs.io>
2022-04-29 12:02:07 +0300

    Add bytesrepr support for GossipedAddress
    Signed-off-by: George Pisaltu <georgep@casperlabs.io>

--- node/src/components/small_network/gossiped_address.rs
+++ node/src/components/small_network/gossiped_address.rs
@@ -4,9 +4,17 @@ use std::{
     net::SocketAddr,
 };
 
-use casper_types::EraId;
 use datasize::DataSize;
+#[cfg(test)]
+use rand::Rng;
 use serde::{Deserialize, Serialize};
+
+#[cfg(test)]
+use casper_types::testing::TestRng;
+use casper_types::{
+    bytesrepr::{allocate_buffer, Error as BytesreprError, FromBytes, ToBytes},
+    EraId,
+};
 
 use crate::types::{Item, Tag};
 
@@ -20,11 +28,50 @@ impl GossipedAddress {
     pub(super) fn new(address: SocketAddr) -> Self {
         GossipedAddress(address)
     }
+
+    #[cfg(test)]
+    pub fn random_ipv4(rng: &mut TestRng) -> Self {
+        let bytes = rng.gen::<[u8; 4]>();
+        let port = rng.gen::<u16>();
+        Self::new(SocketAddr::new(bytes.into(), port))
+    }
+
+    #[cfg(test)]
+    pub fn random_ipv6(rng: &mut TestRng) -> Self {
+        let bytes = rng.gen::<[u8; 16]>();
+        let port = rng.gen::<u16>();
+        Self::new(SocketAddr::new(bytes.into(), port))
+    }
 }
 
 impl Display for GossipedAddress {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
         write!(formatter, "gossiped-address {}", self.0)
+    }
+}
+
+impl ToBytes for GossipedAddress {
+    fn to_bytes(&self) -> Result<Vec<u8>, BytesreprError> {
+        let mut buf = allocate_buffer(self)?;
+        self.write_bytes(&mut buf)?;
+        Ok(buf)
+    }
+
+    fn serialized_length(&self) -> usize {
+        ToBytes::serialized_length(&self.0.to_string())
+    }
+
+    fn write_bytes(&self, writer: &mut Vec<u8>) -> Result<(), BytesreprError> {
+        self.0.to_string().write_bytes(writer)
+    }
+}
+
+impl FromBytes for GossipedAddress {
+    fn from_bytes(bytes: &[u8]) -> Result<(Self, &[u8]), BytesreprError> {
+        let (sock_addr_str, bytes): (String, &[u8]) = FromBytes::from_bytes(bytes)?;
+        let sock_addr = std::str::FromStr::from_str(sock_addr_str.as_str())
+            .map_err(|_| BytesreprError::Formatting)?;
+        Ok((GossipedAddress::new(sock_addr), bytes))
     }
 }
 
@@ -49,5 +96,22 @@ impl Item for GossipedAddress {
 impl From<GossipedAddress> for SocketAddr {
     fn from(gossiped_address: GossipedAddress) -> Self {
         gossiped_address.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use casper_types::bytesrepr;
+
+    use super::*;
+
+    #[test]
+    fn bytesrepr_roundtrip() {
+        let mut rng = crate::new_rng();
+        let gossiped_address_ipv4 = GossipedAddress::random_ipv4(&mut rng);
+        bytesrepr::test_serialization_roundtrip(&gossiped_address_ipv4);
+
+        let gossiped_address_ipv6 = GossipedAddress::random_ipv6(&mut rng);
+        bytesrepr::test_serialization_roundtrip(&gossiped_address_ipv6);
     }
 }
