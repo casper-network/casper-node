2f13debb08271b40b377bfa0bdb6adc980fcd740
Andreas Fackler <andreas@casperlabs.io>
2022-04-29 15:55:02 +0200

    Don't add faulty echos/votes after handle_faulty; filter faulty in reply to SyncState.
    If we detect double-signing we remove all the echos and votes of the
    faulty validator, but we used to still add the second of the two
    conflicting signatures afterwards. That was a bug.
    
    We now also don't send echos and votes of faulty validators when
    responding to SyncState messages, since these are redundant.

--- node/src/components/consensus/protocols/simple_consensus.rs
+++ node/src/components/consensus/protocols/simple_consensus.rs
@@ -776,12 +776,13 @@ impl<C: Context + 'static> SimpleConsensus<C> {
             );
             return vec![];
         }
+        let our_faulty = self.validator_bit_field(first_validator_idx, self.faults.keys().cloned());
         let mut contents = vec![];
         if let Some(hash) = proposal_hash {
             if let Some(echo_map) = round.echos.get(&hash) {
                 let our_echos =
                     self.validator_bit_field(first_validator_idx, echo_map.keys().cloned());
-                let missing_echos = our_echos & !echos;
+                let missing_echos = our_echos & !(echos | faulty | our_faulty);
                 for v_idx in self.iter_validator_bit_field(first_validator_idx, missing_echos) {
                     contents.push((Content::Echo(hash), v_idx, echo_map[&v_idx]));
                 }
@@ -799,14 +800,14 @@ impl<C: Context + 'static> SimpleConsensus<C> {
         }
         let our_true_votes =
             self.validator_bit_field(first_validator_idx, round.votes[&true].keys_some());
-        let missing_true_votes = our_true_votes & !true_votes;
+        let missing_true_votes = our_true_votes & !(true_votes | faulty | our_faulty);
         for v_idx in self.iter_validator_bit_field(first_validator_idx, missing_true_votes) {
             let signature = round.votes[&true][v_idx].unwrap();
             contents.push((Content::Vote(true), v_idx, signature));
         }
         let our_false_votes =
             self.validator_bit_field(first_validator_idx, round.votes[&false].keys_some());
-        let missing_false_votes = our_false_votes & !false_votes;
+        let missing_false_votes = our_false_votes & !(false_votes | faulty | our_faulty);
         for v_idx in self.iter_validator_bit_field(first_validator_idx, missing_false_votes) {
             let signature = round.votes[&false][v_idx].unwrap();
             contents.push((Content::Vote(false), v_idx, signature));
@@ -824,7 +825,6 @@ impl<C: Context + 'static> SimpleConsensus<C> {
                 ProtocolOutcome::CreatedTargetedMessage(msg.serialize(), sender)
             })
             .collect_vec();
-        let our_faulty = self.validator_bit_field(first_validator_idx, self.faults.keys().cloned());
         let missing_faulty = our_faulty & !faulty;
         for v_idx in self.iter_validator_bit_field(first_validator_idx, missing_faulty) {
             match &self.faults[&v_idx] {
@@ -931,6 +931,10 @@ impl<C: Context + 'static> SimpleConsensus<C> {
                 (content2, signature2),
                 now,
             ));
+            if !content.is_proposal() {
+                debug!(?validator_id, "ignoring message from faulty validator");
+                return outcomes;
+            }
         }
 
         match content {
