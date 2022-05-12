7924e8a22c6b823ba3f7e7a4423f2e62f8de6245
Mateusz <mateusz@casperlabs.io>
2022-05-12 15:12:04 +0200

    Commit missed file.

--- node/src/components/chain_synchronizer/operations.rs
+++ node/src/components/chain_synchronizer/operations.rs
@@ -205,7 +205,7 @@ where
     loop {
         let mut new_peer_list = ctx
             .effect_builder
-            .get_fully_connected_validator_peers()
+            .get_fully_connected_non_joiner_peers()
             .await;
         ctx.filter_bad_peers(&mut new_peer_list);
 
@@ -517,7 +517,7 @@ where
     for _ in 0..ctx.config.max_retries_while_not_connected() {
         peers = ctx
             .effect_builder
-            .get_fully_connected_validator_peers()
+            .get_fully_connected_non_joiner_peers()
             .await;
         if !peers.is_empty() {
             break;
@@ -703,7 +703,7 @@ async fn sync_trie_store(state_root_hash: Digest, ctx: &ChainSyncContext<'_>) ->
     let peer_count = cmp::max(
         1,
         ctx.effect_builder
-            .get_fully_connected_validator_peers()
+            .get_fully_connected_non_joiner_peers()
             .await
             .len()
             .saturating_sub(
@@ -1318,7 +1318,7 @@ async fn execute_blocks(
             let mut success = false;
             for peer in ctx
                 .effect_builder
-                .get_fully_connected_validator_peers()
+                .get_fully_connected_non_joiner_peers()
                 .await
                 .into_iter()
                 .take(APPROVAL_FETCH_RETRIES)
@@ -1408,7 +1408,7 @@ async fn fetch_and_store_deploys(
     let peer_count = cmp::max(
         1,
         ctx.effect_builder
-            .get_fully_connected_validator_peers()
+            .get_fully_connected_non_joiner_peers()
             .await
             .len()
             .saturating_sub(
