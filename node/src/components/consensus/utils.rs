69ae8f424bc2dd17168239e64361304eef8f9250
Bartłomiej Kamiński <bart@casperlabs.io>
2022-05-19 14:40:17 +0200

    Verify that the set of finality signatures is minimal

--- node/src/components/consensus/utils.rs
+++ node/src/components/consensus/utils.rs
@@ -16,6 +16,7 @@ pub(crate) fn check_sufficient_finality_signatures(
 ) -> Result<(), FinalitySignatureError> {
     // Calculate the weight of the signatures
     let mut signature_weight: U512 = U512::zero();
+    let mut minimum_weight: Option<U512> = None;
     for (public_key, _) in block_signatures.proofs.iter() {
         match trusted_validator_weights.get(public_key) {
             None => {
@@ -26,10 +27,15 @@ pub(crate) fn check_sufficient_finality_signatures(
                 })
             }
             Some(validator_weight) => {
+                if minimum_weight.map_or(true, |min_w| *validator_weight < min_w) {
+                    minimum_weight = Some(*validator_weight);
+                }
                 signature_weight += *validator_weight;
             }
         }
     }
+
+    let weight_minus_minimum = signature_weight - minimum_weight.unwrap_or_else(U512::zero);
 
     // Check the finality signatures have sufficient weight
     let total_weight: U512 = trusted_validator_weights
@@ -47,6 +53,21 @@ pub(crate) fn check_sufficient_finality_signatures(
             trusted_validator_weights: trusted_validator_weights.clone(),
             block_signatures: Box::new(block_signatures.clone()),
             signature_weight: Box::new(signature_weight),
+            total_validator_weight: Box::new(total_weight),
+            finality_threshold_fraction,
+        });
+    }
+
+    // Verify: the total weight decreased by the smallest weight of a single signature should be
+    // below threshold (anti-spam countermeasure).
+    if weight_minus_minimum * U512::from(*lower_bound.denom())
+        > total_weight * U512::from(*lower_bound.numer())
+    {
+        return Err(FinalitySignatureError::TooManySignatures {
+            trusted_validator_weights: trusted_validator_weights.clone(),
+            block_signatures: Box::new(block_signatures.clone()),
+            signature_weight: Box::new(signature_weight),
+            weight_minus_minimum: Box::new(weight_minus_minimum),
             total_validator_weight: Box::new(total_weight),
             finality_threshold_fraction,
         });
