fe94377d1d44bf6e1094baeae0c0a72c4a325413
Rafał Chabowski <rafal@casperlabs.io>
2022-06-06 14:45:29 +0200

    Clean-up error assertion code

--- execution_engine_testing/tests/src/test/regression/gov_42.rs
+++ execution_engine_testing/tests/src/test/regression/gov_42.rs
@@ -2,8 +2,9 @@ use casper_engine_test_support::{
     DeployItemBuilder, ExecuteRequestBuilder, InMemoryWasmTestBuilder, DEFAULT_ACCOUNT_ADDR,
     DEFAULT_PAYMENT, PRODUCTION_RUN_GENESIS_REQUEST,
 };
+use casper_execution_engine::core::engine_state::ExecError;
 use casper_execution_engine::{
-    core::engine_state::{Error, MAX_PAYMENT},
+    core::engine_state::{self, Error, MAX_PAYMENT},
     shared::wasm_prep::{self, PreprocessingError},
 };
 use casper_types::{runtime_args, Gas, RuntimeArgs, U512};
@@ -11,7 +12,8 @@ use num_traits::Zero;
 
 use crate::{
     test::regression::test_utils::{
-        make_gas_counter_overflow, make_module_without_memory_section, make_stack_overflow,
+        make_gas_counter_overflow, make_module_without_call, make_module_without_memory_section,
+        make_stack_overflow,
     },
     wasm_utils,
 };
@@ -20,14 +22,20 @@ const ARG_AMOUNT: &str = "amount";
 
 struct TestCase<'a> {
     pub input_wasm_bytes: &'a [u8],
-    pub expected_error: wasm_prep::PreprocessingError,
+    pub expected_error_payment: String,
+    pub expected_error_session: String,
 }
 
 impl<'a> TestCase<'a> {
-    fn new(input_wasm_bytes: &'a [u8], expected_error: wasm_prep::PreprocessingError) -> Self {
+    fn new(
+        input_wasm_bytes: &'a [u8],
+        expected_error_payment: String,
+        expected_error_session: String,
+    ) -> Self {
         Self {
             input_wasm_bytes,
-            expected_error,
+            expected_error_payment,
+            expected_error_session,
         }
     }
 }
@@ -41,7 +49,8 @@ enum DeployBytes {
 fn run_test_case(
     TestCase {
         input_wasm_bytes,
-        expected_error,
+        expected_error_payment,
+        expected_error_session,
     }: &TestCase,
     deploy_bytes: DeployBytes,
 ) {
@@ -49,34 +58,40 @@ fn run_test_case(
 
     let expected_transaction_fee = *MAX_PAYMENT;
 
-    let do_minimum_request = {
+    let (do_minimum_request_builder, expected_error_message) = {
         let account_hash = *DEFAULT_ACCOUNT_ADDR;
         let session_args = RuntimeArgs::default();
         let deploy_hash = [42; 32];
 
-        let deploy = match deploy_bytes {
-            DeployBytes::Payment => DeployItemBuilder::new()
-                .with_payment_bytes(
-                    input_wasm_bytes.to_vec(),
-                    runtime_args! {
-                        ARG_AMOUNT =>  U512::from(0),
-                    },
-                )
-                .with_session_bytes(wasm_utils::do_nothing_bytes(), session_args),
-            DeployBytes::Session => DeployItemBuilder::new()
-                .with_session_bytes(input_wasm_bytes.to_vec(), session_args)
-                .with_empty_payment_bytes(runtime_args! {
-                    ARG_AMOUNT => payment_amount,
-                }),
-        }
-        .with_address(account_hash)
-        .with_authorization_keys(&[account_hash])
-        .with_deploy_hash(deploy_hash)
-        .build();
+        let (deploy_item_builder, expected_error_message) = match deploy_bytes {
+            DeployBytes::Payment => (
+                DeployItemBuilder::new()
+                    .with_payment_bytes(
+                        input_wasm_bytes.to_vec(),
+                        runtime_args! {ARG_AMOUNT => U512::from(0),},
+                    )
+                    .with_session_bytes(wasm_utils::do_nothing_bytes(), session_args),
+                expected_error_payment,
+            ),
+            DeployBytes::Session => (
+                DeployItemBuilder::new()
+                    .with_session_bytes(input_wasm_bytes.to_vec(), session_args)
+                    .with_empty_payment_bytes(runtime_args! {ARG_AMOUNT => payment_amount,}),
+                expected_error_session,
+            ),
+        };
+        let deploy = deploy_item_builder
+            .with_address(account_hash)
+            .with_authorization_keys(&[account_hash])
+            .with_deploy_hash(deploy_hash)
+            .build();
 
-        ExecuteRequestBuilder::new().push_deploy(deploy)
-    }
-    .build();
+        (
+            ExecuteRequestBuilder::new().push_deploy(deploy),
+            expected_error_message,
+        )
+    };
+    let do_minimum_request = do_minimum_request_builder.build();
 
     let mut builder = InMemoryWasmTestBuilder::default();
     builder.run_genesis(&PRODUCTION_RUN_GENESIS_REQUEST);
@@ -89,31 +104,36 @@ fn run_test_case(
 
     builder.exec(do_minimum_request).expect_failure().commit();
 
-    let actual_error = builder.get_error().expect("should have error");
+    let actual_error = builder.get_error().expect("should have error").to_string();
+    dbg!(&actual_error);
 
-    if let Error::WasmPreprocessing(error) = actual_error {
-        match error {
-            PreprocessingError::Deserialize(actual_message) => {
-                if let PreprocessingError::Deserialize(expected_message) = expected_error {
-                    assert_eq!(*expected_message, actual_message)
-                } else {
-                    assert!(false, "got unexpected error variant")
-                }
-            }
-            PreprocessingError::OperationForbiddenByGasRules => assert!(matches!(
-                expected_error,
-                PreprocessingError::OperationForbiddenByGasRules
-            )),
-            PreprocessingError::StackLimiter => todo!(),
-            PreprocessingError::MissingMemorySection => assert!(matches!(
-                expected_error,
-                PreprocessingError::MissingMemorySection
-            )),
-            PreprocessingError::MissingModule => panic!(
-                "MissingModule can only be reported by invoking `casper_add_contract_version`"
-            ),
-        }
-    }
+    assert!(actual_error.contains(expected_error_message));
+
+    // if let Error::WasmPreprocessing(error) = actual_error {
+    //     match error {
+    //         PreprocessingError::Deserialize(actual_message) => {
+    //             if let PreprocessingError::Deserialize(expected_message) = expected_error {
+    //                 assert_eq!(*expected_message, actual_message)
+    //             } else {
+    //                 assert!(false, "got unexpected error variant")
+    //             }
+    //         }
+    //         PreprocessingError::OperationForbiddenByGasRules => assert!(matches!(
+    //             expected_error,
+    //             PreprocessingError::OperationForbiddenByGasRules
+    //         )),
+    //         PreprocessingError::StackLimiter => {
+    //             panic!("Unable to inject stack limiter to the module bytes")
+    //         }
+    //         PreprocessingError::MissingMemorySection => assert!(matches!(
+    //             expected_error,
+    //             PreprocessingError::MissingMemorySection
+    //         )),
+    //         PreprocessingError::MissingModule => panic!(
+    //             "MissingModule can only be reported by invoking `casper_add_contract_version`"
+    //         ),
+    //     }
+    // }
 
     let gas = builder.last_exec_gas_cost();
     dbg!(&gas);
@@ -158,37 +178,46 @@ fn should_charge_session_with_incorrect_wasm_files() {
 
     let wasm_stack_overflow = make_stack_overflow();
 
-    let test_cases: &[TestCase; 6] = {
+    let wasm_without_call = make_module_without_call();
+
+    let test_cases: &[TestCase; 7] = {
         &[
             TestCase::new(
                 WASM_BYTES_INVALID_MAGIC_NUMBER,
-                wasm_prep::PreprocessingError::Deserialize(String::from(
-                    "Invalid magic number at start of file",
-                )),
+                "Wasm preprocessing error: Deserialization error: Invalid magic number at start of file".to_string(),
+                "Wasm preprocessing error: Deserialization error: Invalid magic number at start of file".to_string(),
             ),
             TestCase::new(
                 WASM_BYTES_EMPTY,
-                wasm_prep::PreprocessingError::Deserialize(String::from(
-                    "I/O Error: UnexpectedEof",
-                )),
+                "Insufficient payment".to_string(),
+                "Wasm preprocessing error: Deserialization error: I/O Error: UnexpectedEof"
+                    .to_string(),
             ),
             TestCase::new(
                 WASM_BYTES_INVALID_BUT_WITH_CORRECT_MAGIC_NUMBER,
-                wasm_prep::PreprocessingError::Deserialize(String::from(
-                    "I/O Error: UnexpectedEof",
-                )),
+                "I/O Error: UnexpectedEof".to_string(),
+                "I/O Error: UnexpectedEof".to_string()
             ),
             TestCase::new(
                 wasm_bytes_gas_overflow.as_slice(),
-                wasm_prep::PreprocessingError::OperationForbiddenByGasRules,
+                "Wasm preprocessing error: Encountered operation forbidden by gas rules. Consult instruction -> metering config map".to_string(),
+                "Wasm preprocessing error: Encountered operation forbidden by gas rules. Consult instruction -> metering config map".to_string(),
             ),
             TestCase::new(
                 wasm_bytes_no_memory_section.as_slice(),
-                wasm_prep::PreprocessingError::MissingMemorySection,
+                "Memory section should exist".to_string(),
+                "Memory section should exist".to_string(),
             ),
             TestCase::new(
                 wasm_stack_overflow.as_slice(),
-                wasm_prep::PreprocessingError::MissingMemorySection,
+                // TODO: wasm that overflows stack produces this kind of error (?)
+                "Interpreter error: trap: Trap { kind: Unreachable }".to_string(),
+                "Interpreter error: trap: Trap { kind: Unreachable }".to_string(),
+            ),
+            TestCase::new(
+                wasm_without_call.as_slice(),
+                "Interpreter error: Module doesn't have export call".to_string(),
+                "Interpreter error: Module doesn't have export call".to_string(),
             ),
         ]
     };
