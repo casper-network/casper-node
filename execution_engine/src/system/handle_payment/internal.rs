318444d050c5b18f3a61e79e91c6324197ace116
Fraser Hutchison <fraser@casperlabs.io>
2022-06-08 01:35:58 +0100

    simplify refund and reward calculation

--- execution_engine/src/system/handle_payment/internal.rs
+++ execution_engine/src/system/handle_payment/internal.rs
@@ -1,4 +1,4 @@
-use num::{CheckedAdd, CheckedMul, CheckedSub, One, Zero};
+use num::{CheckedMul, One};
 use num_rational::Ratio;
 use tracing::error;
 
@@ -45,72 +45,37 @@ pub(crate) fn get_refund_purse<R: RuntimeProvider>(
     }
 }
 
-/// Returns tuple where 1st element is user part, and 2nd element is the fee part.
-fn calculate_amounts(
-    amount_gas_spent: U512,
+/// Returns tuple where 1st element is the refund, and 2nd element is the reward.
+fn calculate_refund_and_reward(
+    gas_spent: U512,
     payment_purse_balance: U512,
     refund_handling: &RefundHandling,
 ) -> Result<(U512, U512), Error> {
-    let amount_gas_spent = Ratio::from(amount_gas_spent);
-    let payment_purse_balance = Ratio::from(payment_purse_balance);
+    let unspent = payment_purse_balance
+        .checked_sub(gas_spent)
+        .ok_or(Error::ArithmeticOverflow)?;
 
-    let refund_amount = {
-        let refund_amount_raw = payment_purse_balance
-            .checked_sub(&amount_gas_spent)
-            .ok_or(Error::ArithmeticOverflow)?;
-
-        let refund_ratio = match refund_handling {
-            RefundHandling::Refund { refund_ratio } => {
-                debug_assert!(
-                    refund_ratio <= &Ratio::one(),
-                    "refund ratio should be a proper fraction"
-                );
-                *refund_ratio
-            }
-        };
-
-        let refund_ratio_u512 = {
-            let (numer, denom) = refund_ratio.into();
+    let refund_ratio = match refund_handling {
+        RefundHandling::Refund { refund_ratio } => {
+            debug_assert!(
+                refund_ratio <= &Ratio::one(),
+                "refund ratio should be a proper fraction"
+            );
+            let (numer, denom) = (*refund_ratio).into();
             Ratio::new_raw(U512::from(numer), U512::from(denom))
-        };
-
-        refund_amount_raw
-            .checked_mul(&refund_ratio_u512)
-            .ok_or(Error::ArithmeticOverflow)?
+        }
     };
 
-    let fees_reward = payment_purse_balance
-        .checked_sub(&refund_amount)
+    let refund = Ratio::from(unspent)
+        .checked_mul(&refund_ratio)
+        .ok_or(Error::ArithmeticOverflow)?
+        .to_integer();
+
+    let reward = payment_purse_balance
+        .checked_sub(refund)
         .ok_or(Error::ArithmeticOverflow)?;
 
-    let refund_amount_trunc = refund_amount.trunc();
-    let fees_reward_trunc = fees_reward.trunc();
-
-    let dust_amount = fees_reward
-        .fract()
-        .checked_add(&refund_amount.fract())
-        .ok_or(Error::ArithmeticOverflow)?;
-
-    // Move the dust amount to the reward part
-
-    let refund_amount = refund_amount_trunc;
-    debug_assert_eq!(refund_amount.fract(), Ratio::zero());
-
-    let fees_amount = fees_reward_trunc
-        .checked_add(&dust_amount)
-        .ok_or(Error::ArithmeticOverflow)?;
-    debug_assert_eq!(fees_amount.fract(), Ratio::zero());
-
-    // Makes sure both parts: for user, and for validator sums to the total amount in the
-    // payment's purse.
-    debug_assert_eq!(
-        refund_amount + fees_amount,
-        payment_purse_balance,
-        "both user part and validator part should equal to the purse balance"
-    );
-
-    // Safely convert ratio to integer after making sure there's no dust amount left behind.
-    Ok((refund_amount.to_integer(), fees_amount.to_integer()))
+    Ok((refund, reward))
 }
 
 /// Transfers funds from the payment purse to the validator rewards purse, as well as to the
@@ -139,7 +104,7 @@ pub(crate) fn finalize_payment<P: MintProvider + RuntimeProvider>(
     }
 
     let (refund_amount, validator_reward) =
-        calculate_amounts(gas_spent, payment_amount, provider.refund_handling())?;
+        calculate_refund_and_reward(gas_spent, payment_amount, provider.refund_handling())?;
 
     debug_assert_eq!(validator_reward + refund_amount, payment_amount);
 
@@ -155,10 +120,11 @@ pub(crate) fn finalize_payment<P: MintProvider + RuntimeProvider>(
 
     provider.remove_key(REFUND_PURSE_KEY)?; //unset refund purse after reading it
 
-    // give refund
-
+    // Give or burn the refund.
     if !refund_amount.is_zero() {
         if target == URef::default() {
+            // Fee-handling is set to `Burn`.  Deduct the refund from the payment purse and reduce
+            // the total supply (i.e. burn the refund).
             payment_amount = payment_amount
                 .checked_sub(refund_amount)
                 .ok_or(Error::ArithmeticOverflow)?;
@@ -168,7 +134,7 @@ pub(crate) fn finalize_payment<P: MintProvider + RuntimeProvider>(
         } else {
             match refund_purse {
                 Some(refund_purse) => {
-                    // in case of failure to transfer to refund purse we fall back on the account's
+                    // In case of failure to transfer to refund purse we fall back on the account's
                     // main purse
                     match provider.transfer_purse_to_purse(
                         payment_purse,
@@ -177,7 +143,12 @@ pub(crate) fn finalize_payment<P: MintProvider + RuntimeProvider>(
                     ) {
                         Ok(()) => {}
                         Err(error) => {
-                            error!(%error, %refund_amount, %account, "unable to transfer refund to a refund purse; refunding to account");
+                            error!(
+                                %error,
+                                %refund_amount,
+                                %account,
+                                "unable to transfer refund to a refund purse; refunding to account"
+                            );
                             refund_to_account::<P>(
                                 provider,
                                 payment_purse,
@@ -194,19 +165,15 @@ pub(crate) fn finalize_payment<P: MintProvider + RuntimeProvider>(
         }
     }
 
-    // pay the reward to the target
-
+    // Pay or burn the reward.
     if target == URef::default() {
-        payment_amount = payment_amount
-            .checked_sub(validator_reward)
-            .ok_or(Error::ArithmeticOverflow)?;
-
-        provider.write_balance(payment_purse, payment_amount)?;
+        // Fee-handling is set to `Burn`.  Deduct the reward from the payment purse, leaving it
+        // empty, and reduce the total supply (i.e. burn the reward).
+        provider.write_balance(payment_purse, U512::zero())?;
         provider.reduce_total_supply(validator_reward)?;
     } else {
-        // validator_reward purse is already resolved based on fee elimination config which is
-        // either a proposer or rewards purse
-
+        // validator_reward purse is already resolved based on fee-handling config which is either a
+        // proposer or rewards purse.
         match provider.transfer_purse_to_purse(payment_purse, target, validator_reward) {
             Ok(()) => {}
             Err(error) => {
@@ -244,7 +211,7 @@ mod tests {
         let refund_ratio = Ratio::new_raw(1, 3);
         let refund = RefundHandling::Refund { refund_ratio };
 
-        let (a, b) = calculate_amounts(gas, purse_bal, &refund).unwrap();
+        let (a, b) = calculate_refund_and_reward(gas, purse_bal, &refund).unwrap();
 
         assert_eq!(a, U512::from(2u64)); // (10 - 3) * 1/3 ~ 2.33 (.33 is dust)
         assert_eq!(b, U512::from(8u64)); // 10 - 2 = 8
@@ -259,7 +226,7 @@ mod tests {
             let refund_ratio = Ratio::new_raw(percentage, 100);
             let refund = RefundHandling::Refund { refund_ratio };
 
-            let (a, b) = calculate_amounts(gas, purse_bal, &refund).unwrap();
+            let (a, b) = calculate_refund_and_reward(gas, purse_bal, &refund).unwrap();
 
             let a = Ratio::from(a);
             let b = Ratio::from(b);
