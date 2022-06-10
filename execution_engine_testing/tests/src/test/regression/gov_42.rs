f36a87b7c2a895c1e5251d6eb5a5cc3fabf320f0
Rafał Chabowski <rafal@casperlabs.io>
2022-06-10 10:16:27 +0200

    Split test into separate functions

--- execution_engine_testing/tests/src/test/regression/gov_42.rs
+++ execution_engine_testing/tests/src/test/regression/gov_42.rs
@@ -1,3 +1,19 @@
+// This test focuses on testing whether we charge for
+// WASM files that are malformed (unparseable).
+
+// If we're provided with such file, we should charge.
+// The expection here is the "empty wasm" send as
+// a payment, because in such case we use the "default payment"
+// instead.
+
+// For increased security, we also test some other cases in this test
+// like gas overflow (which is a runtime error).
+
+// Other potential test cases:
+// 1. Wasm that doesn't have "call" function    - tested in `regression_20210924`
+// 2. Wasm with unsupported "start" section     - tested in `ee_890` (but without asserting the
+// charge)
+
 use casper_engine_test_support::{
     DeployItemBuilder, ExecuteRequestBuilder, InMemoryWasmTestBuilder, DEFAULT_ACCOUNT_ADDR,
     DEFAULT_PAYMENT, PRODUCTION_RUN_GENESIS_REQUEST,
@@ -13,38 +29,13 @@ use crate::{
 
 const ARG_AMOUNT: &str = "amount";
 
-struct TestCase<'a> {
-    pub name: &'a str,
-    pub input_wasm_bytes: &'a [u8],
-    pub expected_error: &'a str,
-}
-
-impl<'a> TestCase<'a> {
-    fn new(name: &'a str, input_wasm_bytes: &'a [u8], expected_error: &'a str) -> Self {
-        Self {
-            name,
-            input_wasm_bytes,
-            expected_error,
-        }
-    }
-}
-
 #[derive(Copy, Clone, Debug)]
 enum ExecutionPhase {
     Payment,
     Session,
 }
 
-fn run_test_case(
-    TestCase {
-        name,
-        input_wasm_bytes,
-        expected_error,
-    }: &TestCase,
-    execution_phase: ExecutionPhase,
-) {
-    println!("Running test case '{}' in {:?}", name, execution_phase);
-
+fn run_test_case(input_wasm_bytes: &[u8], expected_error: &str, execution_phase: ExecutionPhase) {
     let payment_amount = *DEFAULT_PAYMENT;
 
     let (do_minimum_request_builder, expected_error_message) = {
@@ -121,24 +112,48 @@ fn run_test_case(
 
 #[ignore]
 #[test]
-fn should_charge_session_with_incorrect_wasm_files() {
-    // This test focuses on testing whether we charge for
-    // WASM files that are malformed (unparseable).
-
-    // If we're provided with such file, we should charge.
-    // The expection here is the "empty wasm" send as
-    // a payment, because in such case we use the "default payment"
-    // instead.
-
-    // For increased security, we also test some other cases in this test
-    // like gas overflow (which is a runtime error).
-
+fn should_charge_payment_with_incorrect_wasm_file_invalid_magic_number() {
     const WASM_BYTES_INVALID_MAGIC_NUMBER: &[u8] = &[1, 2, 3, 4, 5]; // Correct WASM magic bytes are: 0x00 0x61 0x73 0x6d ("\0asm")
+    let execution_phase = ExecutionPhase::Payment;
+    let expected_error = " Invalid magic number at start of file";
+    run_test_case(
+        WASM_BYTES_INVALID_MAGIC_NUMBER,
+        expected_error,
+        execution_phase,
+    )
+}
 
-    const WASM_BYTES_EMPTY: &[u8] = &[];
+#[ignore]
+#[test]
+fn should_charge_session_with_incorrect_wasm_file_invalid_magic_number() {
+    const WASM_BYTES: &[u8] = &[1, 2, 3, 4, 5]; // Correct WASM magic bytes are: 0x00 0x61 0x73 0x6d ("\0asm")
+    let execution_phase = ExecutionPhase::Session;
+    let expected_error = "Invalid magic number at start of file";
+    run_test_case(WASM_BYTES, expected_error, execution_phase)
+}
 
-    // These are the first few bytes of the valid WASM module.
-    const WASM_BYTES_INVALID_BUT_WITH_CORRECT_MAGIC_NUMBER: &[u8] = &[
+#[ignore]
+#[test]
+fn should_charge_payment_with_incorrect_wasm_file_empty_bytes() {
+    const WASM_BYTES: &[u8] = &[];
+    let execution_phase = ExecutionPhase::Payment;
+    let expected_error = "I/O Error: UnexpectedEof";
+    run_test_case(WASM_BYTES, expected_error, execution_phase)
+}
+
+#[ignore]
+#[test]
+fn should_charge_session_with_incorrect_wasm_file_empty_bytes() {
+    const WASM_BYTES: &[u8] = &[];
+    let execution_phase = ExecutionPhase::Session;
+    let expected_error = "I/O Error: UnexpectedEof";
+    run_test_case(WASM_BYTES, expected_error, execution_phase)
+}
+
+#[ignore]
+#[test]
+fn should_charge_payment_with_incorrect_wasm_correct_magic_number_incomplete_module() {
+    const WASM_BYTES: &[u8] = &[
         0x00, 0x61, 0x73, 0x6D, 0x01, 0x00, 0x00, 0x00, 0x01, 0x35, 0x09, 0x60, 0x02, 0x7F, 0x7F,
         0x01, 0x7F, 0x60, 0x03, 0x7F, 0x7F, 0x7F, 0x01, 0x7F, 0x60, 0x01, 0x7F, 0x00, 0x60, 0x00,
         0x00, 0x60, 0x01, 0x7F, 0x01, 0x7F, 0x60, 0x03, 0x7F, 0x7F, 0x7F, 0x00, 0x60, 0x05, 0x7F,
@@ -150,52 +165,63 @@ fn should_charge_session_with_incorrect_wasm_files() {
         0x66, 0x65, 0x72, 0x00, 0x01, 0x03, 0x65, 0x6E, 0x76, 0x0D, 0x63, 0x61, 0x73, 0x70, 0x65,
         0x72, 0x5F, 0x72, 0x65, 0x76, 0x65, 0x72, 0x74, 0x00,
     ];
+    let execution_phase = ExecutionPhase::Payment;
+    let expected_error = "I/O Error: UnexpectedEof";
+    run_test_case(WASM_BYTES, expected_error, execution_phase)
+}
 
-    let wasm_bytes_gas_overflow = make_gas_counter_overflow();
+#[ignore]
+#[test]
+fn should_charge_session_with_incorrect_wasm_correct_magic_number_incomplete_module() {
+    const WASM_BYTES: &[u8] = &[
+        0x00, 0x61, 0x73, 0x6D, 0x01, 0x00, 0x00, 0x00, 0x01, 0x35, 0x09, 0x60, 0x02, 0x7F, 0x7F,
+        0x01, 0x7F, 0x60, 0x03, 0x7F, 0x7F, 0x7F, 0x01, 0x7F, 0x60, 0x01, 0x7F, 0x00, 0x60, 0x00,
+        0x00, 0x60, 0x01, 0x7F, 0x01, 0x7F, 0x60, 0x03, 0x7F, 0x7F, 0x7F, 0x00, 0x60, 0x05, 0x7F,
+        0x7F, 0x7F, 0x7F, 0x7F, 0x01, 0x7F, 0x60, 0x02, 0x7F, 0x7F, 0x00, 0x60, 0x04, 0x7F, 0x7F,
+        0x7F, 0x7F, 0x00, 0x02, 0x50, 0x03, 0x03, 0x65, 0x6E, 0x76, 0x16, 0x63, 0x61, 0x73, 0x70,
+        0x65, 0x72, 0x5F, 0x6C, 0x6F, 0x61, 0x64, 0x5F, 0x6E, 0x61, 0x6D, 0x65, 0x64, 0x5F, 0x6B,
+        0x65, 0x79, 0x73, 0x00, 0x00, 0x03, 0x65, 0x6E, 0x76, 0x17, 0x63, 0x61, 0x73, 0x70, 0x65,
+        0x72, 0x5F, 0x72, 0x65, 0x61, 0x64, 0x5F, 0x68, 0x6F, 0x73, 0x74, 0x5F, 0x62, 0x75, 0x66,
+        0x66, 0x65, 0x72, 0x00, 0x01, 0x03, 0x65, 0x6E, 0x76, 0x0D, 0x63, 0x61, 0x73, 0x70, 0x65,
+        0x72, 0x5F, 0x72, 0x65, 0x76, 0x65, 0x72, 0x74, 0x00,
+    ];
+    let execution_phase = ExecutionPhase::Session;
+    let expected_error = "I/O Error: UnexpectedEof";
+    run_test_case(WASM_BYTES, expected_error, execution_phase)
+}
 
-    let wasm_bytes_no_memory_section = make_module_without_memory_section();
+#[ignore]
+#[test]
+fn should_charge_payment_with_incorrect_wasm_gas_counter_overflow() {
+    let wasm_bytes = make_gas_counter_overflow();
+    let execution_phase = ExecutionPhase::Payment;
+    let expected_error = "Encountered operation forbidden by gas rules";
+    run_test_case(&wasm_bytes, expected_error, execution_phase)
+}
 
-    // Other potential test cases:
-    // 1. Wasm that doesn't have "call" function    - tested in `regression_20210924`
-    // 2. Wasm with unsupported "start" section     - tested in `ee_890` (but without asserting the
-    // charge)
+#[ignore]
+#[test]
+fn should_charge_session_with_incorrect_wasm_gas_counter_overflow() {
+    let wasm_bytes = make_gas_counter_overflow();
+    let execution_phase = ExecutionPhase::Session;
+    let expected_error = "Encountered operation forbidden by gas rules";
+    run_test_case(&wasm_bytes, expected_error, execution_phase)
+}
 
-    let test_cases: &[TestCase; 5] = {
-        &[
-            TestCase::new(
-                "Invalid magic number",
-                WASM_BYTES_INVALID_MAGIC_NUMBER,
-                "Wasm preprocessing error: Deserialization error: Invalid magic number at start of file",
-            ),
-            TestCase::new(
-                "Empty bytes",
-                WASM_BYTES_EMPTY,
-                "Wasm preprocessing error: Deserialization error: I/O Error: UnexpectedEof"
-                    ,
-            ),
-            TestCase::new(
-                "Valid magic number but incomplete bytes",
-                WASM_BYTES_INVALID_BUT_WITH_CORRECT_MAGIC_NUMBER,
-                "I/O Error: UnexpectedEof",
-            ),
-            TestCase::new(
-                "Gas overflow",
-                wasm_bytes_gas_overflow.as_slice(),
-                "Wasm preprocessing error: Encountered operation forbidden by gas rules. Consult instruction -> metering config map",
-            ),
-            TestCase::new(
-                "Wasm w/o memory section",
-                wasm_bytes_no_memory_section.as_slice(),
-                "Memory section should exist",
-            ),
-        ]
-    };
+#[ignore]
+#[test]
+fn should_charge_payment_with_incorrect_wasm_no_memory_section() {
+    let wasm_bytes = make_module_without_memory_section();
+    let execution_phase = ExecutionPhase::Payment;
+    let expected_error = "Memory section should exist";
+    run_test_case(&wasm_bytes, expected_error, execution_phase)
+}
 
-    [ExecutionPhase::Payment, ExecutionPhase::Session]
-        .iter()
-        .for_each(|deploy_bytes| {
-            test_cases
-                .iter()
-                .for_each(|test_case| run_test_case(test_case, *deploy_bytes))
-        })
+#[ignore]
+#[test]
+fn should_charge_session_with_incorrect_wasm_no_memory_section() {
+    let wasm_bytes = make_module_without_memory_section();
+    let execution_phase = ExecutionPhase::Session;
+    let expected_error = "Memory section should exist";
+    run_test_case(&wasm_bytes, expected_error, execution_phase)
 }
