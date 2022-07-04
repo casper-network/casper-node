72bdbe20a26da3a97332b601820f1e0dd2e00772
Bartłomiej Kamiński <bart@casperlabs.io>
2022-07-04 16:15:34 +0200

    Make transfers and accounts optional in config

--- utils/global-state-update-gen/src/generic/config.rs
+++ utils/global-state-update-gen/src/generic/config.rs
@@ -4,7 +4,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
+    #[serde(default)]
     pub transfers: Vec<Transfer>,
+    #[serde(default)]
     pub accounts: Vec<AccountConfig>,
     #[serde(default)]
     pub only_listed_validators: bool,
