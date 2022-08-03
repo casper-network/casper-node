6823aa647a2de743a94dff2760590a2effdc1f32
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2022-08-02 21:17:46 -0400

    Bumping EE, casper-node and assembly script (to keep from erroring on publish).

--- node/src/components/rpc_server/rpcs/docs.rs
+++ node/src/components/rpc_server/rpcs/docs.rs
@@ -28,7 +28,7 @@ use super::{
 use crate::effect::EffectBuilder;
 
 pub(crate) const DOCS_EXAMPLE_PROTOCOL_VERSION: ProtocolVersion =
-    ProtocolVersion::from_parts(1, 4, 6);
+    ProtocolVersion::from_parts(1, 4, 7);
 
 const DEFINITIONS_PATH: &str = "#/components/schemas/";
 
