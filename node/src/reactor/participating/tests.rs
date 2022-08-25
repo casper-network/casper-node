ac72aa39d469d02a2103b1e2818009588049b33d
Bartłomiej Kamiński <bart@casperlabs.io>
2022-08-25 17:29:33 +0200

    Allow nodes in run_participating_network more time to start up

--- node/src/reactor/participating/tests.rs
+++ node/src/reactor/participating/tests.rs
@@ -103,8 +103,8 @@ impl TestChain {
         let delegators = vec![];
         chainspec.network_config.accounts_config = AccountsConfig::new(accounts, delegators);
 
-        // Make the genesis timestamp 45 seconds from now, to allow for all validators to start up.
-        let genesis_time = Timestamp::now() + 45000.into();
+        // Make the genesis timestamp 60 seconds from now, to allow for all validators to start up.
+        let genesis_time = Timestamp::now() + 60000.into();
         info!(
             "creating test chain configuration, genesis: {}",
             genesis_time
