f13cc628c1ead995936ad2c6b612a8b67f6e0660
Rafał Chabowski <rafal@casperlabs.io>
2022-11-09 11:24:09 +0100

    Remove unused variants from `BlocklistJustification`

--- node/src/components/small_network/blocklist.rs
+++ node/src/components/small_network/blocklist.rs
@@ -11,34 +11,15 @@ use serde::Serialize;
 
 use crate::{
     components::block_accumulator,
-    types::{BlockHash, BlockHeader, DeployId, Tag},
-    utils::BlockSignatureError,
+    types::{DeployId, Tag},
 };
 
 /// Reasons why a peer was blocked.
 #[derive(DataSize, Debug, Serialize)]
 pub(crate) enum BlocklistJustification {
-    /// Peer sent an item that was not parseable at all.
+    /// Peer sent incorrect item.
     SentBadItem { tag: Tag },
     /// Received a block with incorrect parent, which was specified beforehand.
-    SentBlockWithWrongParent {
-        /// The header that was received.
-        received_header: BlockHeader,
-        /// The parent that was expected.
-        expected_parent_header: BlockHeader,
-    },
-    /// A block header or block was received without the appropriate finality signatures.
-    MissingBlockSignatures {
-        /// Received header/header of block.
-        received_header: BlockHeader,
-    },
-    /// A peer sent a bogus block signature
-    SentSignatureWithBogusValidator {
-        /// The actual validation error.
-        #[serde(skip_serializing)]
-        #[data_size(skip)]
-        error: BlockSignatureError,
-    },
     /// A finality signature that was sent is invalid.
     SentBadFinalitySignature {
         /// The actual cryptographic validation error.
@@ -53,29 +34,6 @@ pub(crate) enum BlocklistJustification {
         #[data_size(skip)]
         error: block_accumulator::Error,
     },
-    /// A received block with signatures failed validation.
-    SentBlockWithInvalidFinalitySignatures {
-        /// The block header for which invalid signatures were received.
-        block_header: BlockHeader,
-        /// The block signature error.
-        #[serde(skip_serializing)]
-        #[data_size(skip)]
-        error: BlockSignatureError,
-    },
-    /// A received block did not yield the expected execution results.
-    SentBlockThatExecutedIncorrectly {
-        /// The hash of the received block.
-        block_hash: BlockHash,
-    },
-    /// An item received failed to validation.
-    SentInvalidItem {
-        /// The tag of the item received.
-        tag: Tag,
-        /// The ID of item, as a human-readable string.
-        item_id: String,
-        /// The validation error, as a human-readable string.
-        error: String,
-    },
     /// A serialized deploy was received that failed to deserialize.
     SentBadDeploy {
         /// The deserialization error.
@@ -83,8 +41,6 @@ pub(crate) enum BlocklistJustification {
         #[data_size(skip)]
         error: bincode::Error,
     },
-    /// A network address was received that should only be received via direct gossip.
-    SentGossipedAddress,
     /// An invalid consensus value was received.
     SentInvalidConsensusValue {
         /// The era for which the invalid value was destined.
@@ -123,54 +79,13 @@ impl Display for BlocklistJustification {
             BlocklistJustification::SentBadItem { tag } => {
                 write!(f, "sent a {:?} item we couldn't parse", tag)
             }
-            BlocklistJustification::SentBlockWithWrongParent {
-                received_header,
-                expected_parent_header,
-            } => write!(
-                f,
-                "sent block (header: {:?}) with wrong parent ({:?})",
-                received_header, expected_parent_header,
-            ),
-            BlocklistJustification::MissingBlockSignatures { received_header } => write!(
-                f,
-                "sent no block signatures along with header {:?}",
-                received_header
-            ),
-            BlocklistJustification::SentSignatureWithBogusValidator { error } => {
-                write!(f, "sent signature with bogus validator ({})", error)
-            }
             BlocklistJustification::SentBadFinalitySignature { error } => write!(
                 f,
                 "sent cryptographically invalid finality signature: {}",
                 error
             ),
-            BlocklistJustification::SentBlockWithInvalidFinalitySignatures {
-                block_header,
-                error,
-            } => write!(
-                f,
-                "sent sent invalid finality signatures for block {:?}: {}",
-                block_header, error
-            ),
-            BlocklistJustification::SentBlockThatExecutedIncorrectly { block_hash } => write!(
-                f,
-                "sent block {}, but the execution of the block did not yield the expected results",
-                block_hash
-            ),
-            BlocklistJustification::SentInvalidItem {
-                tag,
-                item_id,
-                error,
-            } => write!(
-                f,
-                "sent item {} of type {:?} that was invalid: {}",
-                item_id, tag, error
-            ),
             BlocklistJustification::SentBadDeploy { error } => {
                 write!(f, "sent a deploy that could not be deserialized: {}", error)
-            }
-            BlocklistJustification::SentGossipedAddress => {
-                f.write_str("sent a network address via response, which is only ever gossiped")
             }
             BlocklistJustification::SentInvalidConsensusValue { era } => {
                 write!(f, "sent an invalid consensus value in {}", era)
