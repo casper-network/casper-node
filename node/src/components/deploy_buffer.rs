22f215435f0172d3bab4d36ca56b16a3c089f2b8
Ed Hastings <ed@casperlabs.io>
2022-11-20 14:37:40 -0800

    tweaking deploy_buffer

--- node/src/components/deploy_buffer.rs
+++ node/src/components/deploy_buffer.rs
@@ -3,11 +3,12 @@ mod event;
 
 use std::{
     collections::{BTreeMap, BTreeSet, HashMap, HashSet},
+    iter::FromIterator,
     mem,
 };
 
 use datasize::DataSize;
-use tracing::{debug, error, warn};
+use tracing::{debug, error, info, warn};
 
 use casper_types::Timestamp;
 
@@ -55,10 +56,12 @@ pub(crate) struct DeployBuffer {
     hold: BTreeMap<Timestamp, HashSet<DeployHash>>,
     // deploy_hashes that should not be proposed, ever
     dead: HashSet<DeployHash>,
-    // block_height and block_time of blocks added to the local chain
-    // needed to ensure we have seen sufficient blocks
-    // to allow consensus to proceed safely without risking
-    // duplicating deploys
+    // block_height and block_time of blocks added to the local chain needed to ensure we have seen
+    // sufficient blocks to allow consensus to proceed safely without risking duplicating deploys.
+    //
+    // if we have gaps in block_height, we do NOT have full TTL awareness.
+    // if we have no block_height gaps but our earliest block_time is less than now - ttl,
+    // we do NOT have full TTL awareness.
     chain_index: BTreeMap<u64, Timestamp>,
 }
 
@@ -76,7 +79,7 @@ impl DeployBuffer {
         }
     }
 
-    // do you have full TTL worth of deploy awareness
+    /// True if we have full TTL worth of deploy awareness, else false.
     pub(crate) fn have_full_ttl_of_deploys(&self, from_height: u64) -> bool {
         let ttl = self.deploy_config.max_ttl;
         let mut curr = match self.chain_index.get(&from_height) {
@@ -86,6 +89,7 @@ impl DeployBuffer {
             Some(timestamp) => (from_height, timestamp),
         };
 
+        // genesis special case
         if from_height == 0 {
             return true;
         }
@@ -102,6 +106,7 @@ impl DeployBuffer {
         false
     }
 
+    /// Manages cache ejection.
     fn expire<REv>(&mut self, effect_builder: EffectBuilder<REv>) -> Effects<Event>
     where
         REv: From<Event> + From<DeployBufferAnnouncement> + Send,
@@ -111,11 +116,12 @@ impl DeployBuffer {
             .into_iter()
             .partition(|(_, (expiry_time, _))| *expiry_time >= now);
 
+        debug!("DeployBuffer: expiring {} deploys", freed.len());
         // clear expired deploy from all holds, then clear any entries that have no items remaining
         self.hold.iter_mut().for_each(|(_, held_deploys)| {
             held_deploys.retain(|deploy_hash| !freed.contains_key(deploy_hash))
         });
-        self.hold.retain(|_, v| !v.is_empty());
+        self.hold.retain(|_, remaining| !remaining.is_empty());
 
         self.dead
             .retain(|deploy_hash| !freed.contains_key(deploy_hash));
@@ -140,92 +146,111 @@ impl DeployBuffer {
     where
         REv: From<Event> + From<StorageRequest> + Send,
     {
+        debug!(%deploy_id, "DeployBuffer: registering gossied deploy");
         effect_builder
             .get_stored_deploy(deploy_id)
-            .event(move |result| Event::GotDeployFromStorage(Box::new(result)))
+            .event(move |result| Event::StoredDeploy(deploy_id, Box::new(result)))
     }
 
-    fn register_deploy(&mut self, maybe_deploy: Option<Deploy>) {
-        let deploy = match maybe_deploy {
-            None => {
-                error!("received gossip finished notification for deploy which is not available in storage");
-                return;
-            }
-            Some(deploy) => deploy,
-        };
+    /// Update buffer considering new stored deploy.
+    fn register_deploy(&mut self, deploy: Deploy) {
         let deploy_hash = deploy.hash();
         if deploy.is_valid().is_err() {
-            error!(?deploy_hash, "invalid deploy must not be buffered");
+            error!(%deploy_hash, "DeployBuffer: invalid deploy must not be buffered");
             return;
         }
         if self.dead.contains(deploy_hash) {
-            debug!(?deploy_hash, "attempt to register already dead deploy");
+            info!(%deploy_hash, "DeployBuffer: attempt to register already dead deploy");
             return;
         }
         if self.hold.values().any(|dhs| dhs.contains(deploy_hash)) {
-            debug!(?deploy_hash, "attempt to register already held deploy");
+            info!(%deploy_hash, "DeployBuffer: attempt to register already held deploy");
             return;
         }
         let footprint = match deploy.footprint() {
             Ok(footprint) => footprint,
             Err(err) => {
-                error!(%deploy_hash, %err, "tried to register invalid deploy");
+                error!(%deploy_hash, %err, "DeployBuffer: deploy footprint exceeds tolerances");
                 return;
             }
         };
         let expiry_time = deploy.header().expires();
         let approvals = deploy.approvals().clone();
-        self.buffer
-            .insert(*deploy_hash, (expiry_time, Some((footprint, approvals))));
-    }
-
-    fn register_block_proposed(&mut self, proposed_block: ProposedBlock<ClContext>) {
-        if let Some(hold_set) = self.hold.get_mut(&proposed_block.context().timestamp()) {
-            hold_set.extend(proposed_block.value().deploy_and_transfer_hashes())
+        match self
+            .buffer
+            .insert(*deploy_hash, (expiry_time, Some((footprint, approvals))))
+        {
+            Some(prev) => {
+                warn!(%deploy_hash, ?prev, "DeployBuffer: deploy upserted");
+            }
+            None => {
+                debug!(%deploy_hash, "DeployBuffer: new deploy buffered"); // todo!("put this to trace! after testing")
+            }
         }
     }
 
+    /// Update holds considering new proposed block.
+    fn register_block_proposed(&mut self, proposed_block: ProposedBlock<ClContext>) {
+        let timestamp = &proposed_block.context().timestamp();
+        if let Some(hold_set) = self.hold.get_mut(timestamp) {
+            debug!(%timestamp, "DeployBuffer: existing hold timestamp upserted");
+            hold_set.extend(proposed_block.value().deploy_and_transfer_hashes())
+        } else {
+            debug!(%timestamp, "DeployBuffer: new hold timestamp inserted");
+            self.hold.insert(
+                *timestamp,
+                HashSet::from_iter(proposed_block.value().deploy_and_transfer_hashes().copied()),
+            );
+        }
+    }
+
+    /// Update buffer and holds considering new finalized block.
     fn register_block_finalized(&mut self, finalized_block: &FinalizedBlock) {
-        self.chain_index
-            .insert(finalized_block.height(), finalized_block.timestamp());
+        let timestamp = finalized_block.timestamp();
+        self.chain_index.insert(finalized_block.height(), timestamp);
+        let expiry_timestamp = timestamp.saturating_add(self.deploy_config.max_ttl);
         // all deploys in the finalized block must not be included in future proposals
         for hash in finalized_block.deploy_and_transfer_hashes() {
             if !self.buffer.contains_key(hash) {
-                let expiry_time = finalized_block
-                    .timestamp()
-                    .saturating_add(self.deploy_config.max_ttl);
-                self.buffer.insert(*hash, (expiry_time, None));
+                self.buffer.insert(*hash, (expiry_timestamp, None));
             }
             self.dead.insert(*hash);
         }
         // deploys held for proposed blocks which did not get finalized in time are eligible again
         let (hold, _) = mem::take(&mut self.hold)
             .into_iter()
-            .partition(|(timestamp, _)| *timestamp > finalized_block.timestamp());
+            .partition(|(ts, _)| *ts > timestamp);
+        debug!(%timestamp, "DeployBuffer: timestamp finalized");
         self.hold = hold;
     }
 
-    fn proposable(&self) -> Vec<(DeployHash, DeployFootprint, BTreeSet<Approval>)> {
-        // a deploy hash that is not in dead or hold is proposable
+    /// Returns eligible deploys that are buffered and not held or dead.
+    fn proposable(&self) -> Vec<(DeployHashWithApprovals, DeployFootprint)> {
+        debug!("DeployBuffer: getting proposable deploys");
         self.buffer
             .iter()
-            .filter(|(k, _)| !self.hold.values().any(|hs| hs.contains(k)))
-            .filter(|(k, _)| !self.dead.contains(k))
-            .filter_map(|(k, (_, maybe_data))| {
-                maybe_data
-                    .as_ref()
-                    .map(|(footprint, approvals)| (*k, footprint.clone(), approvals.clone()))
+            .filter(|(dh, _)| !self.hold.values().any(|hs| hs.contains(dh)))
+            .filter(|(dh, _)| !self.dead.contains(dh))
+            .filter_map(|(dh, (_, maybe_data))| {
+                maybe_data.as_ref().map(|(footprint, approvals)| {
+                    (
+                        DeployHashWithApprovals::new(*dh, approvals.clone()),
+                        footprint.clone(),
+                    )
+                })
             })
             .collect()
     }
 
+    /// Returns a right-sized payload of deploys that can be proposed.
     fn appendable_block(&mut self, timestamp: Timestamp) -> AppendableBlock {
         let mut ret = AppendableBlock::new(self.deploy_config, timestamp);
         let mut holds = vec![];
-        for (deploy_hash, footprint, approvals) in self.proposable() {
-            let with_approvals = DeployHashWithApprovals::new(deploy_hash, approvals);
+        for (with_approvals, footprint) in self.proposable() {
+            let deploy_hash = *with_approvals.deploy_hash();
             match ret.add(with_approvals, &footprint) {
                 Ok(_) => {
+                    debug!(%deploy_hash, "DeployBuffer: proposing deploy");
                     holds.push(deploy_hash);
                 }
                 Err(error) => {
@@ -233,14 +258,20 @@ impl DeployBuffer {
                         AddError::Duplicate => {
                             // it should be physically impossible for a duplicate deploy to
                             // be in the deploy buffer, thus this should be unreachable
-                            debug!(?deploy_hash, "duplicated deploy in deploy buffer");
+                            error!(
+                                ?deploy_hash,
+                                "DeployBuffer: duplicated deploy in deploy buffer"
+                            );
                             self.dead.insert(deploy_hash);
                             continue;
                         }
                         AddError::InvalidDeploy => {
                             // it should not be possible for an invalid deploy to get buffered
                             // in the first place, thus this should be unreachable
-                            debug!(?deploy_hash, "invalid deploy in deploy buffer");
+                            error!(
+                                ?deploy_hash,
+                                "DeployBuffer: invalid deploy in deploy buffer"
+                            );
                             self.dead.insert(deploy_hash);
                             continue;
                         }
@@ -249,7 +280,12 @@ impl DeployBuffer {
                         | AddError::ApprovalCount
                         | AddError::GasLimit
                         | AddError::BlockSize => {
-                            // one or more block limits have been reached
+                            debug!(
+                                ?deploy_hash,
+                                %error,
+                                "DeployBuffer: a block limit has been reached"
+                            );
+                            // a block limit has been reached
                             break;
                         }
                     }
@@ -290,57 +326,64 @@ where
     ) -> Effects<Self::Event> {
         match (&self.status, event) {
             (ComponentStatus::Fatal(msg), _) => {
-                error!(
+                warn!(
                     msg,
                     "should not handle this event when this component has fatal error"
                 );
                 Effects::new()
             }
-            (ComponentStatus::Uninitialized, Event::Initialize(blocks)) => {
-                for block in blocks {
-                    self.register_block_finalized(&block.into());
+            (ComponentStatus::Uninitialized, evt) => {
+                if let Event::Initialize(blocks) = evt {
+                    for block in blocks {
+                        self.register_block_finalized(&block.into());
+                    }
+                    self.status = ComponentStatus::Initialized;
+                    // start self-expiry management on initialization
+                    effect_builder
+                        .set_timeout(self.cfg.expiry_check_interval().into())
+                        .event(move |_| Event::Expire)
+                } else {
+                    warn!("should not handle this event when component is uninitialized");
+                    Effects::new()
                 }
-                self.status = ComponentStatus::Initialized;
-                // start self-expiry management on initialization
-                effect_builder
-                    .set_timeout(self.cfg.expiry_check_interval().into())
-                    .event(move |_| Event::Expire)
             }
-            (ComponentStatus::Uninitialized, _) => {
-                warn!("should not handle this event when component is uninitialized");
-                Effects::new()
-            }
-            (ComponentStatus::Initialized, Event::Initialize(_)) => {
-                error!("deploy buffer already initialized");
-                Effects::new()
-            }
-            (
-                ComponentStatus::Initialized,
+            (ComponentStatus::Initialized, evt) => match evt {
+                Event::Initialize(_) => {
+                    warn!("deploy buffer already initialized");
+                    Effects::new()
+                }
                 Event::Request(DeployBufferRequest::GetAppendableBlock {
                     timestamp,
                     responder,
-                }),
-            ) => responder.respond(self.appendable_block(timestamp)).ignore(),
-            (ComponentStatus::Initialized, Event::BlockFinalized(finalized_block)) => {
-                self.register_block_finalized(&*finalized_block);
-                Effects::new()
-            }
-            (ComponentStatus::Initialized, Event::Block(block)) => {
-                self.register_block_finalized(&FinalizedBlock::from(*block));
-                Effects::new()
-            }
-            (ComponentStatus::Initialized, Event::BlockProposed(proposed)) => {
-                self.register_block_proposed(*proposed);
-                Effects::new()
-            }
-            (ComponentStatus::Initialized, Event::ReceiveDeployGossiped(deploy_id)) => {
-                self.register_deploy_gossiped(deploy_id, effect_builder)
-            }
-            (ComponentStatus::Initialized, Event::GotDeployFromStorage(maybe_deploy)) => {
-                self.register_deploy(*maybe_deploy);
-                Effects::new()
-            }
-            (ComponentStatus::Initialized, Event::Expire) => self.expire(effect_builder),
+                }) => responder.respond(self.appendable_block(timestamp)).ignore(),
+                Event::BlockFinalized(finalized_block) => {
+                    self.register_block_finalized(&*finalized_block);
+                    Effects::new()
+                }
+                Event::Block(block) => {
+                    self.register_block_finalized(&FinalizedBlock::from(*block));
+                    Effects::new()
+                }
+                Event::BlockProposed(proposed) => {
+                    self.register_block_proposed(*proposed);
+                    Effects::new()
+                }
+                Event::ReceiveDeployGossiped(deploy_id) => {
+                    self.register_deploy_gossiped(deploy_id, effect_builder)
+                }
+                Event::StoredDeploy(deploy_id, maybe_deploy) => {
+                    match *maybe_deploy {
+                        Some(deploy) => {
+                            self.register_deploy(deploy);
+                        }
+                        None => {
+                            warn!("cannot register un-stored deploy({})", deploy_id);
+                        }
+                    }
+                    Effects::new()
+                }
+                Event::Expire => self.expire(effect_builder),
+            },
         }
     }
 }
