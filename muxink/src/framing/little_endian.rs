c9e2c958d05797f5787fb238792804efb2ba9e78
Marc Brinkmann <marc@casperlabs.io>
2023-02-11 18:12:47 +0100

    muxink: Add testcases for `LittleEndian` frame encoding

--- muxink/src/framing/little_endian.rs
+++ muxink/src/framing/little_endian.rs
@@ -63,3 +63,64 @@ int_codec!(i16);
 int_codec!(i32);
 int_codec!(i64);
 int_codec!(i128);
+
+#[cfg(test)]
+mod tests {
+    use futures::io::Cursor;
+
+    use crate::{framing::FrameEncoder, io::FrameReader, testing::collect_stream_results};
+
+    use super::LittleEndian;
+
+    /// Decodes the input string, returning the decoded frames and the remainder.
+    fn run_decoding_stream(input: &[u8], chomp_size: usize) -> (Vec<Vec<u8>>, Vec<u8>) {
+        let stream = Cursor::new(input);
+
+        let mut reader = FrameReader::new(LittleEndian::<u32>::default(), stream, chomp_size);
+
+        let decoded: Vec<_> = collect_stream_results(&mut reader)
+            .into_iter()
+            .map(|bytes| bytes.into_iter().collect::<Vec<u8>>())
+            .collect();
+
+        // Extract the remaining data.
+        let (_decoder, cursor, buffer) = reader.into_parts();
+        let mut remaining = Vec::new();
+        remaining.extend(buffer.into_iter());
+        let cursor_pos = cursor.position() as usize;
+        remaining.extend(&cursor.into_inner()[cursor_pos..]);
+
+        (decoded, remaining)
+    }
+
+    #[test]
+    fn simple_stream_decoding_works() {
+        for chomp_size in 1..=1024 {
+            let input = b"\x01\x02\x03\x04\xAA\xBB\xCC\xDD";
+            let (decoded, remainder) = run_decoding_stream(input, chomp_size);
+            assert_eq!(decoded, &[b"\x01\x02\x03\x04", b"\xAA\xBB\xCC\xDD"]);
+            assert!(remainder.is_empty());
+        }
+    }
+
+    #[test]
+    fn empty_stream_is_empty() {
+        let input = b"";
+
+        let (decoded, remainder) = run_decoding_stream(input, 3);
+        assert!(decoded.is_empty());
+        assert!(remainder.is_empty());
+    }
+
+    #[test]
+    fn encodes_simple_cases_correctly() {
+        let seq = [0x01020304u32, 0xAABBCCDD];
+        let outcomes: &[&[u8]] = &[b"\x04\x03\x02\x01", b"\xDD\xCC\xBB\xAA"];
+
+        for (input, expected) in seq.into_iter().zip(outcomes.into_iter()) {
+            let mut codec = LittleEndian::<u32>::default();
+            let outcome = codec.encode_frame(input).expect("encoding should not fail");
+            assert_eq!(outcome.get_ref(), *expected);
+        }
+    }
+}
