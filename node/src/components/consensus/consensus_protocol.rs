8b62cbc551624b58918bf4f94ab530bbb4e03c55
Bartłomiej Kamiński <bart@casperlabs.io>
2023-03-07 04:38:49 +1300

    Add DES tests for Zug

--- node/src/components/consensus/consensus_protocol.rs
+++ node/src/components/consensus/consensus_protocol.rs
@@ -159,7 +159,7 @@ impl<VID> EraReport<VID> {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub(crate) struct TerminalBlockData<C: Context> {
     /// The list of validators that haven't produced any units.
     pub(crate) inactive_validators: Vec<C::ValidatorId>,
@@ -168,7 +168,7 @@ pub(crate) struct TerminalBlockData<C: Context> {
 /// A finalized block. All nodes are guaranteed to see the same sequence of blocks, and to agree
 /// about all the information contained in this type, as long as the total weight of faulty
 /// validators remains below the threshold.
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub(crate) struct FinalizedBlock<C: Context> {
     /// The finalized value.
     pub(crate) value: C::ConsensusValue,
--- node/src/components/consensus/protocols/zug/message.rs
+++ node/src/components/consensus/protocols/zug/message.rs
@@ -60,7 +60,9 @@ mod relaxed {
     }
 
     /// All messages of the protocol.
-    #[derive(DataSize, Clone, Serialize, Deserialize, Debug, PartialEq, Eq, EnumDiscriminants)]
+    #[derive(
+        DataSize, Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Hash, EnumDiscriminants,
+    )]
     #[serde(bound(
         serialize = "C::Hash: Serialize",
         deserialize = "C::Hash: Deserialize<'de>",
@@ -107,7 +109,7 @@ impl<C: Context> Content<C> {
 impl<C: Context> Copy for Content<C> {}
 
 /// A vote or echo with a signature.
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq, DataSize)]
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Hash, DataSize)]
 #[serde(bound(
     serialize = "C::Hash: Serialize",
     deserialize = "C::Hash: Deserialize<'de>",
@@ -190,7 +192,7 @@ impl<C: Context> SignedMessage<C> {
 ///
 /// For example if there are 500 validators and `first_validator_idx` is 450, the `u128`'s bits
 /// refer to validators 450, 451, ..., 499, 0, 1, ..., 77.
-#[derive(DataSize, Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[derive(DataSize, Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
 #[serde(bound(
     serialize = "C::Hash: Serialize",
     deserialize = "C::Hash: Deserialize<'de>",
@@ -246,7 +248,7 @@ impl<C: Context> SyncRequest<C> {
 
 /// The response to a `SyncRequest`, containing proposals, signatures and evidence the requester is
 /// missing.
-#[derive(DataSize, Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[derive(DataSize, Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
 #[serde(bound(
     serialize = "C::Hash: Serialize",
     deserialize = "C::Hash: Deserialize<'de>",
