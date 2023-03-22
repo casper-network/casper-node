5ba9b627843b46930f276f52cc9e35e7d88330e7
Marc Brinkmann <marc@casperlabs.io>
2023-03-22 12:39:11 +0100

    Add `enum` support for `#[derive(ToBytes)]`

--- bytesrepr_derive/src/lib.rs
+++ bytesrepr_derive/src/lib.rs
@@ -1,19 +1,21 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{DataStruct, Ident};
+use syn::{DataEnum, DataStruct, Ident};
 
+/// Top-level proc macro for `#[derive(ToBytes)]`.
 #[proc_macro_derive(ToBytes)]
 pub fn derive_to_bytes(tokens: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput = syn::parse(tokens).unwrap();
 
     match ast.data {
         syn::Data::Struct(st) => derive_to_bytes_for_struct(ast.ident, st),
-        syn::Data::Enum(_) => panic!("enums are not supported by bytesrepr_derive"),
+        syn::Data::Enum(en) => derive_to_bytes_for_enum(ast.ident, en),
         syn::Data::Union(_) => panic!("unions are not supported by bytesrepr_derive"),
     }
 }
 
+/// Top-level proc macro for `#[derive(FromBytes)]`.
 #[proc_macro_derive(FromBytes)]
 pub fn derive_from_bytes(tokens: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput = syn::parse(tokens).unwrap();
@@ -25,11 +27,11 @@ pub fn derive_from_bytes(tokens: TokenStream) -> TokenStream {
     }
 }
 
+/// Proc macro for `#[derive(ToBytes)]` for `struct`s.
 fn derive_to_bytes_for_struct(st_name: Ident, st: DataStruct) -> TokenStream {
     let mut fields_serialization = proc_macro2::TokenStream::new();
     let mut length_calculation = proc_macro2::TokenStream::new();
 
-    // Fields encoding.
     match st.fields {
         syn::Fields::Named(ref fields) => {
             for field in &fields.named {
@@ -40,8 +42,6 @@ fn derive_to_bytes_for_struct(st_name: Ident, st: DataStruct) -> TokenStream {
                 length_calculation.extend(quote!(
                     + ::casper_types::bytesrepr::ToBytes::serialized_length(&self.#ident)));
             }
-
-            quote!(buffer.extend())
         }
         syn::Fields::Unnamed(ref fields) => {
             for idx in 0..(fields.unnamed.len()) {
@@ -53,8 +53,6 @@ fn derive_to_bytes_for_struct(st_name: Ident, st: DataStruct) -> TokenStream {
                 length_calculation.extend(quote!(
                     + ::casper_types::bytesrepr::ToBytes::serialized_length(&self.#lit)));
             }
-
-            quote!(buffer.extend())
         }
         // TODO: Do we (want to) support zero-sized types?
         syn::Fields::Unit => panic!("unit structs are not supported by bytesrepr_derive"),
@@ -82,6 +80,7 @@ fn derive_to_bytes_for_struct(st_name: Ident, st: DataStruct) -> TokenStream {
     rv.into()
 }
 
+/// Proc macro for `#[derive(FromBytes)]` for `struct`s.
 fn derive_from_bytes_for_struct(st_name: Ident, st: DataStruct) -> TokenStream {
     let mut fields_deserialization = proc_macro2::TokenStream::new();
     let mut fields_assignments = proc_macro2::TokenStream::new();
@@ -128,6 +127,106 @@ fn derive_from_bytes_for_struct(st_name: Ident, st: DataStruct) -> TokenStream {
         }
     };
 
+    eprintln!("{}", rv);
+
+    rv.into()
+}
+
+/// Proc macro for `#[derive(ToBytes)]` for `struct`s.
+fn derive_to_bytes_for_enum(en_name: Ident, en: DataEnum) -> TokenStream {
+    let mut to_bytes_variant_match = proc_macro2::TokenStream::new();
+    let mut length_variant_match = proc_macro2::TokenStream::new();
+
+    for (idx, variant) in en.variants.iter().enumerate() {
+        assert!(
+            idx <= u8::MAX as usize,
+            "cannot serialize this many variants"
+        );
+
+        if variant.discriminant.is_some() {
+            // TODO: Decide whether we want to change the encoding based on these variant
+            //       assignments or not. For now, its best to not do either.
+            panic!("explicit discriminants are currently not supported by bytesrepr_derive");
+        }
+
+        let mut fields_serialization = proc_macro2::TokenStream::new();
+        let mut length_calculation = proc_macro2::TokenStream::new();
+
+        let variant_ident = &variant.ident;
+        match variant.fields {
+            syn::Fields::Named(ref fields) => {
+                for field in &fields.named {
+                    let ident = field.ident.as_ref().unwrap();
+                    fields_serialization.extend(quote!(
+                        buffer.extend(::casper_types::bytesrepr::ToBytes::to_bytes(#ident)?);
+                    ));
+                    length_calculation.extend(quote!(
+                        + ::casper_types::bytesrepr::ToBytes::serialized_length(#ident)));
+                }
+
+                let field_names: Vec<_> = fields.named.iter().map(|f| f.ident.clone()).collect();
+                to_bytes_variant_match.extend(quote! {
+                    Self::#variant_ident { #(#field_names),* } => {
+                        #fields_serialization
+                    }
+                });
+
+                length_variant_match.extend(quote! {
+                    Self::#variant_ident { #(#field_names),* } => {
+                        0 #length_calculation
+                    }
+                });
+            }
+            syn::Fields::Unnamed(ref fields) => {
+                let mut field_names = Vec::new();
+                for idx in 0..(fields.unnamed.len()) {
+                    let idx_ident = Ident::new(&format!("field_{}", idx), en_name.span());
+                    fields_serialization.extend(quote!(
+                        buffer.extend(::casper_types::bytesrepr::ToBytes::to_bytes(#idx_ident)?);
+                    ));
+                    length_calculation.extend(quote!(
+                        + ::casper_types::bytesrepr::ToBytes::serialized_length(#idx_ident)));
+
+                    field_names.push(idx_ident);
+                }
+
+                to_bytes_variant_match.extend(quote! {
+                    Self::#variant_ident ( #(#field_names),* ) => {
+                        #fields_serialization
+                    }
+                });
+
+                length_variant_match.extend(quote! {
+                    Self::#variant_ident ( #(#field_names),* ) => {
+                        0 #length_calculation
+                    }
+                });
+            }
+            // TODO: Do we (want to) support zero-sized types?
+            syn::Fields::Unit => panic!("unit structs are not supported by bytesrepr_derive"),
+        }
+    }
+
+    let rv = quote!(
+        impl ::casper_types::bytesrepr::ToBytes for #en_name {
+            fn to_bytes(&self) -> Result<Vec<u8>, ::casper_types::bytesrepr::Error> {
+                let mut buffer = ::casper_types::bytesrepr::allocate_buffer(self)?;
+                match self {
+                    #to_bytes_variant_match
+                }
+                Ok(buffer)
+            }
+
+            fn serialized_length(&self) -> usize {
+                1 + match self {
+                    #length_variant_match
+                }
+            }
+
+            // TODO: into_bytes
+            // TODO: write_bytes
+        }
+    );
     eprintln!("{}", rv);
 
     rv.into()
--- bytesrepr_derive/tests/struct_derive.rs
+++ bytesrepr_derive/tests/struct_derive.rs
@@ -13,6 +13,13 @@ struct MyNamedStruct {
 #[derive(FromBytes, ToBytes, Debug, PartialEq)]
 struct MyUnnamedStruct(u64, u32, String);
 
+#[derive(ToBytes, Debug, PartialEq)]
+// #[derive(FromBytes, ToBytes, Debug, PartialEq)]
+enum MyEnum {
+    MyNamedVariant { a: u64, b: u32, c: String },
+    MyUnnamedVariant(u64, u32, String),
+}
+
 fn roundtrip_value<T>(value: T)
 where
     T: FromBytes + ToBytes + PartialEq + Debug,
@@ -37,3 +44,12 @@ fn roundtrip_named() {
 fn roundtrip_unnamed() {
     roundtrip_value(MyUnnamedStruct(123, 456, "hello, world".to_owned()));
 }
+
+// #[test]
+// fn roundtrip_enum_named() {
+//     roundtrip_value(MyEnum::MyNamedVariant {
+//         a: 123,
+//         b: 456,
+//         c: "hello, world".to_owned(),
+//     });
+// }
