3b3a471033a013f11b2e5c853693375062c92741
Michał Papierski <michal@casperlabs.io>
2023-03-30 17:12:35 +0200

    Remove commented out code

--- execution_engine/src/core/engine_state/purge.rs
+++ execution_engine/src/core/engine_state/purge.rs
@@ -43,29 +43,3 @@ impl PurgeConfig {
         &self.keys_to_delete
     }
 }
-
-// /// Represents outcomes of a failed protocol upgrade.
-// #[derive(Clone, Error, Debug)]
-// pub enum PurgeError {
-//     /// Error validating a protocol upgrade config.
-//     #[error("Invalid upgrade config")]
-//     InvalidPurgeConfig,
-//     /// Unable to retrieve a system contract.
-//     #[error("Unable to retrieve system contract: {0}")]
-//     UnableToRetrieveSystemContract(String),
-//     /// Unable to retrieve a system contract package.
-//     #[error("Unable to retrieve system contract package: {0}")]
-//     UnableToRetrieveSystemContractPackage(String),
-//     /// Unable to disable previous version of a system contract.
-//     #[error("Failed to disable previous version of system contract: {0}")]
-//     FailedToDisablePreviousVersion(String),
-//     /// (De)serialization error.
-//     #[error("{0}")]
-//     Bytesrepr(bytesrepr::Error),
-//     /// Failed to create system contract registry.
-//     #[error("Failed to insert system contract registry")]
-//     FailedToCreateSystemRegistry,
-//     /// Failed to migrate global state.
-//     #[error("Failed to perform global state migration: {0}")]
-//     MigrationRun(#[from] execution::Error),
-// }
