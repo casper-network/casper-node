4feb192558fc81d3f9010ac4e3c60563040eb467
Michał Papierski <michal@casperlabs.io>
2023-04-17 11:49:48 +0200

    Ensure multi value proposal is not supported.
    Recently updated parity-wasm 0.42.0 changed it's API to allow multi
    value proposal to be enabled without further changes. This test ensures
    that the default flags did not change, and we don't accidentally
    support multi value proposal by upgrading the dependencies.

--- execution_engine/src/shared/wasm_prep.rs
+++ execution_engine/src/shared/wasm_prep.rs
@@ -453,6 +453,7 @@ mod tests {
         builder,
         elements::{CodeSection, Instructions},
     };
+    use walrus::{FunctionBuilder, ModuleConfig, ValType};
 
     use super::*;
 
@@ -604,6 +605,50 @@ mod tests {
         assert!(
             matches!(&error, PreprocessingError::Deserialize(msg)
             if msg == &format!("missing function index {index}", index=u32::MAX)),
+            "{:?}",
+            error,
+        );
+    }
+
+    #[test]
+    fn should_not_accept_multi_value_proposal_wasm() {
+        let module_bytes = {
+            let mut module = walrus::Module::with_config(ModuleConfig::new());
+
+            let _memory_id = module.memories.add_local(false, 11, None);
+
+            // fn func_with_multi_value() -> (i32, i64) {
+
+            let mut func_with_locals =
+                FunctionBuilder::new(&mut module.types, &[], &[ValType::I32, ValType::I64]);
+
+            func_with_locals.func_body().i64_const(0).i32_const(1);
+
+            let func_with_locals = func_with_locals.finish(vec![], &mut module.funcs);
+
+            // }
+
+            // fn call() {
+
+            let mut call_func = FunctionBuilder::new(&mut module.types, &[], &[]);
+
+            // func_with_locals();
+            call_func.func_body().call(func_with_locals);
+
+            let call = call_func.finish(Vec::new(), &mut module.funcs);
+
+            // }
+
+            module.exports.add(DEFAULT_ENTRY_POINT_NAME, call);
+
+            module.emit_wasm()
+        };
+        let error = preprocess(WasmConfig::default(), &module_bytes)
+            .expect_err("should fail with an error");
+        assert!(
+            matches!(&error, PreprocessingError::Deserialize(msg)
+            // TODO: GH-3762 will improve the error message for unsupported wasm proposals.
+            if msg == "Enable the multi_value feature to deserialize more than one function result"),
             "{:?}",
             error,
         );
