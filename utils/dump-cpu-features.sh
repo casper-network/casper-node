826a4a782d41fe67505c865f6b20bffb9c217cfe
Marc Brinkmann <marc@casperlabs.io>
2023-04-17 16:11:13 +0200

    Added `dump-cpu-features.sh` script

---
+++ utils/dump-cpu-features.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Dumps a list of X86 extensions found to be in use by the given binary, in alphabetical order.
+
+set -e
+
+if [ $# -ne 1 ]; then
+  echo "usage: $(basename $0) binary"
+fi;
+
+BINARY=$1
+
+elfx86exts $BINARY | grep -v 'CPU Generation' | cut -f1 -d ' ' | sort
