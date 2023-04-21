8c33b77214e4481ef79c4eca7030653144702ea7
Marc Brinkmann <marc@casperlabs.io>
2023-04-21 17:09:34 +0200

    juliet: Remove flags in favor of simple enum

--- juliet/src/lib.rs
+++ juliet/src/lib.rs
@@ -21,41 +21,42 @@ struct Header {
     flags: HeaderFlags,
 }
 
-bitflags! {
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-    struct HeaderFlags: u8 {
-        const RESPONSE = 0b00000001;
-        const ERROR    = 0b00000010;
-        const CANCEL   = 0b00000100;
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u8)]
+enum HeaderFlags {
+    Request = 0b00000000,
+    Response = 0b00000001,
+    Error = 0b00000010,
+    RequestCancellation = 0b00000100,
+    ResponseCancellation = 0b00000101,
+}
+
+impl TryFrom<u8> for HeaderFlags {
+    type Error = u8;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0b00000000 => Ok(HeaderFlags::Request),
+            0b00000001 => Ok(HeaderFlags::Response),
+            0b00000010 => Ok(HeaderFlags::Error),
+            0b00000100 => Ok(HeaderFlags::RequestCancellation),
+            0b00000101 => Ok(HeaderFlags::ResponseCancellation),
+            _ => Err(value),
+        }
     }
 }
 
-impl Header {
-    #[inline(always)]
-    fn is_response(&self) -> bool {
-        self.flags.contains(HeaderFlags::RESPONSE)
-    }
+impl TryFrom<[u8; 4]> for Header {
+    type Error = u8; // Invalid flags.
 
-    #[inline(always)]
-    fn is_error(&self) -> bool {
-        self.flags.contains(HeaderFlags::ERROR)
-    }
-
-    #[inline(always)]
-    fn is_cancellation(&self) -> bool {
-        self.flags.contains(HeaderFlags::CANCEL)
-    }
-}
-
-impl From<[u8; 4]> for Header {
-    fn from(value: [u8; 4]) -> Self {
-        let flags = HeaderFlags::from_bits_truncate(value[0]);
+    fn try_from(value: [u8; 4]) -> Result<Self, Self::Error> {
+        let flags = HeaderFlags::try_from(value[0])?;
         // TODO: Check if this code is equal to `mem::transmute` usage on LE platforms.
-        Header {
+        Ok(Header {
             id: u16::from_le_bytes(value[2..4].try_into().unwrap()),
             channel: value[1],
             flags,
-        }
+        })
     }
 }
 
@@ -64,7 +65,7 @@ impl From<Header> for [u8; 4] {
     fn from(header: Header) -> Self {
         // TODO: Check if this code is equal to `mem::transmute` usage on LE platforms.
         [
-            header.flags.bits(),
+            header.flags as u8,
             header.channel,
             header.id.to_le_bytes()[0],
             header.id.to_le_bytes()[1],
@@ -82,36 +83,21 @@ impl Receiver {
                 }
 
                 // Grab the header and continue.
-                self.current_header
-                    .insert(Header::from(buf.get_u32_le().to_le_bytes()))
+                self.current_header.insert(
+                    Header::try_from(buf.get_u32_le().to_le_bytes())
+                        .expect("TODO: add error handling"),
+                )
             }
             Some(ref header) => header,
         };
 
-        match (*header).flags {
-            flags if flags.is_empty() => {
-                // A regular request.
-                todo!()
-            }
-            flags if flags == HeaderFlags::RESPONSE => {
-                // A regular response being sent back.
-                todo!()
-            }
-            flags if flags == HeaderFlags::CANCEL => {
-                // Request cancellcation.
-            }
-            flags if flags == HeaderFlags::CANCEL | HeaderFlags::RESPONSE => {
-                // Response cancellcation.
-            }
-            flags if flags == HeaderFlags::ERROR => {
-                // Error.
-            }
-            flags => {
-                todo!("invalid flags error")
-            }
+        match header.flags {
+            HeaderFlags::Request => todo!(),
+            HeaderFlags::Response => todo!(),
+            HeaderFlags::Error => todo!(),
+            HeaderFlags::RequestCancellation => todo!(),
+            HeaderFlags::ResponseCancellation => todo!(),
         }
-
-        todo!()
     }
 }
 
