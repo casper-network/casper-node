da12fbee732250995d880b22c28d4b9185cd1885
Marc Brinkmann <marc@casperlabs.io>
2023-04-21 16:47:59 +0200

    juliet: Setup `flags` with `bitflags`

--- juliet/src/lib.rs
+++ juliet/src/lib.rs
@@ -1,5 +1,6 @@
-use std::{fmt::Debug, mem};
+use std::fmt::Debug;
 
+use bitflags::bitflags;
 use bytes::Buf;
 
 const HEADER_SIZE: usize = 4;
@@ -17,23 +18,43 @@ struct Receiver {
 struct Header {
     id: u16,
     channel: u8,
-    flags: u8,
+    flags: HeaderFlags,
+}
+
+bitflags! {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    struct HeaderFlags: u8 {
+        const RESPONSE = 0b00000001;
+        const ERROR    = 0b00000010;
+        const CANCEL   = 0b00000100;
+    }
 }
 
 impl Header {
     #[inline(always)]
-    fn is_request(&self) -> bool {
-        todo!()
+    fn is_response(&self) -> bool {
+        self.flags.contains(HeaderFlags::RESPONSE)
+    }
+
+    #[inline(always)]
+    fn is_error(&self) -> bool {
+        self.flags.contains(HeaderFlags::ERROR)
+    }
+
+    #[inline(always)]
+    fn is_cancellation(&self) -> bool {
+        self.flags.contains(HeaderFlags::CANCEL)
     }
 }
 
 impl From<[u8; 4]> for Header {
     fn from(value: [u8; 4]) -> Self {
+        let flags = HeaderFlags::from_bits_truncate(value[0]);
         // TODO: Check if this code is equal to `mem::transmute` usage on LE platforms.
         Header {
             id: u16::from_le_bytes(value[2..4].try_into().unwrap()),
             channel: value[1],
-            flags: value[0],
+            flags,
         }
     }
 }
@@ -43,7 +64,7 @@ impl From<Header> for [u8; 4] {
     fn from(header: Header) -> Self {
         // TODO: Check if this code is equal to `mem::transmute` usage on LE platforms.
         [
-            header.flags,
+            header.flags.bits(),
             header.channel,
             header.id.to_le_bytes()[0],
             header.id.to_le_bytes()[1],
@@ -66,6 +87,29 @@ impl Receiver {
             }
             Some(ref header) => header,
         };
+
+        match (*header).flags {
+            flags if flags.is_empty() => {
+                // A regular request.
+                todo!()
+            }
+            flags if flags == HeaderFlags::RESPONSE => {
+                // A regular response being sent back.
+                todo!()
+            }
+            flags if flags == HeaderFlags::CANCEL => {
+                // Request cancellcation.
+            }
+            flags if flags == HeaderFlags::CANCEL | HeaderFlags::RESPONSE => {
+                // Response cancellcation.
+            }
+            flags if flags == HeaderFlags::ERROR => {
+                // Error.
+            }
+            flags => {
+                todo!("invalid flags error")
+            }
+        }
 
         todo!()
     }
