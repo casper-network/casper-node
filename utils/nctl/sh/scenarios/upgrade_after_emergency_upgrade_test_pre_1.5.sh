32076a24e123e1f16a06f5cb4da6a2273b83fda7
Rafał Chabowski <rafal@casperlabs.io>
2023-04-25 16:45:47 +0200

    Provide common `do_await_genesis_era_to_complete()` function and use it in tests

--- utils/nctl/sh/scenarios/upgrade_after_emergency_upgrade_test_pre_1.5.sh
+++ utils/nctl/sh/scenarios/upgrade_after_emergency_upgrade_test_pre_1.5.sh
@@ -128,13 +128,6 @@ function do_start_network() {
     sleep 10
 }
 
-function do_await_genesis_era_to_complete() {
-    log_step "awaiting genesis era to complete"
-    while [ "$(get_chain_era)" != "2" ]; do
-        sleep 1.0
-    done
-}
-
 function do_stop_network() {
     log_step "stopping the network for an emergency upgrade"
     ACTIVATE_ERA="$(get_chain_era)"
