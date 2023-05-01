19f6b81fe897a7eaaaec5197007f8dcdbb17eac8
Fraser Hutchison <fraser@casperlabs.io>
2023-05-01 23:17:28 +0100

    fix network_soundness.py test

--- utils/nctl/sh/contracts-transfers/do_dispatch_native.sh
+++ utils/nctl/sh/contracts-transfers/do_dispatch_native.sh
@@ -28,8 +28,10 @@ function main()
     local CP1_SECRET_KEY
     local CP1_ACCOUNT_KEY
     local CP2_ACCOUNT_KEY
-    local DISPATCHED
+    local DISPATCH_ATTEMPTS
+    local SUCCESSFUL_DISPATCH_COUNT
     local DISPATCH_NODE_ADDRESS
+    local OUTPUT
 
     CHAIN_NAME=$(get_chain_name)
     GAS_PAYMENT=${GAS_PAYMENT:-$NCTL_DEFAULT_GAS_PAYMENT}
@@ -54,14 +56,15 @@ function main()
         log "... transfer interval=$INTERVAL (s)"
         log "... counter-party 1 public key=$CP1_ACCOUNT_KEY"
         log "... counter-party 2 public key=$CP2_ACCOUNT_KEY"
-        log "... dispatched deploys:"    
+        log "... dispatched deploys:"
     fi
 
-    DISPATCHED=0
-    while [ $DISPATCHED -lt "$TRANSFERS" ];
-    do
+    DISPATCH_ATTEMPTS=0
+    SUCCESSFUL_DISPATCH_COUNT=0
+    while [ $DISPATCH_ATTEMPTS -lt "$TRANSFERS" ]; do
+        DISPATCH_ATTEMPTS=$((DISPATCH_ATTEMPTS + 1))
         DISPATCH_NODE_ADDRESS=${NODE_ADDRESS:-$(get_node_address_rpc)}
-        DEPLOY_HASH=$(
+        OUTPUT=$(
             $PATH_TO_CLIENT transfer \
                 --chain-name "$CHAIN_NAME" \
                 --node-address "$DISPATCH_NODE_ADDRESS" \
@@ -70,19 +73,24 @@ function main()
                 --secret-key "$CP1_SECRET_KEY" \
                 --amount "$AMOUNT" \
                 --target-account "$CP2_ACCOUNT_KEY" \
-                --transfer-id $((DISPATCHED + 1)) \
-                | jq '.result.deploy_hash' \
-                | sed -e 's/^"//' -e 's/"$//'
+                --transfer-id $((DISPATCHED + 1))
             )
-        DISPATCHED=$((DISPATCHED + 1))
-        if [ $VERBOSE == true ]; then
-            log "... #$DISPATCHED :: $DISPATCH_NODE_ADDRESS :: $DEPLOY_HASH"
+        if [[ $? -eq 0 ]]; then
+            SUCCESSFUL_DISPATCH_COUNT=$((SUCCESSFUL_DISPATCH_COUNT + 1))
+            DEPLOY_HASH=$(echo $OUTPUT | jq '.result.deploy_hash' | sed -e 's/^"//' -e 's/"$//')
+            if [ $VERBOSE == true ]; then
+                log "... #$DISPATCH_ATTEMPTS :: $DISPATCH_NODE_ADDRESS :: $DEPLOY_HASH"
+            fi
+        else
+            if [ $VERBOSE == true ]; then
+                log "... #$DISPATCH_ATTEMPTS :: $DISPATCH_NODE_ADDRESS :: FAILED to send"
+            fi
         fi
         sleep "$INTERVAL"
     done
 
     if [ $VERBOSE == true ]; then
-        log "dispatched $TRANSFERS native transfers"
+        log "successfully dispatched $SUCCESSFUL_DISPATCH_COUNT of $DISPATCH_ATTEMPTS native transfers"
     fi
 }
 
@@ -104,7 +112,7 @@ do
     case "$KEY" in
         amount) AMOUNT=${VALUE} ;;
         interval) INTERVAL=${VALUE} ;;
-        node) NODE_ID=${VALUE} ;;        
+        node) NODE_ID=${VALUE} ;;
         transfers) TRANSFERS=${VALUE} ;;
         user) USER_ID=${VALUE} ;;
         verbose) VERBOSE=${VALUE} ;;
