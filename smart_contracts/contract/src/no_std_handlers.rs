b54d8425f880ad8c6a6a01e227727e314ab65745
Rafał Chabowski <rafal@casperlabs.io>
2023-07-03 15:51:54 +0200

    Print `PanicInfo` if `test-support` is enabled

--- smart_contracts/contract/src/no_std_handlers.rs
+++ smart_contracts/contract/src/no_std_handlers.rs
@@ -1,9 +1,14 @@
 //! Contains definitions for panic and allocation error handlers.
 
+#[allow(unused)]
+use crate::contract_api::runtime;
+
 /// A panic handler for use in a `no_std` environment which simply aborts the process.
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &core::panic::PanicInfo) -> ! {
+    #[cfg(feature = "test-support")]
+    runtime::print(&alloc::format!("{}", _info));
     core::intrinsics::abort();
 }
 
