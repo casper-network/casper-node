357f1815a004ff090720cd885d22bdc674a4dd98
Rafał Chabowski <rafal@casperlabs.io>
2023-07-04 16:17:10 +0200

    Introduce `dump_debug_info` function

--- execution_engine/src/core/runtime/mod.rs
+++ execution_engine/src/core/runtime/mod.rs
@@ -1056,13 +1056,8 @@ where
             }
         };
 
-        if let wasmi::Error::Trap(Trap::Code(TrapCode::Unreachable)) = &error {
-            if let Some(stack_height) = instance.globals().last() {
-                eprintln!("current stack height = {}", stack_height.get())
-            } else {
-                eprintln!("current stack height unknown");
-            }
-        }
+        #[cfg(feature = "test-support")]
+        dump_debug_info(&error, instance);
 
         if let Some(host_error) = error.as_host_error() {
             // If the "error" was in fact a trap caused by calling `ret` then
@@ -1405,13 +1400,8 @@ where
             }
         };
 
-        if let wasmi::Error::Trap(Trap::Code(TrapCode::Unreachable)) = &error {
-            if let Some(stack_height) = instance.globals().last() {
-                eprintln!("current stack height = {}", stack_height.get())
-            } else {
-                eprintln!("current stack height unknown");
-            }
-        }
+        #[cfg(feature = "test-support")]
+        dump_debug_info(&error, instance);
 
         if let Some(host_error) = error.as_host_error() {
             // If the "error" was in fact a trap caused by calling `ret` then this is normal
@@ -3008,5 +2998,21 @@ where
         }
 
         Ok(Ok(()))
+    }
+}
+
+#[cfg(feature = "test-support")]
+fn dump_debug_info(error: &wasmi::Error, instance: wasmi::ModuleRef) {
+    // Currently, we're only interested in the details for the `Unreachable` error.
+    if let wasmi::Error::Trap(Trap::Code(TrapCode::Unreachable)) = error {
+        eprintln!("Wasm execution error: TrapCode::Unreachable");
+        eprintln!(
+            "current instance stack height = {}",
+            if let Some(stack_height) = instance.globals().last() {
+                stack_height.get().to_string()
+            } else {
+                "unknown".to_string()
+            }
+        );
     }
 }
