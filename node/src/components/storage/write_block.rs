3f93cf9b45df3106eda48fae28230329ffb06947
Rafał Chabowski <rafal@casperlabs.io>
2023-07-13 15:49:51 +0200

    Add mermaid charts for write block flows

--- node/src/components/storage/write_block.rs
+++ node/src/components/storage/write_block.rs
@@ -19,6 +19,24 @@ impl Storage {
     ///
     /// Returns `Ok(true)` if the block has been successfully written, `Ok(false)` if a part of it
     /// couldn't be written because it already existed, and `Err(_)` if there was an error.
+    #[cfg_attr(doc, aquamarine::aquamarine)]
+    /// ```mermaid
+    /// flowchart TD
+    ///     style Start fill:#66ccff,stroke:#333,stroke-width:4px
+    ///     style End fill:#66ccff,stroke:#333,stroke-width:4px
+    ///     style B fill:#00FF00,stroke:#333,stroke-width:4px
+    ///     
+    ///     Start --> A[Block needs to be stored]
+    ///     A --> put_block_to_storage
+    ///     put_block_to_storage --> StorageRequest::PutBlock
+    ///     StorageRequest::PutBlock --> B["write_block<br>(current version)"]
+    ///     B --> write_validated_block
+    ///     write_validated_block --> C[convert into VersionedBlockBody]
+    ///     C --> put_single_versioned_block_body
+    ///     put_single_versioned_block_body --> write_block_header
+    ///     write_block_header --> D[update indices]
+    ///     D --> End
+    /// ```
     pub fn write_block(&mut self, block: &Block) -> Result<bool, FatalStorageError> {
         block.verify()?;
         let env = Rc::clone(&self.env);
@@ -31,10 +49,31 @@ impl Storage {
     }
 
     /// Verifies a versioned block and writes it to a block to storage, updating indices as
-    /// necessary.
+    /// necessary. This function should only be used by components that deal with historical blocks,
+    /// for example: `Fetcher`.
     ///
     /// Returns `Ok(true)` if the block has been successfully written, `Ok(false)` if a part of it
     /// couldn't be written because it already existed, and `Err(_)` if there was an error.
+    #[cfg_attr(doc, aquamarine::aquamarine)]
+    /// ```mermaid
+    /// flowchart TD
+    ///     style Start fill:#66ccff,stroke:#333,stroke-width:4px
+    ///     style End fill:#66ccff,stroke:#333,stroke-width:4px
+    ///     style write_versioned_block fill:#00FF00,stroke:#333,stroke-width:4px
+    ///     
+    ///     Start --> A[Block fetched]
+    ///     A --> put_versioned_block_to_storage
+    ///     put_versioned_block_to_storage --> StorageRequest::PutVersionedBlock
+    ///     StorageRequest::PutVersionedBlock --> write_versioned_block
+    ///     write_versioned_block --> write_validated_versioned_block
+    ///     write_validated_versioned_block --> B{"is it a legacy block?<br>(V1)"}
+    ///     B -->|Yes| put_single_legacy_block_body
+    ///     B -->|No| put_single_versioned_block_body
+    ///     put_single_legacy_block_body --> write_block_header
+    ///     put_single_versioned_block_body --> write_block_header
+    ///     write_block_header --> C[update indices]
+    ///     C --> End
+    /// ```
     pub fn write_versioned_block(
         &mut self,
         block: &VersionedBlock,
@@ -53,6 +92,24 @@ impl Storage {
     ///
     /// Returns `Ok(true)` if the block has been successfully written, `Ok(false)` if a part of it
     /// couldn't be written because it already existed, and `Err(_)` if there was an error.
+    #[cfg_attr(doc, aquamarine::aquamarine)]
+    /// ```mermaid
+    ///     flowchart TD
+    ///     style Start fill:#66ccff,stroke:#333,stroke-width:4px
+    ///     style End fill:#66ccff,stroke:#333,stroke-width:4px
+    ///     style B fill:#00FF00,stroke:#333,stroke-width:4px
+    ///     
+    ///     Start --> A["Validated block needs to be stored<br>(might be coming from contract runtime)"]
+    ///     A --> put_executed_block_to_storage
+    ///     put_executed_block_to_storage --> StorageRequest::PutExecutedBlock
+    ///     StorageRequest::PutExecutedBlock --> put_executed_block
+    ///     put_executed_block --> B["write_validated_block<br>(current version)"]
+    ///     B --> C[convert into VersionedBlockBody]
+    ///     C --> put_single_versioned_block_body
+    ///     put_single_versioned_block_body --> write_block_header
+    ///     write_block_header --> D[update indices]
+    ///     D --> End
+    /// ```
     fn write_validated_block(
         &mut self,
         txn: &mut RwTransaction,
