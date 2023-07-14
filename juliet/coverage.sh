7817187d877d60b04eafe9b88b203ca5b7e5bff8
Marc Brinkmann <marc@casperlabs.io>
2023-07-14 17:11:50 +0200

    juliet: Ensure sufficient number of iterations for coverage generation in testing

--- juliet/coverage.sh
+++ juliet/coverage.sh
@@ -6,6 +6,9 @@
 
 set -e
 
+# Try to make sure there is reasonable coverage on fuzzed tests.
+export PROPTEST_CASES=10000
+
 cargo tarpaulin --engine Llvm -r . --exclude-files '../**' --exclude-files 'examples' --out lcov
 mkdir -p coverage
 genhtml -o coverage lcov.info
