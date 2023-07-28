0f553c6a8d16e3be88d19cdc4e4530058e6a44ba
Michał Papierski <michal@casperlabs.io>
2023-07-28 16:01:39 +0200

    Fix todo placeholder

--- execution_engine_testing/tests/src/test/contract_api/get_call_stack.rs
+++ execution_engine_testing/tests/src/test/contract_api/get_call_stack.rs
@@ -200,14 +200,14 @@ fn assert_each_context_has_correct_call_stack_info(
         let stored_call_stack_key = format!("call_stack-{}", i);
         // we need to know where to look for the call stack information
         let call_stack = match call.entry_point_type {
-            EntryPointType::Contract => builder.get_call_stack_from_contract_context(
-                &stored_call_stack_key,
-                current_contract_package_hash,
-            ),
+            EntryPointType::Contract | EntryPointType::Install | EntryPointType::Normal => builder
+                .get_call_stack_from_contract_context(
+                    &stored_call_stack_key,
+                    current_contract_package_hash,
+                ),
             EntryPointType::Session => {
                 builder.get_call_stack_from_session_context(&stored_call_stack_key)
             }
-            _ => todo!("factory pattern"),
         };
         assert_eq!(
             call_stack.len(),
