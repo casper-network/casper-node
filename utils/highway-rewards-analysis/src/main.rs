e80e4cfe24128a175e56704dc2c7016d15a1a7f1
Bartłomiej Kamiński <bart@casperlabs.io>
2023-10-06 15:24:02 +0200

    Add printing average max quora

--- utils/highway-rewards-analysis/src/main.rs
+++ utils/highway-rewards-analysis/src/main.rs
@@ -10,7 +10,10 @@ use serde::{Deserialize, Serialize};
 
 use casper_node::consensus::{
     highway_core::{
-        finality_detector::{round_participation, RoundParticipation},
+        finality_detector::{
+            assigned_weight_and_latest_unit, find_max_quora, round_participation,
+            RoundParticipation,
+        },
         State,
     },
     protocols::common::validators,
@@ -72,6 +75,8 @@ fn main() {
         validators::<ClContext>(&dump.faulty, &dump.cannot_propose, dump.validators.clone());
 
     find_skipped_rounds(&validators, &dump, args.verbose);
+
+    find_lowest_quorum_participation(&validators, &dump);
 }
 
 const TOP_TO_PRINT: usize = 10;
@@ -89,7 +94,7 @@ fn find_skipped_rounds(validators: &Validators<PublicKey>, dump: &EraDump, verbo
 
     for block_hash in all_blocks {
         let block_unit = state.unit(block_hash);
-        let round_id = block_unit.timestamp;
+        let round_id = block_unit.round_id();
         for i in 0..validators.len() as u32 {
             let observation = state.panorama().get(i.into()).unwrap();
             let round_participation = round_participation(state, observation, round_id);
@@ -132,5 +137,50 @@ fn find_skipped_rounds(validators: &Validators<PublicKey>, dump: &EraDump, verbo
                 skipped_rounds.join(", ")
             );
         }
+    }
+
+    println!();
+}
+
+fn find_lowest_quorum_participation(validators: &Validators<PublicKey>, dump: &EraDump) {
+    let state = &dump.highway_state;
+    let highest_block = state.fork_choice(state.panorama()).unwrap();
+    let mut quora_sum = vec![0.0; validators.len()];
+    let mut num_rounds = 0;
+
+    let hb_unit = state.unit(highest_block);
+    for bhash in state.ancestor_hashes(highest_block) {
+        let proposal_unit = state.unit(bhash);
+        let r_id = proposal_unit.round_id();
+
+        let (assigned_weight, latest) =
+            assigned_weight_and_latest_unit(state, &hb_unit.panorama, r_id);
+
+        let max_quora = find_max_quora(state, bhash, &latest);
+        let max_quora: Vec<f32> = max_quora
+            .into_iter()
+            .map(|quorum_w| quorum_w.0 as f32 / assigned_weight.0 as f32 * 100.0)
+            .collect();
+
+        for (q_sum, max_q) in quora_sum.iter_mut().zip(&max_quora) {
+            *q_sum += max_q;
+        }
+        num_rounds += 1;
+    }
+
+    let mut quora_avg: Vec<_> = quora_sum
+        .into_iter()
+        .enumerate()
+        .map(|(vid, q_sum)| (vid as u32, q_sum / num_rounds as f32))
+        .collect();
+    quora_avg.sort_by(|(_, q_avg1), (_, q_avg2)| q_avg1.partial_cmp(q_avg2).unwrap());
+
+    println!("{} validators with lowest average max quora:", TOP_TO_PRINT);
+    for (vid, q_avg) in quora_avg.iter().take(TOP_TO_PRINT) {
+        println!(
+            "{}: average max quorum {:3.1}%",
+            validators.id((*vid).into()).unwrap(),
+            q_avg
+        );
     }
 }
