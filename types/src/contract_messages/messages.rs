68cfc48878613e015f1b9416cd5e1b8bf5665aef
Alexandru Sardan <alexandru@casperlabs.io>
2023-10-18 13:41:21 +0000

    types/contract_messages: add getters for `Message` fields
    Also add convenience methods to convert contract message related stored
    values to their inner types.
    
    Signed-off-by: Alexandru Sardan <alexandru@casperlabs.io>

--- types/src/contract_messages/messages.rs
+++ types/src/contract_messages/messages.rs
@@ -148,6 +148,31 @@ impl Message {
             index,
         }
     }
+
+    /// Returns a reference to the identity of the entity that produced the message.
+    pub fn entity_addr(&self) -> &HashAddr {
+        &self.entity_addr
+    }
+
+    /// Returns a reference to the payload of the message.
+    pub fn payload(&self) -> &MessagePayload {
+        &self.message
+    }
+
+    /// Returns a reference to the name of the topic on which the message was emitted on.
+    pub fn topic_name(&self) -> &String {
+        &self.topic_name
+    }
+
+    /// Returns a reference to the hash of the name of the topic.
+    pub fn topic_name_hash(&self) -> &TopicNameHash {
+        &self.topic_name_hash
+    }
+
+    /// Returns the index of the message in the topic.
+    pub fn index(&self) -> u32 {
+        self.index
+    }
 }
 
 impl ToBytes for Message {
