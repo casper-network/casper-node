e4cd359583704e05029b2cbb230d1e8469a0e220
Bartłomiej Kamiński <bart@casperlabs.io>
2023-10-25 17:16:40 +0200

    Implement the new method of measuring node's performance

--- node/src/components/consensus/protocols/highway/performance_meter.rs
+++ node/src/components/consensus/protocols/highway/performance_meter.rs
@@ -1,40 +1,39 @@
 pub(crate) mod config;
-#[cfg(test)]
-mod tests;
 
-use std::{cmp::max, collections::VecDeque, mem};
+use std::cmp::{max, min};
 
 use datasize::DataSize;
-use tracing::{error, trace};
+use tracing::error;
 
 use casper_types::{TimeDiff, Timestamp};
 
 use crate::components::consensus::{
-    highway_core::{finality_detector::FinalityDetector, state, State},
+    highway_core::{
+        finality_detector::{
+            assigned_weight_and_latest_unit, find_max_quora, round_participation,
+            RoundParticipation,
+        },
+        state, State,
+    },
     traits::Context,
-    utils::Weight,
+    utils::ValidatorIndex,
 };
 
 use config::*;
 
 #[derive(DataSize, Debug, Clone)]
-pub(crate) struct PerformanceMeter<C>
-where
-    C: Context,
-{
-    // store whether a particular round was successful
-    // index 0 is the last handled round, 1 is the second-to-last etc.
-    rounds: VecDeque<bool>,
-    current_round_id: Timestamp,
-    proposals: Vec<C::Hash>,
+pub(crate) struct PerformanceMeter {
+    our_vid: ValidatorIndex,
     min_round_len: TimeDiff,
     max_round_len: TimeDiff,
     current_round_len: TimeDiff,
+    last_switch_round_id: Timestamp,
     config: Config,
 }
 
-impl<C: Context> PerformanceMeter<C> {
+impl PerformanceMeter {
     pub fn new(
+        our_vid: ValidatorIndex,
         round_len: TimeDiff,
         min_round_len: TimeDiff,
         max_round_len: TimeDiff,
@@ -43,58 +42,17 @@ impl<C: Context> PerformanceMeter<C> {
     ) -> Self {
         let current_round_id = state::round_id(timestamp, round_len);
         Self {
-            rounds: VecDeque::with_capacity(config.num_rounds_to_consider as usize),
-            current_round_id,
-            proposals: Vec::new(),
+            our_vid,
             min_round_len,
             max_round_len,
             current_round_len: round_len,
+            last_switch_round_id: current_round_id,
             config,
         }
     }
 
-    fn change_length(&mut self, new_len: TimeDiff, timestamp: Timestamp) {
-        self.rounds = VecDeque::with_capacity(self.config.num_rounds_to_consider as usize);
-        self.current_round_len = new_len;
-        self.current_round_id = state::round_id(timestamp, new_len);
-        self.proposals = Vec::new();
-    }
-
-    fn check_proposals_success(&self, state: &State<C>, proposal_h: &C::Hash) -> bool {
-        let total_w = state.total_weight();
-
-        #[allow(clippy::integer_arithmetic)] // FTT is less than 100%, so this can't overflow.
-        let finality_detector = FinalityDetector::<C>::new(max(
-            Weight(
-                (u128::from(total_w) * *self.config.acceleration_ftt.numer() as u128
-                    / *self.config.acceleration_ftt.denom() as u128) as u64,
-            ),
-            Weight(1),
-        ));
-
-        // check for the existence of a level-1 summit
-        finality_detector.find_summit(1, proposal_h, state) == 1
-    }
-
-    /// Registers a proposal within this round - if it's finalized within the round, the round will
-    /// be successful.
-    pub fn new_proposal(&mut self, proposal_h: C::Hash, timestamp: Timestamp) {
-        // only add proposals from within the current round
-        if state::round_id(timestamp, self.current_round_len) == self.current_round_id {
-            trace!(
-                %self.current_round_id,
-                timestamp = timestamp.millis(),
-                "adding a proposal"
-            );
-            self.proposals.push(proposal_h);
-        } else {
-            trace!(
-                %self.current_round_id,
-                timestamp = timestamp.millis(),
-                %self.current_round_len,
-                "trying to add proposal for a different round!"
-            );
-        }
+    pub fn current_round_len(&self) -> TimeDiff {
+        self.current_round_len
     }
 
     /// If the current timestamp indicates that the round has ended, checks the known proposals for
@@ -105,102 +63,71 @@ impl<C: Context> PerformanceMeter<C> {
     /// successful, we return a higher round length for the future.
     /// If the length shouldn't grow, and the round ID is divisible by a certain number, a lower
     /// round length is returned.
-    pub fn calculate_new_length(&mut self, state: &State<C>, now: Timestamp) -> TimeDiff {
-        // if the round hasn't finished, just return whatever we have now
-        if state::round_id(now, self.current_round_len) <= self.current_round_id {
-            return self.new_length();
+    pub fn calculate_new_length<C: Context>(&mut self, state: &State<C>) -> TimeDiff {
+        let panorama = state.panorama();
+        let latest_block = match state.fork_choice(panorama) {
+            Some(block) => block,
+            None => {
+                // we have no blocks to check - just return the current setting
+                return self.current_round_len;
+            }
+        };
+
+        let blocks_to_check = state.ancestor_hashes(latest_block);
+
+        let max_quora: Vec<_> = blocks_to_check
+            .take_while(|block| state.unit(block).round_id() >= self.last_switch_round_id)
+            .filter_map(|block| {
+                let round_id = state.unit(block).round_id();
+                (!matches!(
+                    round_participation(state, &panorama[self.our_vid], round_id),
+                    RoundParticipation::Unassigned
+                ))
+                .then(|| {
+                    let (assigned_weight, latest_units) =
+                        assigned_weight_and_latest_unit(state, panorama, round_id);
+                    let max_quorum = find_max_quora(state, block, &latest_units)
+                        .get(self.our_vid)
+                        .copied()
+                        .unwrap_or(0u64.into());
+                    max_quorum.0 as f64 / assigned_weight.0 as f64
+                })
+            })
+            .take(self.config.blocks_to_consider)
+            .collect();
+
+        if max_quora.len() < self.config.blocks_to_consider {
+            return self.current_round_len;
         }
 
-        trace!(%self.current_round_id, "calculating length");
-        let current_round_index = round_index(self.current_round_id, self.current_round_len);
-        let new_round_index = round_index(now, self.current_round_len);
+        let avg_max_quorum = max_quora.iter().sum::<f64>() / max_quora.len() as f64;
 
-        if mem::take(&mut self.proposals)
-            .into_iter()
-            .any(|proposal| self.check_proposals_success(state, &proposal))
+        let current_round_id = state.unit(latest_block).round_id();
+        let current_round_index = round_index(current_round_id, self.current_round_len);
+
+        #[allow(clippy::integer_arithmetic)]
+        if avg_max_quorum < self.config.slowdown_threshold {
+            self.current_round_len = min(self.current_round_len * 2, self.max_round_len);
+            self.last_switch_round_id = current_round_id;
+        } else if avg_max_quorum > self.config.acceleration_threshold
+            && current_round_index % self.config.acceleration_parameter == 0
         {
-            trace!("round succeeded");
-            self.rounds.push_front(true);
-        } else {
-            trace!("round failed");
-            self.rounds.push_front(false);
+            self.current_round_len = max(self.current_round_len / 2, self.min_round_len);
+            self.last_switch_round_id = current_round_id;
         }
 
-        // if we're just switching rounds and more than a single round has passed, all the
-        // rounds since the last registered round have failed
-        let failed_round_count = new_round_index
-            .saturating_sub(current_round_index)
-            .saturating_sub(1);
-        for _ in 0..failed_round_count {
-            trace!("round failed");
-            self.rounds.push_front(false);
-        }
-
-        self.current_round_id =
-            Timestamp::zero() + self.current_round_len.saturating_mul(new_round_index);
-
-        self.clean_old_rounds();
-
-        trace!(
-            %self.current_round_len,
-            "{} failures among the last {} rounds.",
-            self.count_failures(),
-            self.rounds.len()
-        );
-
-        let new_len = self.new_length();
-
-        trace!(%new_len, "new length calculated");
-
-        if new_len != self.current_round_len {
-            self.change_length(new_len, now);
-        }
-
-        new_len
+        self.current_round_len
     }
 
     /// Returns an instance of `Self` for the new era: resetting the counters where appropriate.
-    pub fn next_era(&self, timestamp: Timestamp) -> Self {
+    pub fn next_era(&self) -> Self {
         Self {
-            rounds: self.rounds.clone(),
-            current_round_id: state::round_id(timestamp, self.current_round_len),
-            proposals: Default::default(),
+            our_vid: self.our_vid,
             min_round_len: self.min_round_len,
             max_round_len: self.max_round_len,
             current_round_len: self.current_round_len,
+            last_switch_round_id: self.last_switch_round_id,
             config: self.config,
-        }
-    }
-
-    fn clean_old_rounds(&mut self) {
-        while self.rounds.len() as u64 > self.config.num_rounds_to_consider {
-            self.rounds.pop_back();
-        }
-    }
-
-    fn count_failures(&self) -> usize {
-        self.rounds.iter().filter(|&success| !success).count()
-    }
-
-    /// Returns the round length to be used in the next round, based on the previously used round
-    /// length and the current counts of successes and failures.
-    pub(super) fn new_length(&self) -> TimeDiff {
-        let current_round_index = round_index(self.current_round_id, self.current_round_len);
-        let num_failures = self.count_failures() as u64;
-        #[allow(clippy::integer_arithmetic)] // The acceleration_parameter is not zero.
-        if num_failures > self.config.max_failed_rounds()
-            && self.current_round_len * 2 <= self.max_round_len
-        {
-            self.current_round_len * 2
-        } else if current_round_index % self.config.acceleration_parameter == 0
-            && self.current_round_len > self.min_round_len
-            // we will only accelerate if we collected data about enough rounds
-            && self.rounds.len() as u64 == self.config.num_rounds_to_consider
-            && num_failures < self.config.max_failures_for_acceleration()
-        {
-            self.current_round_len / 2
-        } else {
-            self.current_round_len
         }
     }
 }
--- node/src/components/consensus/protocols/highway/performance_meter/config.rs
+++ node/src/components/consensus/protocols/highway/performance_meter/config.rs
@@ -1,66 +1,36 @@
 use crate::components::consensus::config::Config as ConsensusConfig;
 use datasize::DataSize;
-use num_rational::Ratio;
 
 use serde::{Deserialize, Serialize};
 
-/// The number of most recent rounds we will be keeping track of.
-pub(crate) const NUM_ROUNDS_TO_CONSIDER: usize = 40;
-/// The number of successful rounds that triggers us to slow down: With this many or fewer
-/// successes per `NUM_ROUNDS_TO_CONSIDER`, we increase our round length.
-pub(crate) const NUM_ROUNDS_SLOWDOWN: usize = 10;
-/// The number of successful rounds that triggers us to speed up: With this many or more successes
-/// per `NUM_ROUNDS_TO_CONSIDER`, we decrease our round length.
-pub(crate) const NUM_ROUNDS_SPEEDUP: usize = 32;
+/// The number of most recent blocks for which we average the max quorum in which we participated.
+pub(crate) const BLOCKS_TO_CONSIDER: usize = 5;
+/// The average max quorum that triggers us to slow down: with this big or smaller average max
+/// quorum per `BLOCKS_TO_CONSIDER`, we increase our round length.
+pub(crate) const SLOW_DOWN_THRESHOLD: f64 = 0.8;
+/// The average max quorum that triggers us to speed up: with this big or larger average max quorum
+/// per `BLOCKS_TO_CONSIDER`, we decrease our round length.
+pub(crate) const ACCELERATION_THRESHOLD: f64 = 0.9;
 /// We will try to accelerate (decrease our round length) every `ACCELERATION_PARAMETER` rounds if
-/// we have few enough failures.
+/// we have a big enough average max quorum.
 pub(crate) const ACCELERATION_PARAMETER: u64 = 40;
-/// The FTT, as a percentage (i.e. `THRESHOLD = 1` means 1% of the validators' total weight), which
-/// we will use for looking for a summit in order to determine a proposal's finality.
-/// The required quorum in a summit we will look for to check if a round was successful is
-/// determined by this FTT.
-pub(crate) const THRESHOLD: u64 = 1;
-
-#[cfg(test)]
-pub(crate) const MAX_FAILED_ROUNDS: usize = NUM_ROUNDS_TO_CONSIDER - NUM_ROUNDS_SLOWDOWN - 1;
 
 #[derive(DataSize, Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct Config {
-    pub num_rounds_to_consider: u64,
-    pub num_rounds_slowdown: u64,
-    pub num_rounds_speedup: u64,
+    pub blocks_to_consider: usize,
+    pub slowdown_threshold: f64,
+    pub acceleration_threshold: f64,
     pub acceleration_parameter: u64,
-    #[data_size(skip)]
-    pub acceleration_ftt: Ratio<u64>,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
-            num_rounds_to_consider: NUM_ROUNDS_TO_CONSIDER as u64,
-            num_rounds_slowdown: NUM_ROUNDS_SLOWDOWN as u64,
-            num_rounds_speedup: NUM_ROUNDS_SPEEDUP as u64,
+            blocks_to_consider: BLOCKS_TO_CONSIDER,
+            slowdown_threshold: SLOW_DOWN_THRESHOLD,
+            acceleration_threshold: ACCELERATION_THRESHOLD,
             acceleration_parameter: ACCELERATION_PARAMETER,
-            acceleration_ftt: Ratio::new(THRESHOLD, 100),
         }
-    }
-}
-
-impl Config {
-    /// The maximum number of failures allowed among `num_rounds_to_consider` latest rounds, with
-    /// which we won't increase our round length. Exceeding this threshold will mean that we
-    /// should slow down.
-    pub(crate) fn max_failed_rounds(&self) -> u64 {
-        self.num_rounds_to_consider
-            .saturating_sub(self.num_rounds_slowdown)
-            .saturating_sub(1)
-    }
-
-    /// The maximum number of failures with which we will attempt to accelerate (decrease the round
-    /// exponent).
-    pub(crate) fn max_failures_for_acceleration(&self) -> u64 {
-        self.num_rounds_to_consider
-            .saturating_sub(self.num_rounds_speedup)
     }
 }
 
