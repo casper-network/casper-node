713d3ee448b72f6b628ca22a13ce6b2efdad42e8
Marc Brinkmann <marc@casperlabs.io>
2023-11-20 15:01:15 +0100

    Double default memory buffer size for juliet

--- node/src/components/network/transport.rs
+++ node/src/components/network/transport.rs
@@ -34,7 +34,7 @@ pub(super) fn create_rpc_builder(
     // TODO: Figure out a good value for buffer sizes, and make configurable individually.
     let io_core = juliet::io::IoCoreBuilder::with_default_buffer_size(
         protocol,
-        max_in_flight_demands.max(1) as usize,
+        2 * max_in_flight_demands.max(1) as usize,
     );
 
     juliet::rpc::RpcBuilder::new(io_core)
