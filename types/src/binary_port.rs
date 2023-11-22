6b5c326130c9a78b38f2739a5e5b71fbc2518a9f
Rafał Chabowski <rafal@casperlabs.io>
2023-11-22 16:26:55 +0100

    binary_port: Move `SpeculativeExecutionResult` to a separate module

--- types/src/binary_port.rs
+++ types/src/binary_port.rs
@@ -4,48 +4,4 @@ pub mod db_id;
 pub mod get_request;
 pub mod global_state_query_result;
 pub mod non_persistent_data_request;
-
-use crate::{
-    bytesrepr::{self, FromBytes, ToBytes},
-    contract_messages::Messages,
-    execution::ExecutionResultV2,
-};
-
-/// TODO
-pub struct SpeculativeExecutionResult {
-    /// Result of the execution.
-    pub execution_result: ExecutionResultV2,
-    /// Messages emitted during execution.
-    pub messages: Messages,
-}
-
-impl ToBytes for SpeculativeExecutionResult {
-    fn to_bytes(&self) -> Result<Vec<u8>, bytesrepr::Error> {
-        let mut buffer = bytesrepr::allocate_buffer(self)?;
-        self.write_bytes(&mut buffer)?;
-        Ok(buffer)
-    }
-
-    fn write_bytes(&self, writer: &mut Vec<u8>) -> Result<(), bytesrepr::Error> {
-        self.execution_result.write_bytes(writer)?;
-        self.messages.write_bytes(writer)
-    }
-
-    fn serialized_length(&self) -> usize {
-        self.execution_result.serialized_length() + self.messages.serialized_length()
-    }
-}
-
-impl FromBytes for SpeculativeExecutionResult {
-    fn from_bytes(bytes: &[u8]) -> Result<(Self, &[u8]), bytesrepr::Error> {
-        let (execution_result, remainder) = ExecutionResultV2::from_bytes(bytes)?;
-        let (messages, remainder) = Messages::from_bytes(remainder)?;
-        Ok((
-            SpeculativeExecutionResult {
-                execution_result,
-                messages,
-            },
-            remainder,
-        ))
-    }
-}
+pub mod speculative_execution_result;
---
+++ types/src/binary_port/speculative_execution_result.rs
@@ -0,0 +1,46 @@
+//! The result of the speculative execution request.
+
+use crate::{
+    bytesrepr::{self, FromBytes, ToBytes},
+    contract_messages::Messages,
+    execution::ExecutionResultV2,
+};
+
+/// Result of the speculative execution request.
+pub struct SpeculativeExecutionResult {
+    /// Result of the execution.
+    pub execution_result: ExecutionResultV2,
+    /// Messages emitted during execution.
+    pub messages: Messages,
+}
+
+impl ToBytes for SpeculativeExecutionResult {
+    fn to_bytes(&self) -> Result<Vec<u8>, bytesrepr::Error> {
+        let mut buffer = bytesrepr::allocate_buffer(self)?;
+        self.write_bytes(&mut buffer)?;
+        Ok(buffer)
+    }
+
+    fn write_bytes(&self, writer: &mut Vec<u8>) -> Result<(), bytesrepr::Error> {
+        self.execution_result.write_bytes(writer)?;
+        self.messages.write_bytes(writer)
+    }
+
+    fn serialized_length(&self) -> usize {
+        self.execution_result.serialized_length() + self.messages.serialized_length()
+    }
+}
+
+impl FromBytes for SpeculativeExecutionResult {
+    fn from_bytes(bytes: &[u8]) -> Result<(Self, &[u8]), bytesrepr::Error> {
+        let (execution_result, remainder) = ExecutionResultV2::from_bytes(bytes)?;
+        let (messages, remainder) = Messages::from_bytes(remainder)?;
+        Ok((
+            SpeculativeExecutionResult {
+                execution_result,
+                messages,
+            },
+            remainder,
+        ))
+    }
+}
