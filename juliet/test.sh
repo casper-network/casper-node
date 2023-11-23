9035234ee49f133775350afc653061de57a79800
Marc Brinkmann <marc@casperlabs.io>
2023-11-23 15:06:53 +0100

    Remove `juliet` from source tree, use version published on crates.io instead

--- juliet/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#: Shorthand script to run test with logging setup correctly.
-
-RUST_LOG=${RUST_LOG:-juliet=trace}
-export RUST_LOG
-
-# Run one thread at a time to not get interleaved output.
-exec cargo test --features tracing -- --test-threads=1 --nocapture $@
