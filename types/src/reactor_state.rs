47fd911b1a0c1c8f5f400a8709bba29cba84c6c8
Rafał Chabowski <rafal@casperlabs.io>
2023-11-28 11:59:25 +0100

    Move the aquamarine chart for `ReactorState` from `types` back to `node`

--- types/src/reactor_state.rs
+++ types/src/reactor_state.rs
@@ -5,59 +5,9 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 /// The state of the reactor.
-#[cfg_attr(doc, aquamarine::aquamarine)]
-/// ```mermaid
-/// flowchart TD
-///     %%{init: { 'flowchart': {'diagramPadding':100} }}%%
-///     style Start fill:#66ccff,stroke:#333,stroke-width:4px
-///     style End fill:#66ccff,stroke:#333,stroke-width:4px
-///     
-///     Start --> Initialize
-///     Initialize --> CatchUp
-///     CatchUp --> KeepUp
-///     KeepUp --> CatchUp
-///     KeepUp --> Validate
-///     Validate --> KeepUp
-///     CatchUp --> ShutdownForUpgrade
-///     KeepUp --> ShutdownForUpgrade
-///     Validate --> ShutdownForUpgrade
-///     CatchUp --> Upgrading
-///     CatchUp -->|at genesis| Validate
-///     Upgrading --> CatchUp
-///     ShutdownForUpgrade --> End
-/// ```
-/// ```mermaid
-/// flowchart TD
-///     style Start fill:#66ccff,stroke:#333,stroke-width:4px
-///     style End fill:#66ccff,stroke:#333,stroke-width:4px
-///     style F fill:#ffcc66,stroke:#333,stroke-width:4px
-///     style G fill:#ffcc66,stroke:#333,stroke-width:4px
-///     title[CatchUp process]
-///     title---Start
-///     style title fill:#FFF,stroke:#FFF
-///     linkStyle 0 stroke-width:0;
-///
-///     Start --> A["get sync identifier (sync starting point)"]
-///     A --> BlockHash
-///     A --> BlockIdentifier
-///     A --> SyncedBlockIdentifier
-///     A --> LocalTip
-///     BlockHash --> E[process identifier in<br/>block accumulator]
-///     BlockIdentifier --> E
-///     SyncedBlockIdentifier --> E
-///     LocalTip --> E
-///     CaughtUp --> H[handle upgrade<br/>if needed]
-///     H --> End
-///     E -->|more data needed<br/>from network<br/>to let us sync near tip| Leap
-///     E -->|block represented by<br/>identifier is not stored<br/>locally, sync it| BlockSync
-///     E -->|we think we're close<br/>enough to the tip|CaughtUp
-///     Leap --> F[initiate SyncLeap<br/>and retry later]
-///     BlockSync --> G[initiate BlockSync<br/>and retry later]
-/// ```
 #[derive(
     Copy, Clone, PartialEq, Eq, Serialize, Deserialize, DataSize, Debug, Display, JsonSchema,
 )]
-#[schemars(description = "The state of the reactor.")]
 pub enum ReactorState {
     /// Get all components and reactor state set up on start.
     Initialize,
