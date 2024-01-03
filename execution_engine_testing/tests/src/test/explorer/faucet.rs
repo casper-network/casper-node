16c08b75d8da47a6f5f75e8d4a1f87c79ff1b215
Alexandru Sardan <alexandru@casperlabs.io>
2024-01-03 14:53:00 +0000

    contracts/Makefile: remap CARGO_HOME path prefix if it is set
    On some machines, the `CARGO_HOME` environment variable is set which
    overrides the default `$HOME/.cargo` path. In order for paths to be
    consistent in the contract binaries, also remap paths that start with
    the prefix set in `$CARGO_HOME`.
    
    Signed-off-by: Alexandru Sardan <alexandru@casperlabs.io>

--- execution_engine_testing/tests/src/test/explorer/faucet.rs
+++ execution_engine_testing/tests/src/test/explorer/faucet.rs
@@ -933,7 +933,7 @@ fn faucet_costs() {
     // This test will fail if execution costs vary.  The expected costs should not be updated
     // without understanding why the cost has changed.  If the costs do change, it should be
     // reflected in the "Costs by Entry Point" section of the faucet crate's README.md.
-    const EXPECTED_FAUCET_INSTALL_COST: u64 = 88_380_294_880;
+    const EXPECTED_FAUCET_INSTALL_COST: u64 = 88_365_174_880;
     const EXPECTED_FAUCET_SET_VARIABLES_COST: u64 = 110_504_620;
     const EXPECTED_FAUCET_CALL_BY_INSTALLER_COST: u64 = 2_776_604_180;
     const EXPECTED_FAUCET_CALL_BY_USER_COST: u64 = 2_618_367_020;
