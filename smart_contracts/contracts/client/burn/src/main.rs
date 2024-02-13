0cab1995ca1fbffb60a95e82c856a174104a303b
Jan Hoffmann <jan@hfmn.pl>
2024-02-13 13:44:39 +0100

    added test contract
    Co-authored-by: igor-casper <igor@casper.network>

---
+++ smart_contracts/contracts/client/burn/src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+use casper_contract::contract_api::{runtime, system};
+use casper_types::{runtime_args, system::mint, RuntimeArgs, URef};
+
+const ARG_PURSES: &str = "purses";
+
+fn burn(urefs: Vec<URef>) {
+    let contract_hash = system::get_mint();
+    let args = runtime_args! {
+        mint::ARG_PURSES => urefs,
+    };
+    runtime::call_contract::<()>(contract_hash, mint::METHOD_BURN, args);
+}
+
+// Accepts a public key. Issues an activate-bid bid to the auction contract.
+#[no_mangle]
+pub extern "C" fn call() {
+    let urefs:Vec<URef> = runtime::get_named_arg(ARG_PURSES);
+    burn(urefs);
+}
