19ed72336abcc68b3166c5c0d31e074e07752052
Michał Papierski <michal@casper.network>
2024-02-23 17:52:05 +0800

    update scripts

--- vm2-build-contracts.sh
+++ vm2-build-contracts.sh
@@ -10,6 +10,6 @@ for contract in "${VM2_CONTRACTS[@]}"
 do
   pushd smart_contracts/contracts/vm2/$contract/
   pwd
-  RUSTFLAGS="-g -Z macro-backtrace" cargo build --target wasm32-unknown-unknown --release
+  RUSTFLAGS="-g" cargo build --target wasm32-unknown-unknown --release
   popd
 done
