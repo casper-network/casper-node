9f8cc0c5dffc3b4222a7551b52ed0b3ab3ce6ea5
Jan Hoffmann <jan@hfmn.pl>
2024-02-29 13:04:44 +0100

    implemented should_not_burn_excess_tokens test
    Co-authored-by: igor-casper

--- execution_engine_testing/tests/src/test/system_contracts/mint.rs
+++ execution_engine_testing/tests/src/test/system_contracts/mint.rs
@@ -44,6 +44,7 @@ fn should_burn_tokens_from_provided_purse() {
         U512::from(TEST_DELEGATOR_INITIAL_ACCOUNT_BALANCE),
     );
 
+    let initial_supply = builder.total_supply(None);
     let purse_name = "purse";
     let purse_amount = U512::from(10_000_000_000u64);
 
@@ -125,5 +126,101 @@ fn should_burn_tokens_from_provided_purse() {
             .cloned()
             .unwrap(),
         num_of_tokens_after_burn
+    );
+
+    let supply_after_burns = builder.total_supply(None);
+    let expected_supply_after_burns = initial_supply - U512::from(10_000_000_000u64);
+
+    assert_eq!(
+        supply_after_burns,
+        expected_supply_after_burns
+    );
+}
+
+#[ignore]
+#[test]
+fn should_not_burn_excess_tokens() {
+    let data_dir = TempDir::new().expect("should create temp dir");
+    let mut builder = LmdbWasmTestBuilder::new(data_dir.as_ref());
+    let source = DEFAULT_ACCOUNT_ADDR.clone();
+
+    let delegator_keys = auction::generate_public_keys(1);
+    let validator_keys = auction::generate_public_keys(1);
+
+    auction::run_genesis_and_create_initial_accounts(
+        &mut builder,
+        &validator_keys,
+        delegator_keys
+            .iter()
+            .map(|public_key| public_key.to_account_hash())
+            .collect::<Vec<_>>(),
+        U512::from(TEST_DELEGATOR_INITIAL_ACCOUNT_BALANCE),
+    );
+
+    let initial_supply = builder.total_supply(None);
+    let purse_name = "purse";
+    let purse_amount = U512::from(10_000_000_000u64);
+
+    // Create purse and transfer tokens to it
+    let exec_request = ExecuteRequestBuilder::standard(
+        source,
+        CONTRACT_TRANSFER_TO_NAMED_PURSE,
+        runtime_args! {
+            ARG_PURSE_NAME => purse_name,
+            ARG_AMOUNT => purse_amount,
+        },
+    )
+    .build();
+
+    builder.exec(exec_request).expect_success().commit();
+
+    let account = builder
+        .get_account(source.clone())
+        .expect("should have account");
+
+    let purse_uref: URef = account.named_keys()[purse_name]
+        .into_uref()
+        .expect("should be uref");
+
+    assert_eq!(
+        builder
+            .get_purse_balance_result(purse_uref.clone())
+            .motes()
+            .cloned()
+            .unwrap(),
+        purse_amount
+    );
+
+    // Try to burn more then in a purse
+    let num_of_tokens_to_burn = U512::MAX;
+    let num_of_tokens_after_burn = U512::from(8_000_000_000u64);
+
+    let exec_request = ExecuteRequestBuilder::standard(
+        source,
+        CONTRACT_BURN,
+        runtime_args! {
+            ARG_PURSE_NAME => purse_name.clone(),
+            ARG_AMOUNT => num_of_tokens_to_burn,
+        },
+    )
+    .build();
+
+    builder.exec(exec_request).expect_success().commit();
+
+    assert_eq!(
+        builder
+            .get_purse_balance_result(purse_uref.clone())
+            .motes()
+            .cloned()
+            .unwrap(),
+        U512::zero()
+    );
+
+    let supply_after_burns = builder.total_supply(None);
+    let expected_supply_after_burns = initial_supply - U512::from(10_000_000_000u64);
+
+    assert_eq!(
+        supply_after_burns,
+        expected_supply_after_burns
     );
 }
