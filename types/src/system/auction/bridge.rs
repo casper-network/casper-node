91640ab575c9827da2422d93879907d0ab02b632
Maciej Wójcik <wojcik91@gmail.com>
2024-03-18 15:06:27 +0100

    fix generated event schema

--- types/src/system/auction/bridge.rs
+++ types/src/system/auction/bridge.rs
@@ -6,7 +6,7 @@ use datasize::DataSize;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-/// An entry in the validator map.
+/// A bridge record pointing to a new `ValidatorBid` after the public key was changed..
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Clone)]
 #[cfg_attr(feature = "datasize", derive(DataSize))]
 #[cfg_attr(feature = "json-schema", derive(JsonSchema))]
