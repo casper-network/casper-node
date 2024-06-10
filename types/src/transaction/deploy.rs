337884f7d2b2e1857368222ad38a02d72925aa65
gRoussac <gRoussac@users.noreply.github.com>
2024-06-10 10:48:51 +0200

    Itertools not std

--- types/src/transaction/deploy.rs
+++ types/src/transaction/deploy.rs
@@ -19,7 +19,6 @@ use std::convert::TryFrom;
 
 #[cfg(feature = "datasize")]
 use datasize::DataSize;
-#[cfg(any(feature = "std", test))]
 use itertools::Itertools;
 #[cfg(feature = "json-schema")]
 use once_cell::sync::Lazy;
