1b29141042b01f632630fefbfec9716f647052d5
Alexandru Sardan <alexandru@casperlabs.io>
2024-07-05 15:01:10 +0000

    reactor_tests: add test for checking that contracts properly revert
    Signed-off-by: Alexandru Sardan <alexandru@casperlabs.io>

--- smart_contracts/contracts/test/endless-loop/src/main.rs
+++ smart_contracts/contracts/test/endless-loop/src/main.rs
@@ -1,18 +1,19 @@
 #![no_std]
 #![no_main]
 
-#[macro_use]
 extern crate alloc;
 
-use casper_contract::contract_api::{account, storage};
-use casper_types::bytesrepr::Bytes;
+use casper_contract::contract_api::{account, runtime, storage};
+use casper_types::Key;
 
 #[no_mangle]
 pub extern "C" fn call() {
-    let uref = storage::new_uref(());
+    let mut data: u32 = 1;
+    let uref = storage::new_uref(data);
+    runtime::put_key("new_key", Key::from(uref));
     loop {
         let _ = account::get_main_purse();
-        let data: Bytes = vec![0u8; 4096].into();
+        data += 1;
         storage::write(uref, data);
     }
 }
