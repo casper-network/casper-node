5003f03908f82152f7ee4404feb529f6d9fb2147
Jacek Chmielewski <jchmielewski@teonite.com>
2024-07-11 16:57:27 +0200

    Stub add/cancel reservations smart contracts

--- smart_contracts/contracts/client/add-reservations/src/main.rs
+++ smart_contracts/contracts/client/add-reservations/src/main.rs
@@ -8,18 +8,9 @@ use alloc::vec::Vec;
 use casper_contract::contract_api::{runtime, system};
 use casper_types::{runtime_args, system::auction, PublicKey, U512};
 
-const ARG_VALIDATOR: &str = "validator";
-const ARG_DELEGATORS: &str = "delegators";
 
 fn add_reservations(validator: PublicKey, delegators: &[PublicKey]) {
-    let contract_hash = system::get_auction();
-    for delegator in delegators {
-        let args = runtime_args! {
-            auction::ARG_DELEGATOR => delegator,
-            auction::ARG_VALIDATOR => validator.clone(),
-        };
-        runtime::call_contract::<U512>(contract_hash, auction::METHOD_ADD_RESERVATION, args);
-    }
+    todo!();
 }
 
 // Add delegators to validator's reserved list.
@@ -28,8 +19,8 @@ fn add_reservations(validator: PublicKey, delegators: &[PublicKey]) {
 // Issues an add_reservation request to the auction contract.
 #[no_mangle]
 pub extern "C" fn call() {
-    let delegators: Vec<PublicKey> = runtime::get_named_arg(ARG_DELEGATORS);
-    let validator = runtime::get_named_arg(ARG_VALIDATOR);
+    let delegators: Vec<PublicKey> = runtime::get_named_arg(auction::ARG_DELEGATORS);
+    let validator = runtime::get_named_arg(auction::ARG_VALIDATOR);
 
     add_reservations(validator, &delegators);
 }
--- smart_contracts/contracts/client/cancel-reservations/src/main.rs
+++ smart_contracts/contracts/client/cancel-reservations/src/main.rs
@@ -8,18 +8,9 @@ use alloc::vec::Vec;
 use casper_contract::contract_api::{runtime, system};
 use casper_types::{runtime_args, system::auction, PublicKey, U512};
 
-const ARG_VALIDATOR: &str = "validator";
-const ARG_DELEGATORS: &str = "delegators";
 
 fn cancel_reservations(validator: PublicKey, delegators: &[PublicKey]) {
-    let contract_hash = system::get_auction();
-    for delegator in delegators {
-        let args = runtime_args! {
-            auction::ARG_DELEGATOR => delegator,
-            auction::ARG_VALIDATOR => validator.clone(),
-        };
-        runtime::call_contract::<U512>(contract_hash, auction::METHOD_CANCEL_RESERVATION, args);
-    }
+    todo!();
 }
 
 // Remove delegators from validator's reserved list.
@@ -28,8 +19,8 @@ fn cancel_reservations(validator: PublicKey, delegators: &[PublicKey]) {
 // Issues a cancel_reservation request to the auction contract.
 #[no_mangle]
 pub extern "C" fn call() {
-    let delegators: Vec<PublicKey> = runtime::get_named_arg(ARG_DELEGATORS);
-    let validator = runtime::get_named_arg(ARG_VALIDATOR);
+    let delegators: Vec<PublicKey> = runtime::get_named_arg(auction::ARG_DELEGATORS);
+    let validator = runtime::get_named_arg(auction::ARG_VALIDATOR);
 
     cancel_reservations(validator, &delegators);
 }
