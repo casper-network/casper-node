effcb4c0a9e9838103712264e1750990a5aecf0d
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2024-08-07 18:50:45 -0400

    Format correction.

--- execution_engine_testing/tests/src/test/system_contracts/auction/distribute.rs
+++ execution_engine_testing/tests/src/test/system_contracts/auction/distribute.rs
@@ -11,10 +11,10 @@ use casper_engine_test_support::{
     PRODUCTION_ROUND_SEIGNIORAGE_RATE, PRODUCTION_RUN_GENESIS_REQUEST, SYSTEM_ADDR,
     TIMESTAMP_MILLIS_INCREMENT,
 };
+use casper_execution_engine::core::engine_state::engine_config::DEFAULT_MINIMUM_BID_AMOUNT;
 use casper_execution_engine::core::engine_state::{
     engine_config::DEFAULT_MINIMUM_DELEGATION_AMOUNT, step::RewardItem,
 };
-use casper_execution_engine::core::engine_state::engine_config::DEFAULT_MINIMUM_BID_AMOUNT;
 use casper_types::{
     self,
     account::AccountHash,
