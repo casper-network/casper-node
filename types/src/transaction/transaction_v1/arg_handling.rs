14e0614c8575c1d60dd1838a32969f6a8e631d02
Maciej Wójcik <maciek@wjck.pl>
2024-10-23 11:00:52 +0200

    post-merge fixes

--- types/src/transaction/transaction_v1/arg_handling.rs
+++ types/src/transaction/transaction_v1/arg_handling.rs
@@ -55,10 +55,13 @@ const CHANGE_BID_PUBLIC_KEY_ARG_PUBLIC_KEY: RequiredArg<PublicKey> = RequiredArg
 const CHANGE_BID_PUBLIC_KEY_ARG_NEW_PUBLIC_KEY: RequiredArg<PublicKey> =
     RequiredArg::new("new_public_key");
 
+#[cfg(any(all(feature = "std", feature = "testing"), test))]
 const ADD_RESERVATIONS_ARG_RESERVATIONS: RequiredArg<Vec<Reservation>> =
     RequiredArg::new("reservations");
 
+#[cfg(any(all(feature = "std", feature = "testing"), test))]
 const CANCEL_RESERVATIONS_ARG_VALIDATOR: RequiredArg<PublicKey> = RequiredArg::new("validator");
+#[cfg(any(all(feature = "std", feature = "testing"), test))]
 const CANCEL_RESERVATIONS_ARG_DELEGATORS: RequiredArg<Vec<PublicKey>> =
     RequiredArg::new("delegators");
 
@@ -357,7 +360,6 @@ pub fn has_valid_activate_bid_args(args: &RuntimeArgs) -> Result<(), InvalidTran
 }
 
 /// Checks the given `RuntimeArgs` are suitable for use in a change bid public key transaction.
-#[allow(dead_code)]
 #[cfg(any(all(feature = "std", feature = "testing"), test))]
 pub fn has_valid_change_bid_public_key_args(
     args: &RuntimeArgs,
@@ -368,12 +370,14 @@ pub fn has_valid_change_bid_public_key_args(
 }
 
 /// Checks the given `RuntimeArgs` are suitable for use in a add reservations transaction.
+#[cfg(any(all(feature = "std", feature = "testing"), test))]
 pub fn has_valid_add_reservations_args(args: &RuntimeArgs) -> Result<(), InvalidTransactionV1> {
     let _reservations = ADD_RESERVATIONS_ARG_RESERVATIONS.get(args)?;
     Ok(())
 }
 
 /// Checks the given `RuntimeArgs` are suitable for use in a add reservations transaction.
+#[cfg(any(all(feature = "std", feature = "testing"), test))]
 pub fn has_valid_cancel_reservations_args(args: &RuntimeArgs) -> Result<(), InvalidTransactionV1> {
     let _validator = CANCEL_RESERVATIONS_ARG_VALIDATOR.get(args)?;
     let _delegators = CANCEL_RESERVATIONS_ARG_DELEGATORS.get(args)?;
