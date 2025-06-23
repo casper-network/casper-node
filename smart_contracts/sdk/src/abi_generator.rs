95676bf31672c72023b7e0f822116e3c305fa8f9
Michał Papierski <michal@casper.network>
2025-06-23 15:16:27 +0200

    Write schema in a canonical JSON format

--- smart_contracts/sdk/src/abi_generator.rs
+++ smart_contracts/sdk/src/abi_generator.rs
@@ -102,8 +102,10 @@ pub fn casper_collect_schema() -> Schema {
 #[export_name = "__cargo_casper_collect_schema"]
 pub unsafe extern "C" fn cargo_casper_collect_schema(size_ptr: *mut u64) -> *mut u8 {
     let schema = casper_collect_schema();
-    // Write the schema using the provided writer
-    let mut json_bytes = serde_json::to_vec(&schema).expect("Serialized schema");
+    // Convert the schema to a JSON value.
+    let schema: serde_json::Value = serde_json::to_value(schema).expect("Schema to JSON conversion");
+    // Write the schema using the provided writer in a canonical JSON format.
+    let mut json_bytes = canonical_json::to_string(&schema).expect("Serialized schema");
     NonNull::new(size_ptr)
         .expect("expected non-null ptr")
         .write(json_bytes.len().try_into().expect("usize to u64"));
