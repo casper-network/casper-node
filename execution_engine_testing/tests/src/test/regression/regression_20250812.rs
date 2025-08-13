21162889503adc529bc428d62f5f2e77ab67471d
Karan Dhareshwar <karan@casperlabs.io>
2025-08-13 07:59:33 -0500

    Add function to use chainspec config

--- execution_engine_testing/tests/src/test/regression/regression_20250812.rs
+++ execution_engine_testing/tests/src/test/regression/regression_20250812.rs
@@ -1,19 +1,22 @@
 use casper_engine_test_support::{
-    utils::create_run_genesis_request, ChainspecConfig, ExecuteRequestBuilder, LmdbWasmTestBuilder,
-    DEFAULT_ACCOUNTS, DEFAULT_ACCOUNT_ADDR,
+    utils::create_run_genesis_request_with_chainspec_config, ChainspecConfig,
+    ExecuteRequestBuilder, LmdbWasmTestBuilder, DEFAULT_ACCOUNTS, DEFAULT_ACCOUNT_ADDR,
 };
 use casper_types::RuntimeArgs;
 
-const DO_NOTHING_CONTRACT: &str = "do_nothing.wasm";
+const DO_NOTHING_CONTRACT: &str = "do_nothing_stored.wasm";
 
 #[ignore]
 #[test]
 fn should_correctly_install_and_add_contract_version_with_ae_turned_on() {
-    let chainspec = ChainspecConfig::default().with_enable_addressable_entity(true); // false makes test succeed
+    let chainspec = ChainspecConfig::default().with_enable_addressable_entity(true);
 
-    let mut builder = LmdbWasmTestBuilder::new_temporary_with_config(chainspec);
+    let mut builder = LmdbWasmTestBuilder::new_temporary_with_config(chainspec.clone());
     builder
-        .run_genesis(create_run_genesis_request(DEFAULT_ACCOUNTS.to_vec()))
+        .run_genesis(create_run_genesis_request_with_chainspec_config(
+            DEFAULT_ACCOUNTS.to_vec(),
+            chainspec,
+        ))
         .commit();
 
     let install_request_1 = ExecuteRequestBuilder::standard(
