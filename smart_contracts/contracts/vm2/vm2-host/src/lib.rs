ab6722ef7ee439a4f4cff4a69853c676cfb66ceb
Ed Hastings <ed@casper.network>
2025-08-14 08:31:38 -0700

    removed unused TrapCode variants

--- smart_contracts/contracts/vm2/vm2-host/src/lib.rs
+++ smart_contracts/contracts/vm2/vm2-host/src/lib.rs
@@ -115,6 +115,16 @@ impl MinimalHostWrapper {
         casper::casper_call(&[0u8; 32], 0, "", &[]).1.ok();
     }
 
+    pub fn call_add_bid(&self) {
+        // public key must match initiator's account hash
+        casper::casper_call(&[0u8; 32], 0, "", &[]).1.ok();
+    }
+
+    pub fn call_delegate(&self) {
+        // should be able to send delegate from purse?
+        casper::casper_call(&[0u8; 32], 0, "", &[]).1.ok();
+    }
+
     pub fn input(&self) {
         casper::copy_input();
     }
