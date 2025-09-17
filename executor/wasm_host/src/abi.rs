e9abb4f4d1ba88c9b5e8bfdaad420bcc308728fe
igor-casper <igor@casper.network>
2025-09-17 10:38:34 +0200

    remove safe transmute from deps

--- executor/wasm_host/src/abi.rs
+++ executor/wasm_host/src/abi.rs
@@ -8,9 +8,6 @@ pub(crate) struct ReadInfo {
     pub(crate) data_size: u32,
 }
 
-#[cfg(test)]
-unsafe impl safe_transmute::TriviallyTransmutable for ReadInfo {}
-
 #[repr(C)]
 #[derive(Copy, Clone, Debug, PartialEq, BorshSerialize)]
 
@@ -23,9 +20,6 @@ const _: () = assert!(
     "CreateResult must be 32 bytes"
 );
 
-#[cfg(test)]
-unsafe impl safe_transmute::TriviallyTransmutable for CreateResult {}
-
 #[derive(Clone, Copy, BorshSerialize, Debug, PartialEq)]
 #[repr(C)]
 pub struct EnvInfo {
@@ -35,66 +29,4 @@ pub struct EnvInfo {
     pub caller_kind: u32,
     pub callee_addr: [u8; 32],
     pub callee_kind: u32,
-}
-
-#[cfg(test)]
-unsafe impl safe_transmute::TriviallyTransmutable for EnvInfo {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn serialization_matches_transmute() {
-        let env_info = EnvInfo {
-            block_time: 1234567890,
-            transferred_value: 1000,
-            caller_addr: [1; 32],
-            caller_kind: 2,
-            callee_addr: [3; 32],
-            callee_kind: 4,
-        };
-
-        let borsh_serialized = borsh::to_vec(&env_info).unwrap();
-        let transmuted_object: EnvInfo = safe_transmute::transmute_one(&borsh_serialized).unwrap();
-
-        assert_eq!(env_info, transmuted_object);
-
-        let transmuted_bytes = safe_transmute::transmute_one_to_bytes(&env_info);
-        assert_eq!(borsh_serialized, transmuted_bytes);
-    }
-
-    #[test]
-    fn read_info_transmute() {
-        let read_info = ReadInfo {
-            data_ptr: 42,
-            data_size: 100,
-        };
-
-        let transmuted_bytes = safe_transmute::transmute_one_to_bytes(&read_info);
-        let transmuted_object: ReadInfo = safe_transmute::transmute_one(&transmuted_bytes).unwrap();
-
-        assert_eq!(read_info, transmuted_object);
-        assert_eq!(
-            transmuted_bytes,
-            safe_transmute::transmute_one_to_bytes(&transmuted_object)
-        );
-    }
-
-    #[test]
-    fn create_result_transmute() {
-        let create_result = CreateResult {
-            package_address: [1; 32],
-        };
-        let transmuted_bytes = safe_transmute::transmute_one_to_bytes(&create_result);
-        let transmuted_object: CreateResult =
-            safe_transmute::transmute_one(&transmuted_bytes).unwrap();
-        assert_eq!(create_result, transmuted_object);
-
-        assert_eq!(transmuted_bytes.len(), std::mem::size_of::<CreateResult>());
-        assert_eq!(
-            transmuted_bytes,
-            safe_transmute::transmute_one_to_bytes(&transmuted_object)
-        );
-    }
 }
