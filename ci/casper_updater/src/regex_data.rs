da9c2993190fe3891d2f119bdd796ed1e92f0b71
Karan Dhareshwar <karan@casper.network>
2025-10-23 12:28:16 -0500

    Version bump crates

--- ci/casper_updater/src/regex_data.rs
+++ ci/casper_updater/src/regex_data.rs
@@ -314,7 +314,7 @@ pub mod smart_contracts_sdk_sys {
     pub static DEPENDENT_FILES: Lazy<Vec<DependentFile>> = Lazy::new(|| {
         vec![
             DependentFile::new(
-                "smart_contracts/sdk_sys/Cargo.toml",
+                "smart_contracts/vm2/sdk_sys/Cargo.toml",
                 MANIFEST_VERSION_REGEX.clone(),
                 replacement,
             ),
@@ -329,12 +329,12 @@ pub mod smart_contracts_sdk_sys {
                 replacement,
             ),
             DependentFile::new(
-                "smart_contracts/macros/Cargo.toml",
+                "smart_contracts/vm2/macros/Cargo.toml",
                 SMART_CONTRACTS_SDK_SYS_VERSION_REGEX.clone(),
                 replacement,
             ),
             DependentFile::new(
-                "smart_contracts/sdk/Cargo.toml",
+                "smart_contracts/vm2/sdk/Cargo.toml",
                 SMART_CONTRACTS_SDK_SYS_VERSION_REGEX.clone(),
                 replacement,
             ),
@@ -347,7 +347,7 @@ pub mod smart_contracts_sdk {
 
     pub static DEPENDENT_FILES: Lazy<Vec<DependentFile>> = Lazy::new(|| {
         vec![DependentFile::new(
-            "smart_contracts/sdk/Cargo.toml",
+            "smart_contracts/vm2/sdk/Cargo.toml",
             MANIFEST_VERSION_REGEX.clone(),
             replacement,
         )]
@@ -360,12 +360,12 @@ pub mod smart_contracts_macros {
     pub static DEPENDENT_FILES: Lazy<Vec<DependentFile>> = Lazy::new(|| {
         vec![
             DependentFile::new(
-                "smart_contracts/macros/Cargo.toml",
+                "smart_contracts/vm2/macros/Cargo.toml",
                 MANIFEST_VERSION_REGEX.clone(),
                 replacement,
             ),
             DependentFile::new(
-                "smart_contracts/sdk/Cargo.toml",
+                "smart_contracts/vm2/sdk/Cargo.toml",
                 Regex::new(r#"(?m)(^casper-contract-macros = \{[^\}]*version = )"(?:[^"]+)"#)
                     .unwrap(),
                 replacement,
@@ -408,12 +408,12 @@ pub mod executor_wasm_common {
                 replacement,
             ),
             DependentFile::new(
-                "smart_contracts/macros/Cargo.toml",
+                "smart_contracts/vm2/macros/Cargo.toml",
                 EXECUTOR_WASM_COMMON_VERSION_REGEX.clone(),
                 replacement,
             ),
             DependentFile::new(
-                "smart_contracts/sdk/Cargo.toml",
+                "smart_contracts/vm2/sdk/Cargo.toml",
                 EXECUTOR_WASM_COMMON_VERSION_REGEX.clone(),
                 replacement,
             ),
